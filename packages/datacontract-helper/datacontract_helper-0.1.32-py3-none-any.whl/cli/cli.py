2918ce3aba2983a4c9ffa99fca62d3c7f26d201c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 08:04:06 +0000

    Add package datacontract-helper/0.1.32/datacontract_helper-0.1.32-py3-none-any.whl

---
+++ packages/datacontract-helper/datacontract_helper-0.1.32-py3-none-any.whl/cli/cli.py
@@ -0,0 +1,59 @@
+import logging
+import re
+import click
+import json
+# from src import commands
+import commands
+from pathlib import Path
+import sys
+
+
+
+
+
+
+# Добавляем текущую директорию в путь импорта
+current_dir = Path(__file__).parent.absolute()
+sys.path.insert(0, str(current_dir))
+
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+
+
+@click.group()
+@click.pass_context
+def cli(
+    ctx,
+):
+    ctx.ensure_object(dict)
+    print("is cli")
+
+
+@cli.command()
+# @click.option("--new-schema", required=True, envvar="new_schema", type=dict)
+@click.option("--new-schema", required=True, envvar="NEW_SCHEMA", help="JSON string with schema")
+# @click.option("--new-schema", required=True, envvar="NEW_SCHEMA_PATH", type=click.Path(exists=True), help="Path to JSON schema file")
+@click.pass_context
+def validate(ctx, new_schema: str):
+    # try:
+    #     with open(new_schema, 'r') as f:
+    #         schema_data = json.load(f)
+    # except (json.JSONDecodeError, IOError) as e:
+    #     raise click.BadParameter(f"Error loading schema: {e}")
+    
+    try:
+        new_schema = json.loads(new_schema)
+    except json.JSONDecodeError as e:
+        raise click.BadParameter(f"Invalid JSON: {e}")
+    
+    cmd = commands.Validate(new_schema=new_schema)
+    cmd.do_run()
+
+
+@cli.command()
+@click.pass_context
+def publish_package(ctx):
+    cmd = commands.PublishPackage()
+    cmd.do_run()
