53562e4141ecf085b738cb4589f08d1cce84ef67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 04:03:38 +0000

    Add package agentstate/1.0.0/agentstate-1.0.0.tar.gz

---
+++ packages/agentstate/agentstate-1.0.0.tar.gz/agentstate-1.0.0/agentstate/__init__.py
@@ -0,0 +1,11 @@
+from .client import AgentStateClient
+
+# For backward compatibility
+State = AgentStateClient
+
+__version__ = "1.0.0"
+__author__ = "Ayush Mittal"
+__email__ = "ayushsmittal@gmail.com"
+
+__all__ = ["AgentStateClient", "State"]
+
