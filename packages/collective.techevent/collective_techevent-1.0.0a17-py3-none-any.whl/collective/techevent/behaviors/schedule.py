cafe44c09d555012e25fcd46096daee52c87c5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 08:04:00 +0000

    Add package collective.techevent/1.0.0a17/collective_techevent-1.0.0a17-py3-none-any.whl

---
+++ packages/collective.techevent/collective_techevent-1.0.0a17-py3-none-any.whl/collective/techevent/behaviors/schedule.py
@@ -0,0 +1,60 @@
+from collective.techevent import _
+from collective.techevent.utils.dates import default_end
+from collective.techevent.utils.dates import default_start
+from datetime import datetime
+from plone.app.event.base import default_timezone
+from plone.app.event.dx.behaviors import StartBeforeEnd
+from plone.app.z3cform.widgets.datetime import DatetimeFieldWidget
+from plone.autoform import directives
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.supermodel import model
+from zope import schema
+from zope.interface import invariant
+from zope.interface import provider
+
+
+@provider(IFormFieldProvider)
+class IScheduleSlot(model.Schema):
+    """Slot in the event."""
+
+    model.fieldset(
+        "schedule",
+        label=_("Schedule"),
+        fields=["start", "end", "room"],
+    )
+
+    start = schema.Datetime(
+        title=_("Start time"), required=False, defaultFactory=default_start
+    )
+    directives.widget(
+        "start",
+        DatetimeFieldWidget,
+        default_timezone=default_timezone,
+        klass="event_start",
+    )
+
+    end = schema.Datetime(
+        title=_("End time"), required=False, defaultFactory=default_end
+    )
+    directives.widget(
+        "end",
+        DatetimeFieldWidget,
+        default_timezone=default_timezone,
+        klass="event_end",
+    )
+
+    @invariant
+    def validate_start_end(data):
+        start: datetime | None = data.start
+        end: datetime | None = data.end
+        if (start and end) and start > end:
+            raise StartBeforeEnd(_("End time must be after start time."))
+
+    room = schema.Set(
+        title=_("Room"),
+        description=_("Room the activity will take place"),
+        value_type=schema.Choice(
+            vocabulary="collective.techevent.vocabularies.slot_rooms",
+        ),
+        required=False,
+    )
