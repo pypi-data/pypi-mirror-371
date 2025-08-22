e99539cdc5a8f60f4a5d494ffb4a35ec8d3fd6c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 05:04:22 +0000

    Add package fc-model/1.1.2/fc_model-1.1.2-py3-none-any.whl

---
+++ packages/fc-model/fc_model-1.1.2-py3-none-any.whl/fc_model/fc_set.py
@@ -0,0 +1,33 @@
+from typing import TypedDict
+
+from numpy import dtype, int32
+
+from .fc_value import FCValue
+
+
+class FCSrcSet(TypedDict):
+    id: int
+    name: str
+    apply_to: str
+    apply_to_size: int
+
+
+class FCSet:
+    id: int
+    apply: FCValue
+    name: str
+
+    def __init__(self, src_data: FCSrcSet):
+        self.apply = FCValue(src_data['apply_to'], dtype(int32))
+        self.apply.resize(src_data['apply_to_size'])
+        self.id = src_data['id']
+        self.name = src_data['name']
+
+    def dump(self) -> FCSrcSet:
+        return {
+            "apply_to": self.apply.dump(),
+            "apply_to_size": len(self.apply),
+            "id": self.id,
+            "name": self.name
+        }
+
