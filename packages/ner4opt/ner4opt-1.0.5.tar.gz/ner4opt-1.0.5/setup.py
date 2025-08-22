155a3bed5266dfdb94ffbe22381d36efdc95b4e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 07:05:13 +0000

    Add package ner4opt/1.0.5/ner4opt-1.0.5.tar.gz

---
+++ packages/ner4opt/ner4opt-1.0.5.tar.gz/ner4opt-1.0.5/setup.py
@@ -0,0 +1,33 @@
+import os
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as fh:
+    required = fh.read().splitlines()
+
+with open(os.path.join("ner4opt", "_version.py")) as fp:
+    exec(fp.read())
+
+setuptools.setup(
+    name="ner4opt",
+    description="NER4OPT Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=__version__,
+    url="https://github.com/skadio/ner4opt",
+    packages=(["models"] + setuptools.find_packages()),
+    include_package_data=True,
+    install_requires=required,
+    python_requires=">=3.8,<3.9",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        "Source": "https://github.com/skadio/ner4opt",
+    },
+)
