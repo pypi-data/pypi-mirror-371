748b3639db5092342bf84d34256eba041590400e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 07:05:41 +0000

    Add package supadata/1.4.0/supadata-1.4.0-py3-none-any.whl

---
+++ packages/supadata/supadata-1.4.0-py3-none-any.whl/supadata/__init__.py
@@ -0,0 +1,44 @@
+"""
+Supadata Python SDK
+
+The official Python SDK for Supadata - scrape web and YouTube content with ease.
+"""
+
+from importlib.metadata import version
+
+from supadata.client import Supadata
+from supadata.errors import SupadataError
+from supadata.types import (
+    CrawlJob,
+    CrawlPage,
+    CrawlResponse,
+    Map,
+    Scrape,
+    Transcript,
+    TranscriptChunk,
+    TranslatedTranscript,
+    YoutubeChannel,
+    YoutubePlaylist,
+    YoutubeVideo,
+    YoutubeSearchResponse,
+    YoutubeSearchResult,
+)
+
+__version__ = version("supadata")
+__all__ = [
+    "Supadata",
+    "Transcript",
+    "TranslatedTranscript",
+    "TranscriptChunk",
+    "Scrape",
+    "Map",
+    "SupadataError",
+    "CrawlJob",
+    "CrawlPage",
+    "CrawlResponse",
+    "YoutubeChannel",
+    "YoutubePlaylist",
+    "YoutubeVideo",
+    "YoutubeSearchResponse",
+    "YoutubeSearchResult",
+]
