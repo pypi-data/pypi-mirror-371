ef211731bca3af4c3b5a509cbd4bf756eeef79fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 03:20:54 +0000

    Add package pythonic-xray/0.4/pythonic-xray-0.4.tar.gz

---
+++ packages/pythonic-xray/pythonic-xray-0.4.tar.gz/pythonic-xray-0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pythonic-xray',
+    version='0.4',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies here, e.g. 'numpy', 'requests'
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    include_package_data=True,  # Ensures files from MANIFEST.in are included
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
