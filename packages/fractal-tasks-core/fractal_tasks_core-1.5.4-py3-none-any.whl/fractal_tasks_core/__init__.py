076ff4cc56acdabcf4d5b1ac8fd7cdc826ac114e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 04:04:18 +0000

    Add package fractal-tasks-core/1.5.4/fractal_tasks_core-1.5.4-py3-none-any.whl

---
+++ packages/fractal-tasks-core/fractal_tasks_core-1.5.4-py3-none-any.whl/fractal_tasks_core/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s; %(levelname)s; %(message)s"
+)
+
+
+__VERSION__ = "1.5.4"
+__OME_NGFF_VERSION__ = "0.4"
+__FRACTAL_TABLE_VERSION__ = "1"
