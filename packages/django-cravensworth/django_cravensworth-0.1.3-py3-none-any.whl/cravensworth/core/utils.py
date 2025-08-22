aeb98ebeaff072924bb74d9453ce0c9b9f34d4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 07:05:44 +0000

    Add package django-cravensworth/0.1.3/django_cravensworth-0.1.3-py3-none-any.whl

---
+++ packages/django-cravensworth/django_cravensworth-0.1.3-py3-none-any.whl/cravensworth/core/utils.py
@@ -0,0 +1,54 @@
+from datetime import timedelta
+from uuid import uuid4
+from django.http import HttpRequest, HttpResponse
+
+from cravensworth.core.conf import get_setting
+
+
+DEFAULT_TRACKING_COOKIE = '__cwtk'
+TRACKING_KEY_KEY = object()
+
+
+def _tracking_cookie() -> str:
+    return get_setting('TRACKING_COOKIE', DEFAULT_TRACKING_COOKIE)
+
+
+def generate_tracking_key() -> str:
+    """
+    Generate a new tracking key.
+    """
+    return uuid4().hex
+
+
+def get_tracking_key(request: HttpRequest) -> str:
+    """
+    Gets the tracking key from a request if it exists. If the tracking key does
+    not exist, one will be created and added to the request, so it may be
+    consumed later, if required.
+    """
+    tk = request.META.get(TRACKING_KEY_KEY)
+    if tk is None:
+        tk = request.COOKIES.get(_tracking_cookie())
+    if tk is None:
+        tk = generate_tracking_key()
+    request.META[TRACKING_KEY_KEY] = tk
+    return tk
+
+
+def set_tracking_key(request: HttpRequest, response: HttpResponse):
+    """
+    Sets the current tracking key as a cookie on the provided response. If no
+    key exists, a new one will be generated.
+    """
+    name = _tracking_cookie()
+    value = get_tracking_key(request)
+    response.set_cookie(
+        name,
+        value,
+        max_age=timedelta(days=365),
+        httponly=True,
+        samesite='Lax',
+    )
+
+
+__all__ = []
