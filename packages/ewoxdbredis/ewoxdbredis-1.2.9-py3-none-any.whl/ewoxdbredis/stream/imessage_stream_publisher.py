641d922cfe27cf052a4b24ab75d31446e515960f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 03:20:20 +0000

    Add package ewoxdbredis/1.2.9/ewoxdbredis-1.2.9-py3-none-any.whl

---
+++ packages/ewoxdbredis/ewoxdbredis-1.2.9-py3-none-any.whl/ewoxdbredis/stream/imessage_stream_publisher.py
@@ -0,0 +1,25 @@
+from typing import Any, Callable, List, Dict, Text, Optional, Tuple, Union, Awaitable, TypeVar, Type
+from abc import ABC, abstractmethod
+import redis
+from ewoxcore.message.message_args import MessageArgs
+
+T = TypeVar("T", redis.RedisCluster, redis.Redis)
+
+
+class IMessageStreamPublisher(ABC):
+    @abstractmethod
+    async def publish(self, model:T) -> bool:
+        """ Publish a message to the stream. """
+        raise NotImplementedError("Implement inhereted method")
+
+
+    @abstractmethod
+    async def publish_command(self, command:str, model:T) -> bool:
+        """ Publish a command to the stream. """
+        raise NotImplementedError("Implement inhereted method")
+
+
+    @abstractmethod
+    async def publish_message(self, args:MessageArgs) -> bool:
+        """ Publish a message to the stream. """
+        raise NotImplementedError("Implement inhereted method")
