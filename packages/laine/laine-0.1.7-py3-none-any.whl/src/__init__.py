cc1a38dd4515da7b571f20f73c7cb7fbf6a35c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 07:04:27 +0000

    Add package laine/0.1.7/laine-0.1.7-py3-none-any.whl

---
+++ packages/laine/laine-0.1.7-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,11 @@
+"""
+A CLI tool to deobfuscate & download manga chapters from multiple official Japanese providers.
+"""
+
+__version__ = "0.1.7"
+__description__ = "A CLI tool to deobfuscate & download manga chapters from multiple official Japanese providers."
+__repository__ = "syvixor/laine"
+__url_repository__ = "https://github.com"
+__author__ = "syvixor"
+__author_email__ = "syvixor@proton.me"
+__license__ = "MIT"
