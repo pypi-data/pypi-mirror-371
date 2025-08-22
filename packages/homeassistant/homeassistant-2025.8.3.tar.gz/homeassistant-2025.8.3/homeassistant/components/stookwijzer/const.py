212760172546bc566516e3a5e6121ab97f6f5aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 07:04:27 +0000

    Add package homeassistant/2025.8.3/homeassistant-2025.8.3.tar.gz

---
+++ packages/homeassistant/homeassistant-2025.8.3.tar.gz/homeassistant-2025.8.3/homeassistant/components/stookwijzer/const.py
@@ -0,0 +1,10 @@
+"""Constants for the Stookwijzer integration."""
+
+import logging
+from typing import Final
+
+DOMAIN: Final = "stookwijzer"
+LOGGER = logging.getLogger(__package__)
+
+ATTR_CONFIG_ENTRY_ID = "config_entry_id"
+SERVICE_GET_FORECAST = "get_forecast"
