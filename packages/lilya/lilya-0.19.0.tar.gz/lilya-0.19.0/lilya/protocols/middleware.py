69b9cb9a8bbd1b84f1d3bfa1c6f8c62994653ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 04:04:27 +0000

    Add package lilya/0.19.0/lilya-0.19.0.tar.gz

---
+++ packages/lilya/lilya-0.19.0.tar.gz/lilya-0.19.0/lilya/protocols/middleware.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import ParamSpec, Protocol, runtime_checkable
+
+from lilya.types import ASGIApp, Receive, Scope, Send
+
+P = ParamSpec("P")
+
+
+@runtime_checkable
+class MiddlewareProtocol(Protocol[P]):  # pragma: no cover
+    __slots__ = ("app",)
+
+    def __init__(self, app: ASGIApp, *args: P.args, **kwargs: P.kwargs): ...
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None: ...
