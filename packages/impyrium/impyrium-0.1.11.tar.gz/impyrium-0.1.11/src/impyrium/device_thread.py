149140229dc2e965dbf6f8ca5815b5c717b61001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 04:04:22 +0000

    Add package impyrium/0.1.11/impyrium-0.1.11.tar.gz

---
+++ packages/impyrium/impyrium-0.1.11.tar.gz/impyrium-0.1.11/src/impyrium/device_thread.py
@@ -0,0 +1,34 @@
+from PySide6.QtCore import QThread
+import sched
+import time
+import traceback
+
+worker_ = None
+
+class DeviceThread(QThread):
+    def __init__(self):
+        super().__init__()
+        self.scheduler = sched.scheduler(time.time,
+                                    time.sleep)
+
+    def run(self):
+        while (True):
+            try:
+                self.scheduler.run()
+            except Exception as e:
+                print(traceback.format_exc())
+                print("DEVICE THREAD ERROR:", e)
+            time.sleep(0.5)
+
+def scheduleItem(delay, fun, arguments=(), priority=0):
+    return worker_.scheduler.enter(delay, priority, fun, arguments)
+
+def cancel(event):
+    return worker_.scheduler.cancel(event)
+
+def start():
+    global worker_
+    if worker_ == None:
+        worker_ = DeviceThread()
+    worker_.start()
+
