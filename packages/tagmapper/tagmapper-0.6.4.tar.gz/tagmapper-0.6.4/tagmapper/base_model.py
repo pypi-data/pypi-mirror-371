95b72ef099302b5f3c03519db869a4b4a8f811f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 03:20:11 +0000

    Add package tagmapper/0.6.4/tagmapper-0.6.4.tar.gz

---
+++ packages/tagmapper/tagmapper-0.6.4.tar.gz/tagmapper-0.6.4/tagmapper/base_model.py
@@ -0,0 +1,45 @@
+from tagmapper.connector_api import get_json, get_api_url
+import pandas as pd
+from typing import Optional, List
+
+
+def get_data_base_well(
+    facility_names: Optional[List[str]] = None, limit: int = 10, offset: int = 0
+) -> pd.DataFrame:
+    if facility_names is None:
+        facility_names = []
+
+    query_params = []
+    for name in facility_names:
+        query_params.append(f"gov_fcty_name={name.replace(' ', '%20')}")
+
+    query_params.append(f"limit={limit}")
+    query_params.append(f"offset={offset}")
+    query_string = "&".join(query_params)
+
+    url = f"{get_api_url()}/well-attributes-mapped-to-timeseries-base?{query_string}"
+    response = get_json(url)
+    df = pd.DataFrame(response["data"])
+    return df
+
+
+def get_data_base_separator(
+    facility_names: Optional[List[str]] = None, limit: int = 10, offset: int = 0
+) -> pd.DataFrame:
+    if facility_names is None:
+        facility_names = []
+
+    query_params = []
+    for name in facility_names:
+        query_params.append(f"gov_fcty_name={name.replace(' ', '%20')}")
+
+    query_params.append(f"limit={limit}")
+    query_params.append(f"offset={offset}")
+    query_string = "&".join(query_params)
+
+    url = (
+        f"{get_api_url()}/separator-attributes-mapped-to-timeseries-base?{query_string}"
+    )
+    response = get_json(url)
+    df = pd.DataFrame(response["data"])
+    return df
