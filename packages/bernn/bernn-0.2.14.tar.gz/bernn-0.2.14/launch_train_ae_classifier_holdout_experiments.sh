288dce65f794b9796544659cdafea2a5b605bb97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 07:05:17 +0000

    Add package bernn/0.2.14/bernn-0.2.14.tar.gz

---
+++ packages/bernn/bernn-0.2.14.tar.gz/bernn-0.2.14/launch_train_ae_classifier_holdout_experiments.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+n_trials=30  # The number of hyperparameter configurations to try
+n_repeats=5  # The number of times to repeat the experiment for each hyperparameter configuration
+n_epochs=1000  # The number of epochs to train for.
+early_stop=100  # The number of epochs to wait before stopping training if the validation loss does not improve.
+groupkfold=1
+
+dataset=mice
+exp_id=mice_05_24_2025
+csv_file=proteome_log_AgeMice.csv
+path=data
+best_features_file=''
+update_grid=1
+use_l1=1
+n_emb=0
+prune_network=0
+i=0
+max_jobs=1
+current_jobs=0
+
+for train_after_warmup in 1 0
+do
+    for warmup_after_warmup in 1 0
+    do
+        for prune_threshold in 0
+        do
+            for variational in 0 1
+            do
+                for kan in 1
+                do
+                    for dloss in DANN revTriplet normae no inverseTriplet
+                    do
+                        ((current_jobs++))
+                        cuda=$((i%2)) # Divide by the number of gpus available
+                        /home/simonp/anaconda3/envs/bernn_env/bin/python bernn/dl/train/train_ae_classifier_holdout.py --early_stop=$early_stop --n_epochs=$n_epochs \
+                            --zinb=0 --kan=$kan --variational=$variational --train_after_warmup=$train_after_warmup --tied_weights=0 --bdisc=1 \
+                            --rec_loss=l1 --dloss=$dloss --csv_file=$csv_file --remove_zeros=0 --n_meta=$n_emb \
+                            --groupkfold=$groupkfold --embeddings_meta=$n_emb --device=cuda:$cuda --dataset=$dataset --n_trials=$n_trials \
+                            --n_repeats=$n_repeats --exp_id=$exp_id --path=$path --pool=0 --log_metrics=1 \
+                            --best_features_file=$best_features_file --update_grid=$update_grid --use_l1=$use_l1 \
+                            --prune_threshold=$prune_threshold --warmup_after_warmup=$warmup_after_warmup \
+                            --prune_network=$prune_network &
+                        
+                        i=$((i+1))
+                        
+                        if [ $current_jobs -ge $max_jobs ]; then
+                            wait -n
+                            ((current_jobs--))
+                        fi
+						sleep 60
+                    done
+                done
+            done
+        done
+    done
+done
+
+wait
+
+# exp_id=testbenchmark_08_15_2024
+# train_after_warmup=1
+# warmup_after_warmup=1
+# prune_threshold=0.0001
+# variational=0
+# kan=1
+# dloss='inverseTriplet'
+# cuda=0
+# .conda/bin/python bernn/dl/train/train_ae_classifier_holdout.py --early_stop=$early_stop --n_epochs=$n_epochs \
+#     --zinb=0 --kan=$kan --variational=$variational --train_after_warmup=$train_after_warmup --tied_weights=0 --bdisc=1 \
+#     --rec_loss=l1 --dloss=$dloss --csv_file=$csv_file --remove_zeros=0 --n_meta=$n_emb \
+#     --groupkfold=$groupkfold --embeddings_meta=$n_emb --device=cuda:$cuda --dataset=$dataset --n_trials=$n_trials \
+#     --n_repeats=$n_repeats --exp_id=$exp_id --path=$path --pool=0 --log_metrics=1 \
+#     --best_features_file=$best_features_file --update_grid=$update_grid --use_l1=$use_l1 --prune_threshold=$prune_threshold --warmup_after_warmup=$warmup_after_warmup &
---
+++ packages/bernn/bernn-0.2.14.tar.gz/bernn-0.2.14/mlflow_eval_runs.py
@@ -0,0 +1,290 @@
+import csv
+import json
+import mlflow
+import numpy as np
+import itertools
+import argparse
+from pathlib import Path
+
+
+def setup_results_dir(exp_name, exp_id):
+    """Create and return path to results directory for this experiment."""
+    results_dir = Path('results') / f"{exp_name}_{exp_id}"
+    results_dir.mkdir(parents=True, exist_ok=True)
+    return results_dir
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--exp_name', type=str, default='testbenchmark_08_15_2024', help='Name of the experiment to evaluate'
+    )
+    args = parser.parse_args()
+
+    # get runs
+    exp_name = args.exp_name
+
+    # the adenocarcinoma dataset (which I also call amide, but should be changed) has only 3 batches,
+    # so there is only 3 splits possible for training
+    if 'amide' in exp_name or 'bactTest' in exp_name or 'Adeno' in exp_name or 'adeno' in exp_name:
+        n_per_run = 3
+    else:
+        n_per_run = 5
+
+    exp_id = mlflow.get_experiment_by_name(exp_name).experiment_id
+
+    # Create results directory
+    results_dir = setup_results_dir(exp_name, exp_id)
+
+    print(n_per_run, exp_id)
+
+    runs = mlflow.search_runs(exp_id)
+    runs.index = runs['run_id']
+    # group runs by parameter
+    params = [
+        p for p in runs.columns if p.startswith("params") if p != 'params.parameters' and p != 'params.foldername'
+    ]
+    params_common = [p for p in params if len([x for x in runs[p].unique() if x is not None]) == 1]
+    # params_varied = [p for p in params if len([x for x in runs[p].unique() if x is not None]) > 1]
+    params_varied = ['params.dloss', 'params.variational', 'params.kan']
+    groups = list(runs.groupby(params_varied).run_id)
+    # get history for each metric and run and plot mean + std across identical runs
+    client = mlflow.tracking.MlflowClient()
+    metrics = {'_'.join([''.join([s, x]) for s, x in zip(['', 'vae', 'kan'], g[0])]): {} for g in groups}
+    params = [c for c in runs.columns if c.startswith("params")]
+
+    best_metrics = {g: None for g in (metrics.keys())}
+    best_mccs = {g: -np.inf for g in (metrics.keys())}
+
+    c = 0
+    for gg, run_ids in groups:
+        print(gg)
+        best_mcc = -np.inf
+        for r in run_ids:
+            g = '_'.join([''.join([s, x]) for s, x in zip(['', 'vae', 'kan'], gg)])
+            metrics[g][r] = {c[8:]: -np.inf for c in runs.columns if c.startswith("metrics")}
+            c += 1
+
+            # If there is less than the n_per_runs, which was the number of repeated holdout, than we skip this run
+            if len([s.value for s in client.get_metric_history(r, 'valid/mcc')]) < n_per_run:
+                continue
+
+            for metric in metrics[g][r]:
+                try:
+                    values = [s.value for s in client.get_metric_history(r, metric)]
+                except mlflow.exceptions.MlflowException:
+                    continue
+                try:
+                    if len(values) > 0:
+                        val = np.atleast_2d(values)
+                        try:
+                            mean = np.mean(values)
+                            std = np.std(values)
+                            metrics[g][r][metric] = {'mean': mean, 'std': std, 'values': values}
+                        except Exception as e:
+                            print(e)
+                            pass
+                except Exception as e:
+                    print(e)
+                    pass
+            for p in params:
+                metrics[g][r][p] = runs.loc[r, p]
+            if metrics[g][r]['valid/mcc'] is not None:
+                if metrics[g][r]['valid/mcc']['mean'] > best_mccs[g]:
+                    best_metrics[g] = metrics[g][r]
+                    best_metrics[g]['run_id'] = r
+                    best_mccs[g] = metrics[g][r]['valid/mcc']['mean']
+
+            print(c)
+
+    with open(results_dir / f"metrics_mlflow_{exp_name}_{exp_id}.json", "w") as outfile:
+        json.dump(metrics, outfile)
+
+    with open(results_dir / f"best_metrics_mlflow_{exp_name}_{exp_id}.json", "w") as outfile:
+        json.dump(best_metrics, outfile)
+
+    # now we will open a file for writing
+    data_file = open(results_dir / f'best_metrics_mlflow_{exp_name}_{exp_id}.csv', 'w')
+
+    # create the csv writer object
+    csv_writer = csv.writer(data_file)
+
+    # Counter variable used for writing
+    # headers to the CSV file
+    some_metric = list(best_metrics.keys())[0]
+    count = 0
+    for model in best_metrics:
+        if best_metrics[model] is not None:
+            if count == 0:
+                # Writing headers of CSV file
+                try:
+                    header = [["model", 'run_id']] + [
+                        [x for x in list(best_metrics[some_metric].keys()) if 'param' in x and x != 'run_id']
+                    ] + [[
+                        f"{x}_mean", f"{x}_std"
+                    ] for x in list(best_metrics[some_metric].keys()) if 'param' not in x and x != 'run_id']
+                except Exception as e:
+                    print(e)
+                    continue
+                header = list(itertools.chain(*header))
+                csv_writer.writerow(header)
+                count += 1
+            m = [model, best_metrics[model]['run_id']]
+            for metric_id in best_metrics[model]:
+                if 'params' in metric_id and metric_id != 'run_id':
+                    m += [best_metrics[model][metric_id]]
+            for metric_id in best_metrics[model]:
+                if 'params' not in metric_id and metric_id != 'run_id':
+                    try:
+                        m += [best_metrics[model][metric_id]['mean']]
+                        m += [best_metrics[model][metric_id]['std']]
+                    except Exception as e:
+                        print(e)
+                        m += [np.nan]
+                        m += [np.nan]
+                # Writing data of CSV file
+            # if len(m) > 0:
+            csv_writer.writerow(m)
+
+    data_file.close()
+
+    with open(results_dir / f"best_metrics_mlflow_{exp_name}_{exp_id}_values.json", "w") as outfile:
+        json.dump(best_metrics, outfile)
+
+    # now we will open a file for writing
+    data_file = open(results_dir / f'best_metrics_mlflow_{exp_name}_{exp_id}_values.csv', 'w')
+
+    # create the csv writer object
+    csv_writer = csv.writer(data_file)
+
+    # Counter variable used for writing
+    # headers to the CSV file
+    count = 0
+    for model in best_metrics:
+        if best_metrics[model] is not None:
+            if count == 0:
+                # Writing headers of CSV file
+                try:
+                    header = [["model", 'run_id']] + [
+                        [x for x in list(best_metrics[some_metric].keys()) if 'param' in x and x != 'run_id']
+                    ] + [[x] for x in list(best_metrics[some_metric].keys()) if 'param' not in x and x != 'run_id']
+                except Exception as e:
+                    print(e)
+                    continue
+                header = list(itertools.chain(*header))
+                csv_writer.writerow(header)
+                count += 1
+            m = [model, best_metrics[model]['run_id']]
+            # try:
+            for metric_id in best_metrics[model]:
+                if 'params' in metric_id and metric_id != 'run_id':
+                    m += [best_metrics[model][metric_id]]
+            for metric_id in best_metrics[model]:
+                if 'params' not in metric_id and metric_id != 'run_id':
+                    try:
+                        m += [best_metrics[model][metric_id]['values']]
+                    except Exception as e:
+                        print(e)
+                        m += [np.nan]
+                # Writing data of CSV file
+            # except:
+            #     m = []
+            # if len(m) > 0:
+            csv_writer.writerow(m)
+
+    data_file.close()
+
+    # now we will open a file for writing
+    data_file = open(results_dir / f'metrics_mlflow_{exp_name}_{exp_id}.csv', 'w')
+
+    # create the csv writer object
+    csv_writer = csv.writer(data_file)
+
+    ex_id = list(metrics[some_metric].keys())[0]
+
+    # Counter variable used for writing
+    # headers to the CSV file
+    count = 0
+    for model in metrics:
+        if metrics[model] is not None:
+            if count == 0:
+                # Writing headers of CSV file
+                try:
+                    header = [["model", 'run_id']] + [
+                        [x for x in list(best_metrics[some_metric].keys()) if 'param' in x and x != 'run_id']
+                    ] + [[
+                        f"{x}_mean", f"{x}_std"
+                    ] for x in list(best_metrics[some_metric].keys()) if 'param' not in x and x != 'run_id']
+                except Exception as e:
+                    print(e)
+                    continue
+                header = list(itertools.chain(*header))
+                csv_writer.writerow(header)
+                count += 1
+            for run_id in metrics[model]:
+                m = [model, run_id]
+                if metrics[model][run_id]['acc/train/all_concentrations'] != -np.inf:
+                    for metric_id in metrics[model][run_id]:
+                        if 'params' in metric_id and metric_id != 'run_id':
+                            m += [metrics[model][run_id][metric_id]]
+                    for metric_id in metrics[model][run_id]:
+                        if 'params' not in metric_id and metric_id != 'run_id':
+                            try:
+                                m += [metrics[model][run_id][metric_id]['mean']]
+                                m += [metrics[model][run_id][metric_id]['std']]
+                            except Exception as e:
+                                print(e)
+                                m += [metrics[model][run_id][metric_id]]
+                                m += [metrics[model][run_id][metric_id]]
+                        # Writing data of CSV file
+                # if len(m) > 0:
+                csv_writer.writerow(m)
+
+    data_file.close()
+
+    # now we will open a file for writing
+    data_file = open(results_dir / f'metrics_mlflow_{exp_name}_{exp_id}_values.csv', 'w')
+
+    # create the csv writer object
+    csv_writer = csv.writer(data_file)
+
+    ex_id = list(metrics[some_metric].keys())[0]
+
+    # Counter variable used for writing
+    # headers to the CSV file
+    count = 0
+    for model in metrics:
+        if metrics[model] is not None:
+            if count == 0:
+                # Writing headers of CSV file
+                try:
+                    header = [["model", 'run_id']] + [
+                        [x for x in list(best_metrics[some_metric].keys()) if 'param' in x and x != 'run_id']
+                    ] + [[x] for x in list(best_metrics[some_metric].keys()) if 'param' not in x and x != 'run_id']
+                except Exception as e:
+                    print(e)
+                    continue
+                header = list(itertools.chain(*header))
+                csv_writer.writerow(header)
+                count += 1
+            for run_id in metrics[model]:
+                m = [model, run_id]
+                if metrics[model][run_id]['acc/train/all_concentrations'] != -np.inf:
+                    for metric_id in metrics[model][run_id]:
+                        if 'params' in metric_id and metric_id != 'run_id':
+                            m += [metrics[model][run_id][metric_id]]
+                    for metric_id in metrics[model][run_id]:
+                        if 'params' not in metric_id and metric_id != 'run_id':
+                            try:
+                                m += [metrics[model][run_id][metric_id]['values']]
+                            except Exception as e:
+                                print(e)
+                                m += [metrics[model][run_id][metric_id]]
+                        # Writing data of CSV file
+                    # if len(m) > 0:
+                    csv_writer.writerow(m)
+
+    data_file.close()
+
+    print('DONE')
