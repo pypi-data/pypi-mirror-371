a74bbb8ef4516e62438af70ddc1169b5383b773a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 07:05:11 +0000

    Add package kwik/1.2.1/kwik-1.2.1-py3-none-any.whl

---
+++ packages/kwik/kwik-1.2.1-py3-none-any.whl/kwik/dependencies/sorting_query.py
@@ -0,0 +1,39 @@
+"""Sorting query dependency for FastAPI endpoints."""
+
+from __future__ import annotations
+
+import re
+from typing import Annotated
+
+from fastapi import Depends
+
+ParsedSortingQuery = list[tuple[str, str]]
+
+
+def _parse_sorting_query(sorting: str | None = None) -> ParsedSortingQuery:
+    """
+    Sorting query parameter parser, to be used as endpoint dependency.
+
+    The sorting parameter is a comma-separated list of fields to sort by.
+    Each field can be postfixed with a colon and a direction ("asc" or "desc").
+    If no direction is specified, "asc" is assumed.
+
+    Example:
+    >>> _parse_sorting_query("id:desc,created_at")
+    [("id", "desc"), ("created_at", "asc")]
+
+    """
+    sort = []
+    if sorting is not None:
+        pattern = r"(\w+)(?::(asc|desc))?"
+        for field, direction in re.findall(pattern, sorting):
+            if direction and direction not in ("asc", "desc"):
+                msg = f"Invalid sorting {direction=} for {field=}"
+                raise ValueError(msg)
+            sort.append((field, direction or "asc"))
+    return sort
+
+
+Sorting = Annotated[ParsedSortingQuery, Depends(_parse_sorting_query)]
+
+__all__ = ["Sorting"]
