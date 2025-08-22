9614a3d56d31d43a31cc6aa8dcdff931a37c885f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 07:04:39 +0000

    Add package kaqing/1.91.0/kaqing-1.91.0.tar.gz

---
+++ packages/kaqing/kaqing-1.91.0.tar.gz/kaqing-1.91.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kaqing',
+    version='1.91.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'qing = walker.cli:cli'
+        ]
+    }
+)
