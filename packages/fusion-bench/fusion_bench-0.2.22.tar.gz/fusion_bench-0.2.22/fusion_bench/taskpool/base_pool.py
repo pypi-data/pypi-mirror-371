1cf8c630cbbf726087dedf09be295c0fe98b9e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 03:20:53 +0000

    Add package fusion-bench/0.2.22/fusion_bench-0.2.22.tar.gz

---
+++ packages/fusion-bench/fusion_bench-0.2.22.tar.gz/fusion_bench-0.2.22/fusion_bench/taskpool/base_pool.py
@@ -0,0 +1,37 @@
+from abc import abstractmethod
+from typing import Any, Dict
+
+from fusion_bench.mixins import BaseYAMLSerializable
+
+
+class BaseTaskPool(BaseYAMLSerializable):
+    _program = None
+    _config_key = "taskpool"
+
+    @abstractmethod
+    def evaluate(self, model: Any, *args: Any, **kwargs: Any) -> Dict[str, Any]:
+        """
+        Evaluate the model on all tasks in the task pool, and return a report.
+
+        Take image classification as an example, the report will look like:
+
+        ```python
+        {
+            "mnist": {
+                "accuracy": 0.8,
+                "loss": 0.2,
+            },
+            <task_name>: {
+                <metric_name>: <metric_value>,
+                ...
+            },
+        }
+        ```
+
+        Args:
+            model: The model to evaluate.
+
+        Returns:
+            report (dict): A dictionary containing the results of the evaluation for each task.
+        """
+        pass
