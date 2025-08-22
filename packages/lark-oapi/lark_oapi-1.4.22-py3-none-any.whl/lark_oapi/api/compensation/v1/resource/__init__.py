5261a7a713bc6ba19e6864dc9d75736790014cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 04:03:46 +0000

    Add package lark-oapi/1.4.22/lark_oapi-1.4.22-py3-none-any.whl

---
+++ packages/lark-oapi/lark_oapi-1.4.22-py3-none-any.whl/lark_oapi/api/compensation/v1/resource/__init__.py
@@ -0,0 +1,7 @@
+from .archive import *
+from .change_reason import *
+from .indicator import *
+from .item import *
+from .item_category import *
+from .lump_sum_payment import *
+from .plan import *
