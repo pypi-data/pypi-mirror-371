433de234ad29452ac4b3567ac1ad015d1acec1f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 04:03:56 +0000

    Add package python-citybikes/0.3.1/python_citybikes-0.3.1-py3-none-any.whl

---
+++ packages/python-citybikes/python_citybikes-0.3.1-py3-none-any.whl/citybikes/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'python-citybikes'
+__version__ = '0.3.1'
+
+__all__ = ['Client', 'Network', 'Station']
+
+from citybikes.client import Client, Network, Station
