dc9ef5be24e18d9259d4e5a9b39997fd8dd87d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 04:03:57 +0000

    Add package hdfstream/0.0.14/hdfstream-0.0.14.tar.gz

---
+++ packages/hdfstream/hdfstream-0.0.14.tar.gz/hdfstream-0.0.14/docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'hdfstream'
+copyright = '2025, John Helly'
+author = 'John Helly'
+
+from setuptools_scm import get_version
+release = get_version(root="../..", relative_to=__file__)
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_autodoc_typehints',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
