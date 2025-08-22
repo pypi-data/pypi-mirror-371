49046d571f681b63cbffbdb09798194ad19cec05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 05:04:49 +0000

    Add package McSixAI/0.1.3/mcsixai-0.1.3.tar.gz

---
+++ packages/McSixAI/mcsixai-0.1.3.tar.gz/mcsixai-0.1.3/McSixAI/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENROUTER_API_KEY = "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"
+OPENROUTER_MODEL = "deepseek/deepseek-chat-v3-0324"
+SITE_URL = "http://localhost:8000"
+SITE_NAME = "CodeAssist AI"
