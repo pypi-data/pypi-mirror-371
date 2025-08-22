52c1c2dfdc529d0714cbdc050a847b0d74877585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 03:20:37 +0000

    Add package bluer-ugv/6.688.1/bluer_ugv-6.688.1-py3-none-any.whl

---
+++ packages/bluer-ugv/bluer_ugv-6.688.1-py3-none-any.whl/bluer_ugv/README/sparrow.py
@@ -0,0 +1,27 @@
+from bluer_ugv.parts.db import db_of_parts
+from bluer_ugv.sparrow.README import items
+from bluer_ugv.sparrow.parts import dict_of_parts
+
+docs = [
+    {
+        "items": items,
+        "path": "../docs/bluer_sparrow",
+        "cols": 2,
+    },
+    {"path": "../docs/bluer_sparrow/design"},
+    {"path": "../docs/bluer_sparrow/design/specs.md"},
+    {
+        "path": "../docs/bluer_sparrow/design/parts.md",
+        "items": db_of_parts.as_images(
+            dict_of_parts,
+            reference="../../parts",
+        ),
+        "macros": {
+            "parts:::": db_of_parts.as_list(
+                dict_of_parts,
+                reference="../../parts",
+                log=False,
+            ),
+        },
+    },
+]
