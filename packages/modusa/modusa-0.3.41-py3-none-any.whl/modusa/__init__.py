c17977a28ddc112c1ea324a52eeb99a11e25e007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 05:04:48 +0000

    Add package modusa/0.3.41/modusa-0.3.41-py3-none-any.whl

---
+++ packages/modusa/modusa-0.3.41-py3-none-any.whl/modusa/__init__.py
@@ -0,0 +1,9 @@
+from modusa.utils import excp, config
+
+#=====Giving access to plot functions to plot multiple signals.=====
+from modusa.tools import plot1d, plot2d, plot_dist
+#=====
+
+from modusa.tools import play, convert
+from modusa.tools import download
+from modusa.tools import load, load_ann
