2b3aa0ca1b150272a11d7103f00d1b339320fbc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 07:04:23 +0000

    Add package pmma/5.0.15/pmma-5.0.15-cp311-cp311-win_amd64.whl

---
+++ packages/pmma/pmma-5.0.15-cp311-cp311-win_amd64.whl/pmma/core/pyx_src/General.pyx
@@ -0,0 +1,366 @@
+# cython: boundscheck=False, wraparound=False, cdivision=True, nonecheck=False, initializedcheck=False
+
+from libcpp.string cimport string
+from libcpp cimport bool
+
+import os
+import platform
+import os
+import distutils
+import json
+import datetime
+import random
+import time
+import subprocess
+import locale
+
+import requests
+from pprofile import Profile
+
+from pmma.core.py_src.Constants import Constants
+from pmma.core.py_src.Utility import Registry
+
+from Logger cimport Logger
+
+# Declare the external C++ function
+cdef extern from "PMMA_Core.hpp" namespace "CPP_General" nogil:
+    void Set_PMMA_Location(string location) except + nogil
+    void Set_Path_Separator(string separator) except + nogil
+
+    string Get_PMMA_Location() except + nogil
+
+    bool Is_Power_Saving_Mode_Enabled(bool ForceRefresh) except + nogil
+    bool Is_DebugModeEnabled() except + nogil
+    void Set_DebugModeEnabled(bool DebugMode) except + nogil
+
+    bool IsWindowCreated() except + nogil
+    bool IsApplicationRunning() except + nogil
+
+    bool IsEscapeKeyToCloseWindow() except + nogil
+    void SetEscapeKeyToCloseWindow(bool EscapeKeyToCloseWindow) except + nogil
+
+    bool IsF11KeyToToggleFullscreen() except + nogil
+    void SetF11KeyToToggleFullscreen(bool F11KeyToToggleFullscreen) except + nogil
+
+    string GetCurrent_PMMA_Version() except + nogil
+    string GetLatest_PMMA_Version() except + nogil
+
+    void SetLatest_PMMA_Version(string latest_version) except + nogil
+
+    bool IsUpdateAvailable() except + nogil
+
+    double GetApplicationStartTime() except + nogil
+    double GetApplicationRunTime() except + nogil
+
+    float GetShapeQuality() except + nogil
+    void SetShapeQuality(float quality) except + nogil
+    void Let_PMMA_ControlShapeQuality() except + nogil
+
+    string GetLocale() except + nogil
+    void SetLocale(string locale__) except + nogil
+
+def internal_update_config(update_configuration_location):
+    try:
+        tag_data = requests.get(
+            "https://api.github.com/repos/PycraftDeveloper/PMMA/tags")
+        latest_version = json.loads(tag_data.text)[0]["name"]
+    except Exception as error:
+        return
+
+    current_date_code = datetime.datetime.now().strftime("%Y%m%d")
+    future_date = datetime.datetime.now() + datetime.timedelta(
+        days=random.randint(14, 30))
+    future_date_code = future_date.strftime("%Y%m%d")
+
+    data = {
+        "LastCheckDate": current_date_code,
+        "NextCheckDate": future_date_code,
+        "LatestVersion": latest_version
+    }
+    with open(update_configuration_location, "w") as update_config_file:
+        json.dump(data, update_config_file)
+
+    return latest_version
+
+cdef class General:
+    cdef:
+        Logger logger
+
+    def __cinit__(self):
+        self.logger = Logger()
+
+        self.logger.internal_log_debug(
+            24,
+            ("General.init - You don't need to instantiate "
+                "this class in order to use it."),
+            False
+        )
+
+    @staticmethod
+    def set_pmma_location(path):
+        cdef:
+            string encoded_path = path.encode('utf-8')
+
+        Set_PMMA_Location(encoded_path)
+
+    @staticmethod
+    def set_path_separator():
+        cdef:
+            string encoded_separator = str(os.sep).encode('utf-8')
+
+        Set_Path_Separator(encoded_separator)
+
+    @staticmethod
+    def get_pmma_install_directory():
+        cdef string pmma_location = Get_PMMA_Location()
+        return pmma_location.c_str().decode('utf-8')
+
+    @staticmethod
+    def is_power_saving_mode_enabled(force_refresh=False):
+        return Is_Power_Saving_Mode_Enabled(force_refresh)
+
+    @staticmethod
+    def get_operating_system():
+        if platform.system() == "Windows":
+            return Constants.WINDOWS
+        elif platform.system() == "Linux":
+            if "ANDROID_STORAGE" in os.environ:
+                return Constants.ANDROID
+            return Constants.LINUX
+        elif platform.system() == "Darwin":
+            return Constants.MACOS
+        elif platform.system() == "Java":
+            return Constants.JAVA
+
+    @staticmethod
+    def find_executable_nvidia_smi():
+        if General.get_operating_system() == Constants.WINDOWS:
+            # If the platform is Windows and nvidia-smi
+            # could not be found from the environment path,
+            # try to find it from system drive with default installation path
+            nvidia_smi = distutils.spawn.find_executable("nvidia-smi")
+            if nvidia_smi is None:
+                nvidia_smi = f"{os.environ['systemdrive']}\\Program Files\\NVIDIA Corporation\\NVSMI\\nvidia-smi.exe"
+                if not os.path.isfile(nvidia_smi):
+                    nvidia_smi = None
+        else:
+            nvidia_smi = "nvidia-smi"
+
+        return nvidia_smi
+
+    @staticmethod
+    def set_debug_mode(is_debug_mode):
+        Set_DebugModeEnabled(is_debug_mode)
+
+    @staticmethod
+    def is_debug_mode_enabled():
+        return Is_DebugModeEnabled()
+
+    @staticmethod
+    def is_window_created():
+        return IsWindowCreated()
+
+    @staticmethod
+    def is_escape_key_to_close_window():
+        return IsEscapeKeyToCloseWindow();
+
+    @staticmethod
+    def set_escape_key_to_close_window(escape_key_to_close_window):
+        return SetEscapeKeyToCloseWindow(escape_key_to_close_window)
+
+    @staticmethod
+    def is_f11_key_to_toggle_full_screen():
+        return IsF11KeyToToggleFullscreen()
+
+    @staticmethod
+    def set_f11_key_to_toggle_full_screen(f11_key_to_toggle_full_screen):
+        SetF11KeyToToggleFullscreen(f11_key_to_toggle_full_screen)
+
+    @staticmethod
+    def is_application_running():
+        return IsApplicationRunning()
+
+    @staticmethod
+    def get_current_pmma_version():
+        cdef string cpp_str = GetCurrent_PMMA_Version()
+        return cpp_str.c_str().decode('utf-8')
+
+    @staticmethod
+    def get_latest_pmma_version():
+        if Registry.checking_for_updates and Registry.update_checking_thread is not None:
+            Registry.update_checking_thread.join()
+
+        cdef encoded_latest_version
+        cdef string cpp_str = GetLatest_PMMA_Version()
+
+        return cpp_str.c_str().decode('utf-8')
+
+    @staticmethod
+    def is_update_available():
+        if Registry.checking_for_updates and Registry.update_checking_thread is not None:
+            Registry.update_checking_thread.join()
+        return IsUpdateAvailable()
+
+    @staticmethod
+    def internal_check_for_updates():
+        cdef encoded_latest_version
+        cdef string pmma_location = Get_PMMA_Location()
+        cdef string current_pmma_version_cpp_str = GetCurrent_PMMA_Version()
+
+        current_pmma_version = current_pmma_version_cpp_str.c_str().decode('utf-8')
+
+        pmma_install_location = pmma_location.c_str().decode('utf-8')
+
+        configuration_location = pmma_install_location + os.sep + "config"
+
+        os.makedirs(configuration_location, exist_ok=True)
+
+        update_configuration_location = configuration_location + os.sep + "update_config.json"
+
+        display_update_prompt = False
+
+        if os.path.exists(update_configuration_location):
+            with open(update_configuration_location, "r") as update_config_file:
+                data = json.load(update_config_file)
+
+            current_date_code = datetime.datetime.now().strftime("%Y%m%d")
+
+            if current_date_code >= data["NextCheckDate"]:
+                latest_version = internal_update_config(update_configuration_location)
+                if latest_version is None:
+                    latest_version = data["LatestVersion"]
+                display_update_prompt = True
+
+            else:
+                latest_version = data["LatestVersion"]
+
+        else:
+            latest_version = internal_update_config(update_configuration_location)
+            if latest_version is None:
+                latest_version = "0.0.0"
+            display_update_prompt = True
+
+        encoded_latest_version = latest_version.encode("utf-8")
+        SetLatest_PMMA_Version(encoded_latest_version)
+
+        if display_update_prompt and IsUpdateAvailable():
+            logger = Logger()
+
+            logger.internal_log_debug(
+                17,
+                ("Did you know there is a new version of PMMA? You are "
+f"currently on version: {current_pmma_version} and the latest version is: "
+f"{latest_version}. You can check out the latest features here: "
+"'https://github.com/PycraftDeveloper/PMMA/releases' and use `pip install "
+"--upgrade pmma` to perform the update!"),
+                False
+            )
+
+        Registry.checking_for_updates = False
+
+    @staticmethod
+    def get_application_start_time():
+        return GetApplicationStartTime()
+
+    @staticmethod
+    def get_application_run_time():
+        return GetApplicationRunTime()
+
+    @staticmethod
+    def get_shape_quality():
+        return GetShapeQuality()
+
+    @staticmethod
+    def set_shape_quality(value=None):
+        if value is None:
+            Let_PMMA_ControlShapeQuality()
+        else:
+            SetShapeQuality(value)
+
+    @staticmethod
+    def get_execution_time(function, *args, **kwargs):
+        start_time = time.perf_counter()
+        result = function(*args, **kwargs)
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+        return execution_time, result
+
+    @staticmethod
+    def get_execution_inverse_time(function, *args, **kwargs):
+        start_time = time.perf_counter()
+        result = function(*args, **kwargs)
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+        return 1/execution_time, result
+
+    @staticmethod
+    def set_locale(newlocale=None):
+        cdef encoded_locale
+
+        if newlocale is None:
+            if General.get_operating_system() == Constants.WINDOWS:
+                try:
+                    from ctypes import windll
+                    windll = windll.kernel32
+                    detected_language = locale.windows_locale[
+                        windll.GetUserDefaultUILanguage()]
+                except:
+                    try:
+                        result = subprocess.run(
+                            ['locale'],
+                            capture_output=True,
+                            text=True,
+                            check=True)
+
+                        for line in result.stdout.split('\n'):
+                            if line.startswith('LANG='):
+                                detected_language = line.split('=')[1]
+                    except subprocess.CalledProcessError:
+                        detected_language = None
+            else:
+                try:
+                    result = subprocess.run(
+                        ['locale'],
+                        capture_output=True,
+                        text=True,
+                        check=True)
+
+                    for line in result.stdout.split('\n'):
+                        if line.startswith('LANG='):
+                            detected_language = line.split('=')[1]
+                except subprocess.CalledProcessError:
+                    detected_language = None
+
+            if detected_language is None:
+                detected_language = "en_US"
+
+            encoded_locale = detected_language.encode("utf-8")
+        else:
+            encoded_locale = newlocale.encode("utf-8")
+
+        SetLocale(encoded_locale)
+
+    @staticmethod
+    def get_locale():
+        cdef string cpp_str = GetLocale()
+        return cpp_str.c_str().decode("utf-8")
+
+    @staticmethod
+    def profile_start():
+        if Registry.profiler_instance is None:
+            Registry.profiler_instance = Profile()
+        Registry.profiler_instance.enable()
+
+    @staticmethod
+    def profile_end():
+        if Registry.profiler_instance is None:
+            logger = Logger()
+            logger.internal_log_warn(
+                45,
+                "This function call has been ignored as you have not yet \
+started profiling anything. Please call `General.profile_start` to begin \
+profiling a section of your application.",
+                True
+            )
+        else:
+            Registry.profiler_instance.disable()
---
+++ packages/pmma/pmma-5.0.15-cp311-cp311-win_amd64.whl/pmma/core/pyx_src/Logger.pyx
@@ -0,0 +1,113 @@
+# cython: boundscheck=False, wraparound=False, cdivision=True, nonecheck=False, initializedcheck=False
+
+from libcpp.string cimport string
+
+cdef class Logger:
+    def __cinit__(self):
+        self.cpp_class_ptr = new CPP_Logger()
+
+    def __dealloc__(self):
+        del self.cpp_class_ptr
+        self.cpp_class_ptr = NULL
+
+    def set_log_to_file(self, log_to_file):
+        self.cpp_class_ptr.SetLogToFile(log_to_file)
+
+    def is_log_to_file(self):
+        return self.cpp_class_ptr.GetLogToFile()
+
+    def set_log_to_console(self, log_to_console):
+        self.cpp_class_ptr.SetLogToConsole(log_to_console)
+
+    def is_log_to_console(self):
+        return self.cpp_class_ptr.GetLogToConsole()
+
+    def set_max_log_files_to_keep(self, value):
+        self.cpp_class_ptr.SetKeepCount(value)
+
+    def get_max_log_files_to_keep(self):
+        return self.cpp_class_ptr.GetKeepCount()
+
+    def set_log_debug(self, log_debug):
+        self.cpp_class_ptr.SetLogDebug(log_debug)
+
+    def set_log_warn(self, log_warn):
+        self.cpp_class_ptr.SetLogWarn(log_warn)
+
+    def set_log_error(self, log_error):
+        self.cpp_class_ptr.SetLogError(log_error)
+
+    def get_log_debug(self):
+        return self.cpp_class_ptr.GetLogDebug()
+
+    def get_log_warn(self):
+        return self.cpp_class_ptr.GetLogWarn()
+
+    def get_log_error(self):
+        return self.cpp_class_ptr.GetLogError()
+
+    def log_debug_with_id(self, log_id, content, product_name="", repeat_for_effect=False):
+        cdef string encoded_id = log_id.encode("utf-8")
+        cdef string encoded_content = content.encode("utf-8")
+        cdef string encoded_product_name = product_name.encode("utf-8")
+
+        self.cpp_class_ptr.LogDebug(encoded_id, encoded_content, encoded_product_name, repeat_for_effect)
+
+    def log_debug(self, content, values=[], product_name="", repeat_for_effect=False):
+        cdef string encoded_id = content.encode("utf-8")
+        cdef string encoded_content
+        cdef string encoded_product_name = product_name.encode("utf-8")
+
+        content = content.format(*values)
+        encoded_content = content.encode("utf-8")
+
+        self.cpp_class_ptr.LogDebug(encoded_id, encoded_content, encoded_product_name, repeat_for_effect)
+
+    def log_warn_with_id(self, log_id, content, product_name="", repeat_for_effect=True):
+        cdef string encoded_id = log_id.encode("utf-8")
+        cdef string encoded_content = content.encode("utf-8")
+        cdef string encoded_product_name = product_name.encode("utf-8")
+
+        self.cpp_class_ptr.LogWarn(encoded_id, encoded_content, encoded_product_name, repeat_for_effect)
+
+    def log_warn(self, content, values=[], product_name="", repeat_for_effect=True):
+        cdef string encoded_id = content.encode("utf-8")
+        cdef string encoded_content
+        cdef string encoded_product_name = product_name.encode("utf-8")
+
+        content = content.format(*values)
+        encoded_content = content.encode("utf-8")
+
+        self.cpp_class_ptr.LogWarn(encoded_id, encoded_content, encoded_product_name, repeat_for_effect)
+
+    def log_error_with_id(self, log_id, content, product_name="", repeat_for_effect=True):
+        cdef string encoded_id = log_id.encode("utf-8")
+        cdef string encoded_content = content.encode("utf-8")
+        cdef string encoded_product_name = product_name.encode("utf-8")
+
+        self.cpp_class_ptr.LogError(encoded_id, encoded_content, encoded_product_name, repeat_for_effect)
+
+    def log_error(self, content, values=[], product_name="", repeat_for_effect=True):
+        cdef string encoded_id = content.encode("utf-8")
+        cdef string encoded_content
+        cdef string encoded_product_name = product_name.encode("utf-8")
+
+        content = content.format(*values)
+        encoded_content = content.encode("utf-8")
+
+        self.cpp_class_ptr.LogError(encoded_id, encoded_content, encoded_product_name, repeat_for_effect)
+
+    cpdef internal_log_debug(self, int log_id, content, repeat_for_effect):
+        cdef string encoded_content = content.encode("utf-8")
+
+        self.cpp_class_ptr.InternalLogDebug(log_id, encoded_content, repeat_for_effect)
+
+    cpdef internal_log_warn(self, int log_id, content, repeat_for_effect):
+        cdef string encoded_content = content.encode("utf-8")
+
+        self.cpp_class_ptr.InternalLogWarn(log_id, encoded_content, repeat_for_effect)
+
+    cpdef internal_log_error(self, int log_id, content, repeat_for_effect):
+        cdef string encoded_content = content.encode("utf-8")
+
+        self.cpp_class_ptr.InternalLogError(log_id, encoded_content, repeat_for_effect)
