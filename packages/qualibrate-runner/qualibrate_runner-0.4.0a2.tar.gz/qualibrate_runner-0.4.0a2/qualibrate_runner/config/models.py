250311c309d81a2f7f910f3453e7bc303527d395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 03:20:32 +0000

    Add package qualibrate-runner/0.4.0a2/qualibrate_runner-0.4.0a2.tar.gz

---
+++ packages/qualibrate-runner/qualibrate_runner-0.4.0a2.tar.gz/qualibrate_runner-0.4.0a2/qualibrate_runner/config/models.py
@@ -0,0 +1,21 @@
+from typing import Optional, Union
+
+from pydantic import BaseModel, ConfigDict
+
+from qualibrate_runner.core.models.enums import RunStatusEnum
+from qualibrate_runner.core.models.last_run import LastRun
+from qualibrate_runner.core.types import QGraphType, QNodeType
+
+
+class State(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    last_run: Optional[LastRun] = None
+    run_item: Optional[Union[QNodeType, QGraphType]] = None
+
+    @property
+    def is_running(self) -> bool:
+        return (
+            self.last_run is not None
+            and self.last_run.status == RunStatusEnum.RUNNING
+        )
