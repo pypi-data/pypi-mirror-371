90af78e92b0b0219d57f7b6b391d96fb6b8af1f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 04:04:22 +0000

    Add package mex-common/1.0.0/mex_common-1.0.0.tar.gz

---
+++ packages/mex-common/mex_common-1.0.0.tar.gz/mex_common-1.0.0/mex/common/merged/main.py
@@ -0,0 +1,178 @@
+from collections.abc import Mapping, Sequence
+from typing import Any, Literal, overload
+
+from pydantic_core import ValidationError
+
+from mex.common.exceptions import MergingError
+from mex.common.fields import MERGEABLE_FIELDS_BY_CLASS_NAME
+from mex.common.logging import logger
+from mex.common.merged.utils import extend_list_in_dict, prune_list_in_dict
+from mex.common.models import (
+    MERGED_MODEL_CLASSES_BY_NAME,
+    PREVIEW_MODEL_CLASSES_BY_NAME,
+    AnyAdditiveModel,
+    AnyExtractedModel,
+    AnyMergedModel,
+    AnyPreventiveModel,
+    AnyPreviewModel,
+    AnyRuleSetRequest,
+    AnyRuleSetResponse,
+    AnySubtractiveModel,
+)
+from mex.common.transform import ensure_prefix
+from mex.common.types import Identifier, Validation
+
+
+def _merge_extracted_items_and_apply_preventive_rule(
+    merged_dict: dict[str, Any],
+    mergeable_fields: Sequence[str],
+    extracted_items: Sequence[AnyExtractedModel],
+    rule: AnyPreventiveModel | None,
+) -> None:
+    """Merge a list of extracted items while applying a preventive rule.
+
+    Collect unique values from the extracted items and write them into `merged_dict`,
+    unless the primary source of the extracted item was blocked by the rule.
+
+    Args:
+        merged_dict: Mapping from field names to lists of merged values
+        mergeable_fields: List of mergeable field names
+        extracted_items: List of extracted items
+        rule: Preventive rules with primary source identifiers, can be None
+    """
+    for extracted_item in sorted(extracted_items, key=lambda e: e.identifier):
+        for field_name in mergeable_fields:
+            if rule is not None and extracted_item.hadPrimarySource in getattr(
+                rule, field_name
+            ):
+                continue
+            extracted_value = getattr(extracted_item, field_name)
+            extend_list_in_dict(merged_dict, field_name, extracted_value)
+
+
+def _apply_additive_rule(
+    merged_dict: dict[str, Any],
+    mergeable_fields: Sequence[str],
+    rule: AnyAdditiveModel,
+) -> None:
+    """Merge the values from an additive rule into a `merged_dict`.
+
+    Args:
+        merged_dict: Mapping from field names to lists of merged values
+        mergeable_fields: List of mergeable field names
+        rule: Additive rule with values to be added
+    """
+    for field_name in mergeable_fields:
+        rule_value = getattr(rule, field_name)
+        extend_list_in_dict(merged_dict, field_name, rule_value)
+
+
+def _apply_subtractive_rule(
+    merged_dict: dict[str, Any],
+    mergeable_fields: Sequence[str],
+    rule: AnySubtractiveModel,
+) -> None:
+    """Prune values of a subtractive rule from a `merged_dict`.
+
+    Args:
+        merged_dict: Mapping from field names to lists of merged values
+        mergeable_fields: List of mergeable field names
+        rule: Subtractive rule with values to remove
+    """
+    for field_name in mergeable_fields:
+        rule_value = getattr(rule, field_name)
+        prune_list_in_dict(merged_dict, field_name, rule_value)
+
+
+@overload
+def create_merged_item(
+    identifier: Identifier,
+    extracted_items: list[AnyExtractedModel],
+    rule_set: AnyRuleSetRequest | AnyRuleSetResponse | None,
+    validation: Literal[Validation.LENIENT],
+) -> AnyPreviewModel: ...
+
+
+@overload
+def create_merged_item(
+    identifier: Identifier,
+    extracted_items: list[AnyExtractedModel],
+    rule_set: AnyRuleSetRequest | AnyRuleSetResponse | None,
+    validation: Literal[Validation.STRICT],
+) -> AnyMergedModel: ...
+
+
+@overload
+def create_merged_item(
+    identifier: Identifier,
+    extracted_items: list[AnyExtractedModel],
+    rule_set: AnyRuleSetRequest | AnyRuleSetResponse | None,
+    validation: Literal[Validation.IGNORE],
+) -> AnyMergedModel: ...
+
+
+def create_merged_item(
+    identifier: Identifier,
+    extracted_items: list[AnyExtractedModel],
+    rule_set: AnyRuleSetRequest | AnyRuleSetResponse | None,
+    validation: Literal[Validation.STRICT, Validation.LENIENT, Validation.IGNORE],
+) -> AnyPreviewModel | AnyMergedModel | None:
+    """Merge a list of extracted items with a set of rules.
+
+    Args:
+        identifier: Identifier the finished merged item should have
+        extracted_items: List of extracted items, can be empty
+        rule_set: Rule set, with potentially empty rules
+        validation: Controls how strictly the merged item needs to validate:
+            - STRICT: Validates all required fields and list lengths. Returns a
+                fully validated merged item or raises MergingError on validation failure
+            - LENIENT: Skips validation checks and returns a "preview" merged item
+                that may be missing required fields and even may be using blocked values
+            - IGNORE: In case of validation errors, this mode will safely return None
+
+    Raises:
+        MergingError: When the given items cannot be merged (in STRICT mode)
+        MergingError: When neither extracted nor rule items are given (in STRICT mode)
+
+    Returns:
+        Instance of a merged or preview item
+    """
+    model_class_lookup: Mapping[str, type[AnyPreviewModel | AnyMergedModel]]
+    if validation == Validation.LENIENT:
+        model_prefix = "Preview"
+        model_class_lookup = PREVIEW_MODEL_CLASSES_BY_NAME
+    else:
+        model_prefix = "Merged"
+        model_class_lookup = MERGED_MODEL_CLASSES_BY_NAME
+
+    if rule_set:
+        entity_type = ensure_prefix(rule_set.stemType, model_prefix)
+    elif extracted_items:
+        entity_type = ensure_prefix(extracted_items[0].stemType, model_prefix)
+    elif validation == Validation.STRICT:
+        msg = "One of rule_set or extracted_items is required."
+        raise MergingError(msg)
+    else:
+        logger.debug("One of rule_set or extracted_items is required.")
+        return None
+
+    fields = MERGEABLE_FIELDS_BY_CLASS_NAME[entity_type]
+    cls = model_class_lookup[entity_type]
+
+    merged_dict: dict[str, Any] = {"identifier": identifier}
+
+    _merge_extracted_items_and_apply_preventive_rule(
+        merged_dict, fields, extracted_items, rule_set.preventive if rule_set else None
+    )
+    if rule_set:
+        _apply_additive_rule(merged_dict, fields, rule_set.additive)
+        _apply_subtractive_rule(merged_dict, fields, rule_set.subtractive)
+
+    try:
+        return cls.model_validate(merged_dict)
+    except ValidationError as error:
+        msg = "Could not validate merged model."
+        if validation == Validation.STRICT:
+            raise MergingError(msg) from error
+        logger.debug("%s %s:%s", msg, entity_type, identifier)
+    return None
---
+++ packages/mex-common/mex_common-1.0.0.tar.gz/mex_common-1.0.0/tests/merged/test_main.py
@@ -0,0 +1,416 @@
+from typing import Any, Literal
+
+import pytest
+
+from mex.common.exceptions import MExError
+from mex.common.merged.main import (
+    _apply_additive_rule,
+    _apply_subtractive_rule,
+    _merge_extracted_items_and_apply_preventive_rule,
+    create_merged_item,
+)
+from mex.common.models import (
+    ActivityRuleSetRequest,
+    AdditiveOrganizationalUnit,
+    AdditivePerson,
+    AdditiveResource,
+    AnyExtractedModel,
+    AnyRuleSetRequest,
+    ContactPointRuleSetRequest,
+    ExtractedActivity,
+    ExtractedContactPoint,
+    ExtractedPerson,
+    ExtractedResource,
+    PersonRuleSetRequest,
+    PreventiveContactPoint,
+    PreventivePerson,
+    PreventiveResource,
+    ResourceRuleSetRequest,
+    SubtractiveActivity,
+    SubtractiveContactPoint,
+    SubtractiveOrganizationalUnit,
+    SubtractivePerson,
+    SubtractiveResource,
+)
+from mex.common.testing import Joker
+from mex.common.types import (
+    AccessRestriction,
+    Identifier,
+    Text,
+    TextLanguage,
+    Theme,
+)
+from mex.common.types.validation import Validation
+
+
+def test_merge_extracted_items_stable_order() -> None:
+    # create a batch of 20 contact points
+    contact_points = [
+        ExtractedContactPoint(
+            email=[f"{i}@contact-point.com"],
+            hadPrimarySource=Identifier.generate(seed=1),
+            identifierInPrimarySource=f"{i}",
+        )
+        for i in range(20)
+    ]
+
+    # merge the extracted items into a merged item
+    merged_dict: dict[str, Any] = {}
+    _merge_extracted_items_and_apply_preventive_rule(
+        merged_dict,
+        ["email"],
+        contact_points,
+        None,
+    )
+
+    # check that the list of emails is stable in its order
+    assert merged_dict["email"] == [
+        c.email[0] for c in sorted(contact_points, key=lambda e: e.identifier)
+    ]
+
+
+def test_merge_extracted_items_and_apply_preventive_rule() -> None:
+    merged_dict: dict[str, Any] = {}
+    contact_points: list[AnyExtractedModel] = [
+        ExtractedContactPoint(
+            email=["info@contact-point.one"],
+            hadPrimarySource=Identifier.generate(seed=1),
+            identifierInPrimarySource="one",
+        ),
+        ExtractedContactPoint(
+            email=["hello@contact-point.two"],
+            hadPrimarySource=Identifier.generate(seed=2),
+            identifierInPrimarySource="two",
+        ),
+    ]
+    rule = PreventiveContactPoint(
+        email=[contact_points[1].hadPrimarySource],
+    )
+    _merge_extracted_items_and_apply_preventive_rule(
+        merged_dict,
+        ["email"],
+        contact_points,
+        rule,
+    )
+    assert merged_dict == {
+        "email": ["info@contact-point.one"],
+    }
+
+    merged_dict.clear()
+    _merge_extracted_items_and_apply_preventive_rule(
+        merged_dict,
+        ["email"],
+        contact_points,
+        None,
+    )
+    assert merged_dict == {
+        "email": ["info@contact-point.one", "hello@contact-point.two"],
+    }
+
+    merged_dict.clear()
+    _merge_extracted_items_and_apply_preventive_rule(
+        merged_dict,
+        ["email"],
+        [],
+        rule,
+    )
+    assert merged_dict == {}
+
+
+def test_apply_additive_rule() -> None:
+    merged_dict: dict[str, Any] = {
+        "email": ["info@org-unit.one"],
+    }
+    rule = AdditiveOrganizationalUnit(
+        email=["new-mail@who.dis", "info@org-unit.one"],
+        name=[Text(value="org unit one", language=TextLanguage.EN)],
+    )
+    _apply_additive_rule(
+        merged_dict,
+        ["email", "name"],
+        rule,
+    )
+    assert merged_dict == {
+        "email": ["info@org-unit.one", "new-mail@who.dis"],
+        "name": [Text(value="org unit one", language=TextLanguage.EN)],
+    }
+
+
+def test_apply_subtractive_rule() -> None:
+    merged_dict: dict[str, Any] = {
+        "email": ["info@org-unit.one"],
+        "name": [Text(value="org unit one", language=TextLanguage.EN)],
+    }
+    rule = SubtractiveOrganizationalUnit(
+        email=["unknown@email.why", "info@org-unit.one"],
+        name=[Text(value="org unit one", language=TextLanguage.DE)],
+    )
+    _apply_subtractive_rule(
+        merged_dict,
+        ["email", "name"],
+        rule,
+    )
+    assert merged_dict == {
+        "email": [],
+        "name": [Text(value="org unit one", language=TextLanguage.EN)],
+    }
+
+
+@pytest.mark.parametrize(
+    ("extracted_items", "rule_set", "validation", "expected"),
+    [
+        (
+            [
+                ExtractedResource(
+                    identifierInPrimarySource="r1",
+                    hadPrimarySource=Identifier.generate(seed=42),
+                    accessRestriction=AccessRestriction["OPEN"],
+                    contact=[Identifier.generate(seed=999)],
+                    unitInCharge=[Identifier.generate(seed=999)],
+                    theme=[Theme["PUBLIC_HEALTH"]],
+                    title=[Text(value="Dummy resource")],
+                )
+            ],
+            ResourceRuleSetRequest(
+                additive=AdditiveResource(),
+                subtractive=SubtractiveResource(),
+                preventive=PreventiveResource(),
+            ),
+            Validation.STRICT,
+            {
+                "accessRestriction": "https://mex.rki.de/item/access-restriction-1",
+                "contact": ["bFQoRhcVH5DIax"],
+                "theme": ["https://mex.rki.de/item/theme-1"],
+                "title": [{"value": "Dummy resource", "language": TextLanguage.EN}],
+                "unitInCharge": ["bFQoRhcVH5DIax"],
+                "entityType": "MergedResource",
+                "identifier": "bFQoRhcVH5DHU6",
+            },
+        ),
+        (
+            [
+                ExtractedPerson(
+                    fullName="Dr. Zoidberg",
+                    affiliation=Identifier.generate(seed=99),
+                    email="z@express.planet",
+                    identifierInPrimarySource="drz",
+                    hadPrimarySource=Identifier.generate(seed=9),
+                ),
+                ExtractedPerson(
+                    fullName="Mr. Krabs",
+                    email="manager@krusty.ocean",
+                    affiliation=Identifier.generate(seed=101),
+                    memberOf=[
+                        Identifier.generate(seed=500),
+                        Identifier.generate(seed=750),
+                    ],
+                    hadPrimarySource=Identifier.generate(seed=11),
+                    identifierInPrimarySource="mrk",
+                ),
+            ],
+            PersonRuleSetRequest(
+                additive=AdditivePerson(
+                    givenName=["Eugene", "Harold", "John"],
+                    memberOf=[Identifier.generate(seed=500)],
+                ),
+                subtractive=SubtractivePerson(
+                    email=["manager@krusty.ocean"],
+                    givenName=["John"],
+                ),
+                preventive=PreventivePerson(
+                    email=[Identifier.generate(seed=9)],
+                    fullName=[Identifier.generate(seed=9)],
+                ),
+            ),
+            Validation.STRICT,
+            {
+                "affiliation": [
+                    Identifier.generate(seed=99),
+                    Identifier.generate(seed=101),
+                ],
+                "fullName": ["Mr. Krabs"],
+                "givenName": ["Eugene", "Harold"],
+                "memberOf": [
+                    Identifier.generate(seed=500),
+                    Identifier.generate(seed=750),
+                ],
+                "identifier": Identifier.generate(seed=42),
+                "entityType": "MergedPerson",
+            },
+        ),
+        (
+            [],
+            PersonRuleSetRequest(
+                additive=AdditivePerson(
+                    givenName=["Eugene", "Harold", "John"],
+                    memberOf=[Identifier.generate(seed=500)],
+                ),
+                subtractive=SubtractivePerson(
+                    email=["manager@krusty.ocean"],
+                    givenName=["John"],
+                ),
+                preventive=PreventivePerson(
+                    email=[Identifier.generate(seed=9)],
+                    fullName=[Identifier.generate(seed=9)],
+                ),
+            ),
+            Validation.STRICT,
+            {
+                "givenName": ["Eugene", "Harold"],
+                "memberOf": [
+                    Identifier.generate(seed=500),
+                ],
+                "identifier": Identifier.generate(seed=42),
+                "entityType": "MergedPerson",
+            },
+        ),
+        (
+            [
+                ExtractedPerson(
+                    fullName="Dr. Zoidberg",
+                    affiliation=Identifier.generate(seed=99),
+                    email="z@express.planet",
+                    identifierInPrimarySource="drz",
+                    hadPrimarySource=Identifier.generate(seed=9),
+                ),
+                ExtractedPerson(
+                    fullName="Mr. Krabs",
+                    email="manager@krusty.ocean",
+                    affiliation=Identifier.generate(seed=101),
+                    memberOf=[
+                        Identifier.generate(seed=500),
+                        Identifier.generate(seed=750),
+                    ],
+                    hadPrimarySource=Identifier.generate(seed=11),
+                    identifierInPrimarySource="mrk",
+                ),
+            ],
+            None,
+            Validation.STRICT,
+            {
+                "affiliation": [
+                    Identifier.generate(seed=99),
+                    Identifier.generate(seed=101),
+                ],
+                "email": ["z@express.planet", "manager@krusty.ocean"],
+                "fullName": ["Dr. Zoidberg", "Mr. Krabs"],
+                "memberOf": [
+                    Identifier.generate(seed=500),
+                    Identifier.generate(seed=750),
+                ],
+                "identifier": Identifier.generate(seed=42),
+                "entityType": "MergedPerson",
+            },
+        ),
+        (
+            [],
+            None,
+            Validation.STRICT,
+            "One of rule_set or extracted_items is required.",
+        ),
+        (
+            [
+                ExtractedContactPoint(
+                    identifierInPrimarySource="krusty",
+                    hadPrimarySource=Identifier.generate(seed=99),
+                    email=["manager@krusty.ocean"],
+                )
+            ],
+            ContactPointRuleSetRequest(
+                subtractive=SubtractiveContactPoint(
+                    email=["flipper@krusty.ocean", "manager@krusty.ocean"]
+                )
+            ),
+            Validation.STRICT,
+            "List should have at least 1 item after validation, not 0",
+        ),
+        (
+            [
+                ExtractedActivity(
+                    title=Text(value="Burger flipping"),
+                    contact=Identifier.generate(seed=97),
+                    responsibleUnit=Identifier.generate(seed=98),
+                    identifierInPrimarySource="BF",
+                    hadPrimarySource=Identifier.generate(seed=99),
+                ),
+            ],
+            ActivityRuleSetRequest(
+                subtractive=SubtractiveActivity(title=Text(value="Burger flipping")),
+            ),
+            Validation.LENIENT,
+            {
+                "contact": [Identifier.generate(seed=97)],
+                "identifier": Identifier.generate(seed=42),
+                "responsibleUnit": [Identifier.generate(seed=98)],
+                "entityType": "PreviewActivity",
+            },
+        ),
+        (
+            [
+                ExtractedContactPoint(
+                    identifierInPrimarySource="krusty",
+                    hadPrimarySource=Identifier.generate(seed=99),
+                    email=["manager@krusty.ocean"],
+                )
+            ],
+            ContactPointRuleSetRequest(
+                subtractive=SubtractiveContactPoint(
+                    email=["flipper@krusty.ocean", "manager@krusty.ocean"]
+                )
+            ),
+            Validation.LENIENT,
+            {
+                "identifier": Joker(),
+                "entityType": "PreviewContactPoint",
+            },
+        ),
+        (
+            [
+                ExtractedContactPoint(
+                    identifierInPrimarySource="orders",
+                    hadPrimarySource=Identifier.generate(seed=98),
+                    email=["orders@krusty.ocean"],
+                )
+            ],
+            ContactPointRuleSetRequest(
+                subtractive=SubtractiveContactPoint(
+                    email=["orders@krusty.ocean"],
+                )
+            ),
+            Validation.IGNORE,
+            None,
+        ),
+    ],
+    ids=(
+        "single extracted item",
+        "extracted items and rule set",
+        "only rule set",
+        "only extracted items",
+        "error if neither is supplied",
+        "merging raises cardinality error",
+        "get preview of merged items",
+        "preview allows cardinality error",
+        "ignore mode validation returns none",
+    ),
+)
+def test_create_merged_item(
+    extracted_items: list[AnyExtractedModel],
+    rule_set: AnyRuleSetRequest | None,
+    validation: Literal[Validation.STRICT, Validation.LENIENT, Validation.IGNORE],
+    expected: dict[str, Any] | str | None,
+) -> None:
+    try:
+        merged_item = create_merged_item(
+            Identifier.generate(seed=42),
+            extracted_items,
+            rule_set,
+            validation,
+        )
+    except MExError as error:
+        if str(expected) not in f"{error}: {error.__cause__}":
+            raise AssertionError(expected) from error
+    else:
+        if merged_item is None:
+            assert expected is None
+        else:
+            assert {k: v for k, v in merged_item.model_dump().items() if v} == expected
