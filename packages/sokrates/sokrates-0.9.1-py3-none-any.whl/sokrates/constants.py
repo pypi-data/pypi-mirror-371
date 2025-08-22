0d2bebec57c453ca6e5345aca8fdfda0f63775b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 07:04:15 +0000

    Add package sokrates/0.9.1/sokrates-0.9.1-py3-none-any.whl

---
+++ packages/sokrates/sokrates-0.9.1-py3-none-any.whl/sokrates/constants.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+class Constants:  
+  # Endpoint
+  DEFAULT_API_ENDPOINT = "http://localhost:1234/v1"
+  DEFAULT_API_KEY = "notrequired"
+  
+  # Model settings
+  DEFAULT_MODEL = "qwen3-4b-instruct-2507-mlx"
+  DEFAULT_MODEL_TEMPERATURE = 0.7
+  
+  # paths
+  DEFAULT_PROMPTS_DIRECTORY = str((Path(__file__).parent / "prompts").resolve())
+  
+  # Task daemon
+  DEFAULT_DAEMON_PROCESSING_INTERVAL = 15
+  DEFAULT_TASK_DAEMON_DEAD_LETTER_QUEUE_ENABLED = True
+  DEFAULT_TASK_DAEMON_BASE_RETRY_DELAY = 2
+  DEFAULT_TASK_DAEMON_MAX_RETRIES = 2
