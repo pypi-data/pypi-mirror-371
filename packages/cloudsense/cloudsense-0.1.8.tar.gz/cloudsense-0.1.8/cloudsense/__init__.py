9ef8044a141c4c9a146537347cc7425b3ee9505e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 08:04:13 +0000

    Add package cloudsense/0.1.8/cloudsense-0.1.8.tar.gz

---
+++ packages/cloudsense/cloudsense-0.1.8.tar.gz/cloudsense-0.1.8/cloudsense/__init__.py
@@ -0,0 +1,11 @@
+"""CloudSense - AWS Cost Dashboard
+
+A CLI and interactive GUI for AWS cost tracking.
+"""
+
+__version__ = "0.1.8"
+__author__ = "veloduff"
+
+from .app import create_app
+
+__all__ = ["create_app"]
