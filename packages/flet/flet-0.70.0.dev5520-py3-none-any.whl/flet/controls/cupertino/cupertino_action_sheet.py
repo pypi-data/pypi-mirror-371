f6a319b6d3216043dbbbff16ab83fe1e863ae5c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 06:04:14 +0000

    Add package flet/0.70.0.dev5520/flet-0.70.0.dev5520-py3-none-any.whl

---
+++ packages/flet/flet-0.70.0.dev5520-py3-none-any.whl/flet/controls/cupertino/cupertino_action_sheet.py
@@ -0,0 +1,47 @@
+from typing import Optional
+
+from flet.controls.base_control import control
+from flet.controls.constrained_control import ConstrainedControl
+from flet.controls.control import Control
+from flet.controls.types import StrOrControl
+
+__all__ = ["CupertinoActionSheet"]
+
+
+@control("CupertinoActionSheet")
+class CupertinoActionSheet(ConstrainedControl):
+    """
+    An iOS-style action sheet.
+    """
+
+    title: Optional[StrOrControl] = None
+    """
+    A control containing the title of the action sheet.
+
+    Typically a [`Text`][flet.Text] control.
+    """
+
+    message: Optional[StrOrControl] = None
+    """
+    A control containing a descriptive message that provides more details about the
+    reason for the alert.
+
+    Typically a [`Text`][flet.Text] control.
+    """
+
+    actions: Optional[list[Control]] = None
+    """
+    A list of action buttons to be shown in the sheet.
+
+    These actions are typically [`CupertinoActionSheetAction`][flet.CupertinoActionSheetAction]s.
+
+    This list must have at least one action.
+    """
+
+    cancel: Optional[Control] = None
+    """
+    An optional control to be shown below the actions but grouped separately from them.
+
+    Typically a [`CupertinoActionSheetAction`][flet.CupertinoActionSheetAction]
+    button.
+    """
