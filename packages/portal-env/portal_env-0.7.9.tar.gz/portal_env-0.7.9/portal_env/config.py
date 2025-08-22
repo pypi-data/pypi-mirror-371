bfa44ad6e90bc4812b92f5cca9cc85e14161fc25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 03:20:19 +0000

    Add package portal-env/0.7.9/portal_env-0.7.9.tar.gz

---
+++ packages/portal-env/portal_env-0.7.9.tar.gz/portal_env-0.7.9/portal_env/config.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass, field
+
+
+@dataclass()
+class Config:
+    host_name: str = "env_portal"
+    port: int = 7000
+    env_ports: dict[str, int] = field(default_factory=dict)
+    docker_network_name: str = "portal_env_net"
+
+
+config = Config(
+    env_ports={
+        'ale': 7001,
+        'mujoco': 7002,
+        'retro': 7003,
+        'craftium': 7004,
+        'vizdoom': 7005,
+        'flappy-bird': 7006,
+    }
+)
+
+
