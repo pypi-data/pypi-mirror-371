26cdca3bcc4e1e95d9cafa860530c0500c81024a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 07:05:47 +0000

    Add package turpan/0.0.0/turpan-0.0.0.tar.gz

---
+++ packages/turpan/turpan-0.0.0.tar.gz/turpan-0.0.0/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 31 00:52:16 2019
+
+@author: Jin Dou
+"""
+
+import setuptools
+import re
+
+with open("./README.md", "r") as fh:
+  long_description = fh.read()
+
+with open("turpan/__init__.py") as file:
+    for line in file.readlines():
+        m = re.match("__version__ *= *['\"](.*)['\"]", line)
+        if m:
+            version = m.group(1)
+
+setuptools.setup(
+  name="turpan",
+  version=version,
+  author="Powerfulbean",
+  author_email="powerfulbean@gmail.com",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/powerfulbean/turpan",
+  packages=setuptools.find_packages(),
+  classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+  ],
+  install_requires=[
+  ],
+)
