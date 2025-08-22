39edbd7201d6204b6fba17d9a8cc0e10f7dcefd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 08:04:11 +0000

    Add package kolena-agents/0.3.6/kolena_agents-0.3.6.tar.gz

---
+++ packages/kolena-agents/kolena_agents-0.3.6.tar.gz/kolena_agents-0.3.6/src/kolena_agents/types/agent_run.py
@@ -0,0 +1,15 @@
+from kolena_agents._generated.openapi_client import AgentRun  # type: ignore
+from kolena_agents._generated.openapi_client import AgentRunError  # type: ignore
+from kolena_agents._generated.openapi_client import AgentRunStatus  # type: ignore
+from kolena_agents._generated.openapi_client import (  # type: ignore
+    ListAgentRunsResponse,
+)
+from kolena_agents._generated.openapi_client import WaitRunResponse  # type: ignore
+
+__all__ = [
+    "AgentRun",
+    "AgentRunStatus",
+    "ListAgentRunsResponse",
+    "WaitRunResponse",
+    "AgentRunError",
+]
