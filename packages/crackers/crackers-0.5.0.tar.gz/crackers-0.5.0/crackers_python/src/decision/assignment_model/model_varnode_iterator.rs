fec0247458e49ade35ca7ac9801382280c527acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 04:04:21 +0000

    Add package crackers/0.5.0/crackers-0.5.0.tar.gz

---
+++ packages/crackers/crackers-0.5.0.tar.gz/crackers-0.5.0/crackers_python/src/decision/assignment_model/model_varnode_iterator.rs
@@ -0,0 +1,29 @@
+use jingle::python::z3::ast::PythonAst;
+use pyo3::{Py, PyAny, PyRef, PyRefMut, pyclass, pymethods};
+use z3::ast::BV;
+
+#[pyclass(unsendable)]
+pub struct ModelVarNodeIterator {
+    vn: Box<dyn Iterator<Item = (String, BV)>>,
+}
+
+impl ModelVarNodeIterator {
+    pub fn new<T: Iterator<Item = (String, BV)> + 'static>(vn: T) -> Self {
+        Self { vn: Box::new(vn) }
+    }
+}
+
+#[pymethods]
+impl ModelVarNodeIterator {
+    pub fn __iter__(slf: PyRef<Self>) -> PyRef<Self> {
+        slf
+    }
+
+    pub fn __next__(mut slf: PyRefMut<Self>) -> Option<(String, Py<PyAny>)> {
+        let (name, bv) = slf.vn.next()?;
+        match bv.try_into_python() {
+            Ok(bv) => Some((name, bv)),
+            _ => None,
+        }
+    }
+}
---
+++ packages/crackers/crackers-0.5.0.tar.gz/crackers-0.5.0/crackers_python/src/synthesis.rs
@@ -0,0 +1,95 @@
+use crate::decision::PythonDecisionResult;
+use crate::decision::assignment_model::PythonAssignmentModel;
+use crackers::error::CrackersError;
+use crackers::synthesis::DecisionResult;
+use crackers::synthesis::builder::{
+    StateConstraintGenerator, SynthesisParams, TransitionConstraintGenerator,
+};
+use jingle::JingleContext;
+use jingle::modeling::{ModeledBlock, State};
+use jingle::python::modeled_block::PythonModeledBlock;
+use jingle::python::state::PythonState;
+use jingle::python::z3::ast::PythonAst;
+use pyo3::{Py, PyAny, PyResult, Python, pyclass, pymethods};
+use std::sync::Arc;
+use z3::ast::Bool;
+
+#[pyclass(name = "SynthesisParams")]
+#[derive(Clone)]
+pub struct PythonSynthesisParams {
+    pub inner: SynthesisParams,
+}
+
+#[pymethods]
+impl PythonSynthesisParams {
+    pub fn run(&self) -> PyResult<PythonDecisionResult> {
+        let res = Python::with_gil(|py| {
+            py.allow_threads(|| match self.inner.combine_instructions {
+                false => self.inner.build_single()?.decide(),
+                true => self.inner.build_combined()?.decide(),
+            })
+        })?;
+        match res {
+            DecisionResult::AssignmentFound(a) => {
+                let a = a.build()?;
+                let a = PythonAssignmentModel::try_from(a)?;
+                Ok(PythonDecisionResult::AssignmentFound(a))
+            }
+            DecisionResult::Unsat(u) => Ok(PythonDecisionResult::Unsat(u)),
+        }
+    }
+
+    pub fn add_precondition(&mut self, obj: Py<PyAny>) {
+        let closure: Arc<PythonStateConstraintGenerator> = Arc::new(move |_, s, a| {
+            let state = PythonState::from(s.clone());
+            Python::with_gil(|py| {
+                let res = obj.call(py, (state, a), None)?;
+                let bool = Bool::try_from_python(res).map_err(CrackersError::PythonError)?;
+                Ok(bool)
+            })
+        });
+        let transmuted_closure: Arc<StateConstraintGenerator> =
+            unsafe { std::mem::transmute(closure) };
+        self.inner.preconditions.push(transmuted_closure);
+    }
+
+    pub fn add_postcondition(&mut self, obj: Py<PyAny>) {
+        let closure: Arc<PythonStateConstraintGenerator> = Arc::new(move |_, s, a| {
+            let state = PythonState::from(s.clone());
+            Python::with_gil(|py| {
+                let res = obj.call(py, (state, a), None)?;
+                let bool = Bool::try_from_python(res).map_err(CrackersError::PythonError)?;
+                Ok(bool)
+            })
+        });
+        let transmuted_closure: Arc<StateConstraintGenerator> =
+            unsafe { std::mem::transmute(closure) };
+        self.inner.postconditions.push(transmuted_closure);
+    }
+
+    pub fn add_transition_constraint(&mut self, obj: Py<PyAny>) {
+        let closure: Arc<PythonTransitionConstraintGenerator> = Arc::new(move |_, b| {
+            let block = PythonModeledBlock { instr: b.clone() };
+            Python::with_gil(|py| {
+                let res = obj.call(py, (block,), None)?;
+                if res.is_none(py) {
+                    Ok(None)
+                } else {
+                    let bool = Bool::try_from_python(res).map_err(CrackersError::PythonError)?;
+                    Ok(Some(bool))
+                }
+            })
+        });
+        let transmuted_closure: Arc<TransitionConstraintGenerator> =
+            unsafe { std::mem::transmute(closure) };
+        self.inner.pointer_invariants.push(transmuted_closure);
+    }
+}
+
+pub type PythonStateConstraintGenerator =
+    dyn Fn(&JingleContext, &State, u64) -> Result<Bool, CrackersError> + Send + Sync + 'static;
+
+pub type PythonTransitionConstraintGenerator = dyn Fn(&JingleContext, &ModeledBlock) -> Result<Option<Bool>, CrackersError>
+    + Send
+    + Sync
+    + 'static;
