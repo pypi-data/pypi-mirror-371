42cd2d64818d8da53224d2eee7ba24b16f5b8109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 04:03:37 +0000

    Add package karabo.native/3.0.2/karabo_native-3.0.2.tar.gz

---
+++ packages/karabo.native/karabo_native-3.0.2.tar.gz/karabo_native-3.0.2/src/karabo/common/tests/test_util.py
@@ -0,0 +1,36 @@
+# This file is part of Karabo.
+#
+# http://www.karabo.eu
+#
+# Copyright (C) European XFEL GmbH Schenefeld. All rights reserved.
+#
+# Karabo is free software: you can redistribute it and/or modify it under
+# the terms of the MPL-2 Mozilla Public License.
+#
+# You should have received a copy of the MPL-2 Public License along with
+# Karabo. If not, see <https://www.mozilla.org/en-US/MPL/2.0/>.
+#
+# Karabo is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.
+from karabo.common.api import WeakMethodRef
+
+
+def test_weak_method_ref():
+    """Test that we can safely call bound methods"""
+    global called
+    called = False
+
+    class Device:
+        def move(self):
+            global called
+            called = True
+
+    device = Device()
+    func = WeakMethodRef(device.move)
+    func()
+    assert called is True
+    called = False
+    del device
+    func()
+    assert called is False
