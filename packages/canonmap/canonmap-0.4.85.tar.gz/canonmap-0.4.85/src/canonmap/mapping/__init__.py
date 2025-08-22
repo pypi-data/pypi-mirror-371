db70e2a31318166b7b96c7fb8cf607679f734b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 05:04:56 +0000

    Add package canonmap/0.4.85/canonmap-0.4.85.tar.gz

---
+++ packages/canonmap/canonmap-0.4.85.tar.gz/canonmap-0.4.85/src/canonmap/mapping/__init__.py
@@ -0,0 +1,15 @@
+from .mapping_pipeline import MappingPipeline
+from .models import (
+    EntityMappingRequest,
+    EntityMappingResponse,
+    MappingWeights,
+    SingleMappedEntity,
+)
+
+__all__ = [
+    "MappingPipeline",
+    "EntityMappingRequest",
+    "EntityMappingResponse",
+    "MappingWeights",
+    "SingleMappedEntity",
+]
