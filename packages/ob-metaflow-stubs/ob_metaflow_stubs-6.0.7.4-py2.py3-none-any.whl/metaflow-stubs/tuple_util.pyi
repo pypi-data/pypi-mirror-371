472e86ad047ce83ad5ba5cccece562e534c3c0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 05:04:48 +0000

    Add package ob-metaflow-stubs/6.0.7.4/ob_metaflow_stubs-6.0.7.4-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.7.4-py2.py3-none-any.whl/metaflow-stubs/tuple_util.pyi
@@ -0,0 +1,15 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.17.1.0+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-08-21T17:03:57.335369                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+def namedtuple_with_defaults(typename, field_descr, defaults = ()):
+    ...
+
+foreach_frame_field_list: list
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.7.4-py2.py3-none-any.whl/metaflow-stubs/user_decorators/__init__.pyi
@@ -0,0 +1,15 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.17.1.0+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-08-21T17:03:57.328339                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import mutable_flow as mutable_flow
+from . import common as common
+from . import user_step_decorator as user_step_decorator
+from . import mutable_step as mutable_step
+from . import user_flow_decorator as user_flow_decorator
+
