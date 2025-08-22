b598565ea47501eb6d64cbff179254d611ec3bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-22 08:04:22 +0000

    Add package flexivsimplugin/1.1.0/flexivsimplugin-1.1.0-cp311-cp311-manylinux_2_31_x86_64.whl

---
+++ packages/flexivsimplugin/flexivsimplugin-1.1.0-cp311-cp311-manylinux_2_31_x86_64.whl/flexivsimplugin/__init__.py
@@ -0,0 +1,13 @@
+# flexivsimplugin/__init__.py
+
+"""
+A middleware plugin to connect Flexiv Elements Studio to any external simulator.
+
+- GitHub: https://github.com/flexivrobotics/flexiv_sim_plugin
+
+"""
+
+__version__ = "1.1.0"
+__author__ = "Flexiv Ltd."
+
+from .flexivsimplugin import *
