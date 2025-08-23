38a4be0a696cdb113be83fb90d2226c4e3c56de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 06:04:28 +0000

    Add package DLMS-SPODES/0.86.16/dlms_spodes-0.86.16-py3-none-any.whl

---
+++ packages/DLMS-SPODES/dlms_spodes-0.86.16-py3-none-any.whl/DLMS_SPODES/cosem_interface_classes/data.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from .__class_init__ import *
+from ..types import choices
+
+
+class Data(ic.COSEMInterfaceClasses):
+    """ Object stores data related to internal meter object(s). The meaning of the value is identified by the logical_name.
+    The data type of the value is CHOICE. “Data” is typically used to store configuration data and parameters """
+    CLASS_ID = ClassID.DATA
+    VERSION = Version.V0
+    A_ELEMENTS = ic.ICAElement("value", choices.common_dt, classifier=ic.Classifier.NOT_SPECIFIC),
+
+    def characteristics_init(self):
+        """nothing do it"""
+
+    @property
+    def value(self) -> cdt.CommonDataTypes:
+        """Contains the data"""
+        return self.get_attr(2)
