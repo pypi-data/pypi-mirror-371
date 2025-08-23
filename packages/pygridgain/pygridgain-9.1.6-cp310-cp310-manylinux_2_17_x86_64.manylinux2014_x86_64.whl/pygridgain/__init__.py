9ef98f28f54fd36ad5f22da1420b10b9b10e3784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 07:04:46 +0000

    Add package pygridgain/9.1.6/pygridgain-9.1.6-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pygridgain/pygridgain-9.1.6-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pygridgain/__init__.py
@@ -0,0 +1,25 @@
+# Copyright (C) GridGain Systems. All Rights Reserved.
+# _________        _____ __________________        _____
+# __  ____/___________(_)______  /__  ____/______ ____(_)_______
+# _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
+# / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
+# \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+import os
+
+from pygridgain.async_binary_map import AsyncBinaryMap
+from pygridgain.async_client import AsyncClient
+from pygridgain.async_structures import AsyncStructures
+from pygridgain.end_point import EndPoint
+from pygridgain.ignite_error import IgniteError
+from pygridgain.ssl_config import SSLConfig
+
+def _read_version():
+    version_path = os.path.join(os.path.dirname(__file__), "_version.txt")
+    with open(version_path, "r") as fd:
+        version = fd.read().strip()
+        if not version:
+            raise RuntimeError('Cannot find version information')
+        return version
+
+__version__ = _read_version()
+
