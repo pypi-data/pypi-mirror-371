308a4512e3ae9c5e56b0fd04dafc0b14d222359c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 03:18:53 +0000

    Add package duden-webscraper/0.1.0/duden_webscraper-0.1.0.tar.gz

---
+++ packages/duden-webscraper/duden_webscraper-0.1.0.tar.gz/duden_webscraper-0.1.0/README.md
@@ -0,0 +1,5 @@
+## Exercutar testes
+
+```sh
+uv run -m pytest --cov=duden_webscraper --cov-report=term-missing
+```
---
+++ packages/duden-webscraper/duden_webscraper-0.1.0.tar.gz/duden_webscraper-0.1.0/src/duden_webscraper/endpoint.py
@@ -0,0 +1,5 @@
+
+class Endpoint:
+    BASE = "https://www.duden.de"
+    DICTIONARY_SEARCH = "/search_api_autocomplete/dictionary_search"
+    ORTHOGRAPHY = "/rechtschreibung"
