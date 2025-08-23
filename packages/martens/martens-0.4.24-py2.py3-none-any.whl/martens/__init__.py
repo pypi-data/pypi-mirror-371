ef5dc4ba715bc70bdc586f0fbf242bec551d872c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 04:04:28 +0000

    Add package martens/0.4.24/martens-0.4.24-py2.py3-none-any.whl

---
+++ packages/martens/martens-0.4.24-py2.py3-none-any.whl/martens/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for Martens."""
+
+__author__ = 'Alex Rowley'
+__email__ = ''
+__version__ = '0.4.24'
+
+from .martens import Dataset, SourceFile, SourceStream, stack, initialise, average, median
