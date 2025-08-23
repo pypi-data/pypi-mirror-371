746ccdf02b9977888bc74be95807b9f8ea0b44cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 05:05:03 +0000

    Add package pgscatalog-utils/2.0.2/pgscatalog_utils-2.0.2-py3-none-any.whl

---
+++ packages/pgscatalog-utils/pgscatalog_utils-2.0.2-py3-none-any.whl/pgscatalog/utils/__init__.py
@@ -0,0 +1,8 @@
+import importlib.metadata
+
+__version__ = importlib.metadata.version("pgscatalog.utils")
+
+# intentionally empty :)
+#     |\__/,|   (`\
+#   _.|o o  |_   ) )
+# -(((---(((--------
