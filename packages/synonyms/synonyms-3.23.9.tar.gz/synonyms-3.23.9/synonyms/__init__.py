3bee40e590c524b55cca6111c17476585f1b0e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 06:04:38 +0000

    Add package synonyms/3.23.9/synonyms-3.23.9.tar.gz

---
+++ packages/synonyms/synonyms-3.23.9.tar.gz/synonyms-3.23.9/synonyms/__init__.py
@@ -0,0 +1,17 @@
+__all__ = ["seg",
+    "nearby", 
+    "compare", 
+    "display", 
+    "keywords",
+    "KeyedVectors", 
+    "any2utf8",
+    "sigmoid",
+    "cosine",
+    "any2unicode",
+    "np",
+    "v",
+    "__version__"]
+
+from .word2vec import *
+from .synonyms import *
+from .synonyms import __version__
