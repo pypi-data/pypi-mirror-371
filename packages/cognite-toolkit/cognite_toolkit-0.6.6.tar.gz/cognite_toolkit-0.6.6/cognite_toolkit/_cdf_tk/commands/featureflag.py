87ec3fdf112e885017c26ad952fac1a7e9de5e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 05:05:40 +0000

    Add package cognite-toolkit/0.6.6/cognite_toolkit-0.6.6.tar.gz

---
+++ packages/cognite-toolkit/cognite_toolkit-0.6.6.tar.gz/cognite_toolkit-0.6.6/cognite_toolkit/_cdf_tk/commands/featureflag.py
@@ -0,0 +1,27 @@
+from rich import print
+from rich.table import Table
+
+from cognite_toolkit._cdf_tk.cdf_toml import CDFToml
+from cognite_toolkit._cdf_tk.commands._base import ToolkitCommand
+from cognite_toolkit._cdf_tk.feature_flags import Flags
+
+
+class FeatureFlagCommand(ToolkitCommand):
+    @staticmethod
+    def list() -> None:
+        user_settings = CDFToml.load().alpha_flags
+        table = Table(title="feature flags")
+        table.add_column("Name", justify="left")
+        table.add_column("Description", justify="left")
+        table.add_column("Status", justify="left")
+
+        for flag in Flags:
+            is_enabled = user_settings.get(flag.name, False)
+            if is_enabled or flag.value.get("visible", False):
+                table.add_row(
+                    flag.name,
+                    str(flag.value.get("description", "")),
+                    "enabled" if is_enabled else "disabled",
+                    style="yellow" if is_enabled else "",
+                )
+        print(table)
---
+++ packages/cognite-toolkit/cognite_toolkit-0.6.6.tar.gz/cognite_toolkit-0.6.6/last_changelog_entry.md
@@ -0,0 +1,9 @@
+## cdf 
+
+### Added
+
+- [alpha] In the `cdf purge instances` you can now unlink files
+
+## templates
+
+No changes.
