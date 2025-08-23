6e1d22b6f044e5d908303c4a4f02e7f7de924f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 14:05:15 +0000

    Add package sportsball/0.16.38/sportsball-0.16.38.tar.gz

---
+++ packages/sportsball/sportsball-0.16.38.tar.gz/sportsball-0.16.38/sportsball/data/epl/position.py
@@ -0,0 +1,34 @@
+"""The enumeration of the different supported positions."""
+
+from enum import StrEnum
+
+
+class Position(StrEnum):
+    """An enumeration over the different positions."""
+
+    GOALKEEPER = "G"
+    CENTRE_DEFENDER_LEFT = "CD-L"
+    CENTRE_DEFENDER_RIGHT = "CD-R"
+    LEFT_BACK = "LB"
+    RIGHT_BACK = "RB"
+    ATTACKING_MIDFIELDER = "AM"
+    CENTRAL_MIDFIELDER = "CM"
+    LEFT_MIDFIELDER = "LM"
+    RIGHT_MIDFIELDER = "RM"
+    CENTRAL_MIDFIELDER_LEFT = "CM-L"
+    FORWARD = "F"
+    LEFT_FORWARD = "LF"
+    CENTRE_DEFENDER = "CD"
+    ATTACKING_MIDFIELDER_LEFT = "AM-L"
+    RIGHT_FORWARD = "RF"
+
+
+_POSITIONS = {str(x): x for x in Position}
+
+
+def position_from_str(position_str: str) -> Position:
+    """Find a position from a string."""
+    position = _POSITIONS.get(position_str)
+    if position is None:
+        raise ValueError(f"Unrecognised position: {position_str}")
+    return position
---
+++ packages/sportsball/sportsball-0.16.38.tar.gz/sportsball-0.16.38/sportsball/data/nhl/position.py
@@ -0,0 +1,12 @@
+"""The enumeration of the different supported positions."""
+
+from enum import StrEnum
+
+
+class Position(StrEnum):
+    """An enumeration over the different positions."""
+
+    DEFENSEMAN = "D"
+    CENTRE = "C"
+    GOALTENDER = "G"
+    LEFT_WING = "LW"
