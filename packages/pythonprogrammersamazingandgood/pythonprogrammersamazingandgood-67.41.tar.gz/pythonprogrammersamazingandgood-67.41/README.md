18f76643cd56730ed8f9194c0a8f3b7695b0e0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 03:19:21 +0000

    Add package pythonprogrammersamazingandgood/67.41/pythonprogrammersamazingandgood-67.41.tar.gz

---
+++ packages/pythonprogrammersamazingandgood/pythonprogrammersamazingandgood-67.41.tar.gz/pythonprogrammersamazingandgood-67.41/README.md
@@ -0,0 +1,7 @@
+so like do  
+`from pythonprogrammersamazingandgood import helloWorld`  
+  
+and then like do  
+`helloWorld("print")`  
+  
+ya
---
+++ packages/pythonprogrammersamazingandgood/pythonprogrammersamazingandgood-67.41.tar.gz/pythonprogrammersamazingandgood-67.41/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pythonprogrammersamazingandgood",
+    version="67.41",
+    packages=find_packages(),
+    install_requires=[],
+    description="the best module to ever exist",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+)
