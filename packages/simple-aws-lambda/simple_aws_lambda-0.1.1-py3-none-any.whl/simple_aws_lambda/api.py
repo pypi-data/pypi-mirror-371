9523db5ddc5d2bd6e006276fb0bed336f01b9d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 15:05:11 +0000

    Add package simple-aws-lambda/0.1.1/simple_aws_lambda-0.1.1-py3-none-any.whl

---
+++ packages/simple-aws-lambda/simple_aws_lambda-0.1.1-py3-none-any.whl/simple_aws_lambda/api.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from .model import LatestMatchingLayerVersion
+from .model import Layer
+from .model import LayerIterproxy
+from .model import LayerContent
+from .model import LayerVersion
+from .model import LayerVersionIterproxy
+from .client import list_layers
+from .client import list_layer_versions
+from .client import get_layer_version
