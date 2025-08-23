7dc65f2c82c8c30cf21d24b6533a2d85bdde9f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 07:04:58 +0000

    Add package datetimeplus/0.0.0/datetimeplus-0.0.0.tar.gz

---
+++ packages/datetimeplus/datetimeplus-0.0.0.tar.gz/datetimeplus-0.0.0/src/core.py
@@ -0,0 +1,5 @@
+from datetime import datetime as base_datetime
+
+
+class datetime(base_datetime):
+    """extended datetime class"""
---
+++ packages/datetimeplus/datetimeplus-0.0.0.tar.gz/datetimeplus-0.0.0/tests/test_core.py
@@ -0,0 +1,3 @@
+class TestCore:
+    def test_sample(self) -> None:
+        assert True
