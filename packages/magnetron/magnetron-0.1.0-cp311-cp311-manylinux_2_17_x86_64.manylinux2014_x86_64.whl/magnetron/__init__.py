21e09a8d32b8bbf4f0f005d73286ea6c65849d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 14:05:07 +0000

    Add package magnetron/0.1.0/magnetron-0.1.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/magnetron/magnetron-0.1.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/magnetron/__init__.py
@@ -0,0 +1,13 @@
+# (c) 2025 Mario "Neo" Sieg. <mario.sieg.64@gmail.com>
+
+__author__ = 'Mario Sieg'
+__email__ = 'mario.sieg.64@gmail.com'
+__author_email__ = 'mario.sieg.64@gmail.com'
+__license__ = 'Apache 2.0'
+__url__ = 'https://github.com/MarioSieg/magnetron'
+
+from ._core import *
+from ._dtype import *
+from ._context import *
+from ._tensor import *
+__version__ = "0.2.0"
