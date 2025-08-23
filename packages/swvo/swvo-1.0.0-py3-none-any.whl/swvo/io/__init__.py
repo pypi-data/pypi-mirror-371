1af1f07d13f78091e357d7fa60b233f0efce6027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 06:04:42 +0000

    Add package swvo/1.0.0/swvo-1.0.0-py3-none-any.whl

---
+++ packages/swvo/swvo-1.0.0-py3-none-any.whl/swvo/io/__init__.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2025 GFZ Helmholtz Centre for Geosciences
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import swvo.io.base_file_reader
+from swvo.io import (
+    dst,
+    f10_7,
+    hp,
+    kp,
+    omni,
+    plasmasphere,
+    RBMDataSet,
+    solar_wind,
+)
