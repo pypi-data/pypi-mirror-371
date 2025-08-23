cab311789390aab134fe129a3266ad8d5e7d4722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 11:05:21 +0000

    Add package alwayson-py/0.1.0/alwayson_py-0.1.0-cp312-cp312-win_amd64.whl

---
+++ packages/alwayson-py/alwayson_py-0.1.0-cp312-cp312-win_amd64.whl/alwayson/__init__.py
@@ -0,0 +1,15 @@
+"""
+AlwaysOn Python - Python bindings for ThingWorx AlwaysOn protocol
+
+This package provides Python bindings for the alwayson-codec Rust library,
+enabling efficient encoding and decoding of ThingWorx AlwaysOn protocol messages.
+"""
+
+from ._native import AlwaysOnError, BaseType, TwPrim, __version__
+
+__all__ = [
+    "AlwaysOnError",
+    "BaseType",
+    "TwPrim",
+    "__version__",
+]
