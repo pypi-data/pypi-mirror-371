dc16474f36988170f49049900b6d6636423a6943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 14:05:05 +0000

    Add package spounge-proto-py/0.2.9/spounge_proto_py-0.2.9-py3-none-any.whl

---
+++ packages/spounge-proto-py/spounge_proto_py-0.2.9-py3-none-any.whl/spounge/polykey/v2/__init__.py
@@ -0,0 +1,4 @@
+# Auto-generated __init__.py
+from . import auth_pb2, auth_pb2_grpc, batch_operations_pb2, batch_operations_pb2_grpc, common_pb2, common_pb2_grpc, key_types_pb2, key_types_pb2_grpc, metrics_pb2, metrics_pb2_grpc, service_pb2, service_pb2_grpc, single_operations_pb2, single_operations_pb2_grpc, stream_operations_pb2, stream_operations_pb2_grpc
+
+__all__ = ["auth_pb2", "auth_pb2_grpc", "batch_operations_pb2", "batch_operations_pb2_grpc", "common_pb2", "common_pb2_grpc", "key_types_pb2", "key_types_pb2_grpc", "metrics_pb2", "metrics_pb2_grpc", "service_pb2", "service_pb2_grpc", "single_operations_pb2", "single_operations_pb2_grpc", "stream_operations_pb2", "stream_operations_pb2_grpc"]
