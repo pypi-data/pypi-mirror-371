87f1cdaf25cae46611d99aac9e2a0fc55dbfd92b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 09:04:42 +0000

    Add package omlish/0.0.0.dev409/omlish-0.0.0.dev409.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev409.tar.gz/omlish-0.0.0.dev409/omlish/lang/maysyncs.py
@@ -0,0 +1,44 @@
+import typing as ta
+
+from ..lite.maysyncs import Maywaitable
+from ..lite.maysyncs import make_maysync as _make_maysync
+from ..lite.maysyncs import maysync as _maysync
+from .functions import as_async
+
+
+T = ta.TypeVar('T')
+P = ta.ParamSpec('P')
+
+MaysyncP: ta.TypeAlias = ta.Callable[P, Maywaitable[T]]
+
+
+##
+
+
+def make_maysync(
+        s: ta.Callable[P, T],
+        a: ta.Callable[P, ta.Awaitable[T]],
+) -> MaysyncP[P, T]:
+    return ta.cast('MaysyncP[P, T]', _make_maysync(
+        s,
+        a,
+    ))
+
+
+def make_maysync_from_sync(
+        s: ta.Callable[P, T],
+        a: ta.Callable[P, ta.Awaitable[T]] | None = None,
+) -> MaysyncP[P, T]:
+    return ta.cast('MaysyncP[P, T]', _make_maysync(
+        s,
+        a if a is not None else as_async(s),
+    ))
+
+
+##
+
+
+def maysync(m: ta.Callable[P, ta.Awaitable[T]]) -> MaysyncP[P, T]:
+    return ta.cast('MaysyncP[P, T]', _maysync(
+        m,
+    ))
