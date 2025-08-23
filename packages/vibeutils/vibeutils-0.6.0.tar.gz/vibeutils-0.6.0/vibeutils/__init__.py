63a9363b2681f1c3f21d1f793323f708d4aec1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 05:05:02 +0000

    Add package vibeutils/0.6.0/vibeutils-0.6.0.tar.gz

---
+++ packages/vibeutils/vibeutils-0.6.0.tar.gz/vibeutils-0.6.0/vibeutils/__init__.py
@@ -0,0 +1,9 @@
+"""
+vibeutils - A Python library that provides various utilities using OpenAI and Anthropic APIs
+"""
+
+from .core import vibecount, vibecompare, vibeeval, Provider
+
+__version__ = "0.6.0"
+__author__ = "chuyang-deng"
+__all__ = ["vibecount", "vibecompare", "vibeeval", "Provider"]
