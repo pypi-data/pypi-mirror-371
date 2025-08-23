ba478d5988aadc639c738fd5f104eb4fa6c60e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 11:05:09 +0000

    Add package telegram-bot-discussion/0.0.11/telegram_bot_discussion-0.0.11.tar.gz

---
+++ packages/telegram-bot-discussion/telegram_bot_discussion-0.0.11.tar.gz/telegram_bot_discussion-0.0.11/telegram_bot_discussion/handlers/__init__.py
@@ -0,0 +1,4 @@
+from .buttons_handler.buttons_handler import ButtonsHandler as ButtonsHandler
+from .commands_handler.commands_handler import CommandsHandler as CommandsHandler
+from .middleware.base_middleware import BaseMiddleware as BaseMiddleware
+from .replicas_handler.replicas_handler import ReplicasHandler as ReplicasHandler
