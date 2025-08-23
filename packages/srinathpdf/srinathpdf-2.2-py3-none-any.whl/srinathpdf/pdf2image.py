313c42c67910cd801d3cf75539f96a6ebd3dedad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 15:05:12 +0000

    Add package srinathpdf/2.2/srinathpdf-2.2-py3-none-any.whl

---
+++ packages/srinathpdf/srinathpdf-2.2-py3-none-any.whl/srinathpdf/pdf2image.py
@@ -0,0 +1,11 @@
+def convert():
+    """
+    Convert the given PDF to image.
+
+    Parameters:
+    path(str): The path to a PDF file.
+
+    Returns:
+    str: The content of the PDF file as image.
+    """
+    print("pdf2image")
---
+++ packages/srinathpdf/srinathpdf-2.2-py3-none-any.whl/srinathpdf/pdf2text.py
@@ -0,0 +1,11 @@
+def convert():
+    """
+    Convert the given PDF to text.
+
+    Parameters:
+    path(str): The path to a PDF file.
+
+    Returns:
+    str: The content of the PDF file as text.
+    """ 
+    print("pdf2text")
