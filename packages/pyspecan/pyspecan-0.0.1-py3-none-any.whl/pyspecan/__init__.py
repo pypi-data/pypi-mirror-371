edcbde5c839007911d78b8a0d869d6e1dcf193bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 14:05:06 +0000

    Add package pyspecan/0.0.1/pyspecan-0.0.1-py3-none-any.whl

---
+++ packages/pyspecan/pyspecan-0.0.1-py3-none-any.whl/pyspecan/__init__.py
@@ -0,0 +1,12 @@
+"""
+pyspecan
+
+A spectrum analyzer library
+
+Github: https://github.com/Anonoei/pyspecan
+
+PyPI: https://pypi.org/project/pyspecan/
+"""
+
+__version__ = "0.0.1"
+__author__ = "Anonoei <to+dev@an0.cx>"
