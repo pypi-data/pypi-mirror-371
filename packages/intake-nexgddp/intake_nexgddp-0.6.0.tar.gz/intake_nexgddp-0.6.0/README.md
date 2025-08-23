7062fec9688add9fc58498163f1cba6bdf8f7c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 04:05:08 +0000

    Add package intake-nexgddp/0.6.0/intake_nexgddp-0.6.0.tar.gz

---
+++ packages/intake-nexgddp/intake_nexgddp-0.6.0.tar.gz/intake_nexgddp-0.6.0/README.md
@@ -0,0 +1,14 @@
+# intake-nexgddp
+
+### To publish to pypi,
+`pip install setuptools wheel twine`
+
+`python -m build`
+
+`twine upload dist/* `
+
+
+
+## Developer Only
+Pypi token:
+get one from pypi.org
---
+++ packages/intake-nexgddp/intake_nexgddp-0.6.0.tar.gz/intake_nexgddp-0.6.0/setup.py
@@ -0,0 +1,22 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='intake-nexgddp',
+    version='0.6.0',
+    description='Intake plugin for NEX-GDDP-CMIP6 via OpenVisus',
+    author='Aashish Panta',
+    packages=find_packages(),
+    entry_points={
+        'intake.source_plugins': [
+            'nexgddp = intake_nexgddp.catalog',
+        ]
+    },
+    install_requires=[
+        'intake',
+        'xarray',
+        'numpy',
+        'openvisus',
+    ]
+)
