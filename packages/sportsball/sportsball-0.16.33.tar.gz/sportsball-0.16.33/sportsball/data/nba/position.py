f4d7a32a16be95b8d37c84f63356cdc30aee3701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 11:05:39 +0000

    Add package sportsball/0.16.33/sportsball-0.16.33.tar.gz

---
+++ packages/sportsball/sportsball-0.16.33.tar.gz/sportsball-0.16.33/sportsball/data/nba/position.py
@@ -0,0 +1,22 @@
+"""The enumeration of the different supported positions."""
+
+# pylint: disable=duplicate-code
+from enum import StrEnum
+
+
+class Position(StrEnum):
+    """An enumeration over the different positions."""
+
+    POWER_FORWARD = "PF"
+    SHORT_GUARD = "SG"
+
+
+_POSITIONS = {str(x): x for x in Position}
+
+
+def position_from_str(position_str: str) -> Position:
+    """Find a position from a string."""
+    position = _POSITIONS.get(position_str)
+    if position is None:
+        raise ValueError(f"Unrecognised position: {position_str}")
+    return position
