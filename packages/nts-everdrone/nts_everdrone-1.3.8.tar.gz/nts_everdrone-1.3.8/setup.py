4bd10bba24cb1269f10b52d0a0fb597aee6cf3af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 05:05:36 +0000

    Add package nts-everdrone/1.3.8/nts_everdrone-1.3.8.tar.gz

---
+++ packages/nts-everdrone/nts_everdrone-1.3.8.tar.gz/nts_everdrone-1.3.8/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+setuptools.setup(
+    name="nts-everdrone",
+    version="1.3.8",
+    author="Giorgio Tropiano",
+    author_email="giorgiotropiano@gmail.com",
+    description="NTS Radio downloader tool",
+    long_description=open('readme.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/everdrone/nts",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT',
+    entry_points={'console_scripts': ['nts=nts.cli:main']},
+    install_requires=[
+        'yt-dlp==2024.7.25',
+        'beautifulsoup4==4.9.3',
+        'mutagen==1.45.1',
+        'requests==2.32.4',
+        'cssutils==2.7.1',
+        'ffmpeg-python==0.2.0',
+        'music-tag==0.4.3',
+        'pillow==10.4.0',
+    ],
+    python_requires='>=3.7.5',
+)
