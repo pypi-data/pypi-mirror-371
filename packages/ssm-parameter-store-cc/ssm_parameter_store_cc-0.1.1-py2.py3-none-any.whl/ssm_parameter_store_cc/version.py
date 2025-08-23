7763f7f508098d35dee024141a827a852d9bd611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 14:05:19 +0000

    Add package ssm-parameter-store-cc/0.1.1/ssm_parameter_store_cc-0.1.1-py2.py3-none-any.whl

---
+++ packages/ssm-parameter-store-cc/ssm_parameter_store_cc-0.1.1-py2.py3-none-any.whl/ssm_parameter_store_cc/version.py
@@ -0,0 +1,6 @@
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("ssm-parameter-store")
+except PackageNotFoundError:
+    __version__ = 'unknown'  # package not installed
