7e481109c4eddeac63aa76d5f4980d231a9f140f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 11:05:44 +0000

    Add package fourlabs-auth/0.1.17/fourlabs_auth-0.1.17.tar.gz

---
+++ packages/fourlabs-auth/fourlabs_auth-0.1.17.tar.gz/fourlabs_auth-0.1.17/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fourlabs-auth',
+    version='0.1.17',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='Autenticação por email fourlabs para Django',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Fourlabs',
+    author_email='fourlabs-un2@foursys.com.br',
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'Django>=5.2',
+        'msal>=1.30.0',  # Adicionado MSAL
+        'requests>=2.28.0',  # Também necessário para GraphEmailClient
+    ],
+    python_requires='>=3.8',
+)
