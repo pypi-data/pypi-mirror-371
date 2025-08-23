91f80924279f8e6d03f11d37b8e74aead596a616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 05:05:25 +0000

    Add package simtoolsz/0.1.3/simtoolsz-0.1.3-py3-none-any.whl

---
+++ packages/simtoolsz/simtoolsz-0.1.3-py3-none-any.whl/simtoolsz/__init__.py
@@ -0,0 +1,15 @@
+import importlib.metadata
+
+import simtoolsz.mail as mail
+import simtoolsz.utils as utils
+
+
+try:
+    __version__ = importlib.metadata.version("simtoolsz")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.1.3"
+
+__all__ = [
+    '__version__', 'mail', 'utils'
+
+]
