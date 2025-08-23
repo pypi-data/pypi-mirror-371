d1b41a72ac94a567c3b1504d24a5079f10dfedb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 09:04:42 +0000

    Add package h2m2t/0.4.0/h2m2t-0.4.0.tar.gz

---
+++ packages/h2m2t/h2m2t-0.4.0.tar.gz/h2m2t-0.4.0/h2m2t/__init__.py
@@ -0,0 +1,13 @@
+from h2m2t.h2m import html_to_markdown
+from h2m2t.h2t import html_to_text
+from h2m2t.m2b import markdown_to_bbcode
+from h2m2t.m2h import markdown_to_html
+from h2m2t.m2t import markdown_to_text
+
+__all__ = [
+    "html_to_markdown",
+    "html_to_text",
+    "markdown_to_bbcode",
+    "markdown_to_html",
+    "markdown_to_text",
+]
