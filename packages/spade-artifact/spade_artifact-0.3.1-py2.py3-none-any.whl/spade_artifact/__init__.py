82fe05a954bcd082c3be618ac60dd5e7cd88c4b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 11:05:08 +0000

    Add package spade-artifact/0.3.1/spade_artifact-0.3.1-py2.py3-none-any.whl

---
+++ packages/spade-artifact/spade_artifact-0.3.1-py2.py3-none-any.whl/spade_artifact/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for spade-artifact."""
+
+__author__ = """Javi Palanca"""
+__email__ = "jpalanca@gmail.com"
+__version__ = "0.3.1"
+
+from .artifact import Artifact
+from .agent import ArtifactMixin
+
+__all__ = ['Artifact', 'ArtifactMixin']
+
