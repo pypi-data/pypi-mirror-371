954785e6e99d6e04d0efbf0a033d9bb93c42f27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 15:05:35 +0000

    Add package geodepoly/0.1.8/geodepoly-0.1.8-py3-none-any.whl

---
+++ packages/geodepoly/geodepoly-0.1.8-py3-none-any.whl/geodepoly/__init__.py
@@ -0,0 +1,28 @@
+from .solver import solve_poly, solve_all, solve_one
+from .series_solve import series_solve_all, series_one_root
+from .hyper_catalan import (
+    hyper_catalan_coefficient,
+    evaluate_hyper_catalan,
+    catalan_number,
+    evaluate_quadratic_slice,
+)
+from .batched import (
+    batched_poly_eval,
+    batched_newton_step,
+    torch_root_layer,
+)
+
+__all__ = [
+    "solve_poly",
+    "solve_all",
+    "solve_one",
+    "series_solve_all",
+    "series_one_root",
+    "hyper_catalan_coefficient",
+    "evaluate_hyper_catalan",
+    "catalan_number",
+    "evaluate_quadratic_slice",
+    "batched_poly_eval",
+    "batched_newton_step",
+    "torch_root_layer",
+]
