ceff00510873811810d7a2b00b82f6ce17322697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 07:05:17 +0000

    Add package pycleancopy/2025.8.22.0/pycleancopy-2025.8.22.0.tar.gz

---
+++ packages/pycleancopy/pycleancopy-2025.8.22.0.tar.gz/pycleancopy-2025.8.22.0/src_py/cleancopy/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from cleancopy._cst.ast_transformer import transform
+from cleancopy._cst.treesitter_ import parse
+
+__all__ = [
+    'parse',
+    'transform'
+]
