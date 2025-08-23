66e42b88fed28dca95a3cd9999d2c6802428f9c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 03:20:02 +0000

    Add package hdgdk/0.1.1/hdgdk-0.1.1.tar.gz

---
+++ packages/hdgdk/hdgdk-0.1.1.tar.gz/hdgdk-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hdgdk',
+    version='0.1.1',
+    description='HD RPG 引擎框架(GDK) Python API',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='HD GDK API',
+    author_email='hdgdk@example.com',
+    url='https://github.com/hdgdk/hdgdk-python',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+    ],
+    python_requires='>=3.6',
+    install_requires=[],
+    include_package_data=True,
+    package_data={
+        # 可以在这里指定要包含的额外文件
+    },
+)
