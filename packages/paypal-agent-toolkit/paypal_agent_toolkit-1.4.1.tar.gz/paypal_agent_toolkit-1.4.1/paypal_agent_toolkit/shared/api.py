fd69d49ed83b2ae0a5e3de9c947c01def0616c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 08:05:25 +0000

    Add package paypal-agent-toolkit/1.4.1/paypal_agent_toolkit-1.4.1.tar.gz

---
+++ packages/paypal-agent-toolkit/paypal_agent_toolkit-1.4.1.tar.gz/paypal_agent_toolkit-1.4.1/paypal_agent_toolkit/shared/api.py
@@ -0,0 +1,28 @@
+
+
+from typing import Optional
+from pydantic import BaseModel
+from .configuration import Context
+from .paypal_client import PayPalClient
+from .tools import tools
+
+class PayPalAPI(BaseModel):
+    
+    _context: Context
+    _paypal_client: PayPalClient
+    
+    def __init__(self, client_id: str, secret: str, context: Optional[Context]):
+        super().__init__()
+
+        self._context = context if context is not None else Context()
+        self._paypal_client = PayPalClient(client_id=client_id, secret=secret, context=context)
+        
+    
+    def run(self, method: str, params: dict) -> str:
+        for tool in tools:
+            if tool.get("method") == method:
+                execute_fn = tool.get("execute")
+                if execute_fn:
+                    return execute_fn(self._paypal_client, params)
+        raise ValueError(f"method: {method} not found in tools list")
+
