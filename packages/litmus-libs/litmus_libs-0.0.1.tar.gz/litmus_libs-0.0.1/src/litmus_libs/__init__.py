6d90a39736cd3c6e7e2e97b41bbbf870cba87f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 07:04:47 +0000

    Add package litmus-libs/0.0.1/litmus_libs-0.0.1.tar.gz

---
+++ packages/litmus-libs/litmus_libs-0.0.1.tar.gz/litmus_libs-0.0.1/src/litmus_libs/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Utilities to work with litmus."""
+
+from .models import DatabaseConfig
+
+__all__ = [
+    "DatabaseConfig"
+]
