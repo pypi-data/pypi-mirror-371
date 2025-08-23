2f5e0cb47e9b8500e129044e14b9bed686db52ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 05:04:50 +0000

    Add package apscale-gui/2.0.8/apscale_gui-2.0.8.tar.gz

---
+++ packages/apscale-gui/apscale_gui-2.0.8.tar.gz/apscale_gui-2.0.8/apscale_gui/__main__.py
@@ -0,0 +1,16 @@
+def main():
+    message = """
+            APSCALE has been updated to version 4
+            
+            Please update Python to Python 3.12
+            
+            Conda users: Upgrade the environment to APSCALE4
+            
+            Find more information here:
+            https://github.com/TillMacher/apscale_gui
+            https://github.com/TillMacher/apscale_installer
+            """
+    print(message)
+
+if __name__ == "__main__":
+    main()
