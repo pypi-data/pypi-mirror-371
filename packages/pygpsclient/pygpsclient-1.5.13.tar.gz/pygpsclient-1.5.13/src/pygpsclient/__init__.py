e5e7fed7d43f8ea0f0881d92e773c94613907faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 15:05:12 +0000

    Add package pygpsclient/1.5.13/pygpsclient-1.5.13.tar.gz

---
+++ packages/pygpsclient/pygpsclient-1.5.13.tar.gz/pygpsclient-1.5.13/src/pygpsclient/__init__.py
@@ -0,0 +1,14 @@
+"""
+Created on 27 Sep 2020
+
+:author: semuadmin
+:copyright: 2020 SEMU Consulting
+:license: BSD 3-Clause
+"""
+
+# pylint: disable=invalid-name
+
+from pygpsclient._version import __version__
+from pygpsclient.helpers import nmea2preset, ubx2preset
+
+version = __version__
