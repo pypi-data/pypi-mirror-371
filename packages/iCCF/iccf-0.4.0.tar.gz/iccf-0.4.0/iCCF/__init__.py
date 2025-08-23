27c5382fd7332695da7ec76cd30f1e46bea98a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 05:04:47 +0000

    Add package iCCF/0.4.0/iccf-0.4.0.tar.gz

---
+++ packages/iCCF/iccf-0.4.0.tar.gz/iccf-0.4.0/iCCF/__init__.py
@@ -0,0 +1,23 @@
+"""
+Analysis tools for common line profile indicators measured from the
+cross-correlation function (CCF).
+"""
+
+from .version import __version__
+
+from .bisector import BIS, BISplus, BISminus, BIS_HARPS
+from .gaussian import gauss, gaussfit, RV, FWHM, contrast
+from .bigaussian import bigauss, bigaussfit
+from .vspan import vspan
+from .wspan import wspan
+from .meta import calculate_ccf
+from .masks import Mask
+
+from .chromatic import chromaticRV
+
+# from .iCCF import EPS, nEPS
+
+from .iCCF import Indicators
+from_file = Indicators.from_file
+
+from .iCCF import bjd, vrad, svrad
