55d22f50cb328d3672eb3fa35848c0835dc779f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 07:05:34 +0000

    Add package spechomo/0.10.4/spechomo-0.10.4.tar.gz

---
+++ packages/spechomo/spechomo-0.10.4.tar.gz/spechomo-0.10.4/AUTHORS.rst
@@ -0,0 +1,9 @@
+Development Lead
+----------------
+
+* Daniel Scheffler <daniel.scheffler@gfz.de>
+
+Contributors
+------------
+
+None yet. Why not be the first?
---
+++ packages/spechomo/spechomo-0.10.4.tar.gz/spechomo-0.10.4/setup.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# spechomo, Spectral homogenization of multispectral satellite data
+#
+# Copyright (C) 2019–2025
+# - Daniel Scheffler (GFZ Potsdam, daniel.scheffler@gfz.de)
+# - GFZ Helmholtz Centre for Geosciences, Potsdam, Germany (https://www.gfz.de)
+#
+# This software was developed within the context of the GeoMultiSens project funded
+# by the German Federal Ministry of Education and Research
+# (project grant code: 01 IS 14 010 A-C).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   https://www.apache.org/licenses/LICENSE-2.0
+#
+# Please note the following exception: `spechomo` depends on tqdm, which is
+# distributed under the Mozilla Public Licence (MPL) v2.0 except for the files
+# "tqdm/_tqdm.py", "setup.py", "README.rst", "MANIFEST.in" and ".gitignore".
+# Details can be found here: https://github.com/tqdm/tqdm/blob/master/LICENCE.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+version = {}
+with open("spechomo/version.py") as version_file:
+    exec(version_file.read(), version)
+
+req = [
+    'dill',
+    'geoarray>=0.15.0',
+    'matplotlib',
+    'natsort',
+    'nested_dict',
+    'numpy<2',
+    'pandas',
+    'pyrsr>=0.5.0',
+    'pyyaml',
+    'scikit-learn>=0.23.2,<1',  # Classifiers trained with 0.23.2; >1 breaks list_available_transformations; >1.1 breaks QR  # noqa
+    'scipy<1.11',  # >=1.11 breaks create_classifiers_RR
+    'seaborn',
+    'specclassify>=0.3.0',
+    'tabulate',
+    'tqdm',
+]
+
+req_setup = ['setuptools-git']  # needed for package_data version-controlled by GIT
+
+req_test = ['pytest', 'pytest-cov', 'pytest-reporter-html1', 'urlchecker']
+
+req_doc = ['sphinx-argparse', 'sphinx_rtd_theme', 'sphinx-autodoc-typehints']
+
+req_lint = ['flake8', 'pycodestyle', 'pydocstyle']
+
+req_dev = req_setup + req_test + req_doc + req_lint
+
+setup(
+    author="Daniel Scheffler",
+    author_email='daniel.scheffler@gfz.de',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    description="Spectral homogenization of multispectral satellite data.",
+    extras_require={
+        "doc": req_doc,
+        "test": req_test,
+        "lint": req_lint,
+        "dev": req_dev
+    },
+    install_requires=req,
+    license="Apache-2.0",
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/x-rst',
+    include_package_data=True,
+    package_data={'spechomo': ['options_default.yaml',
+                               # 'resources/**/**/*'
+                               ]},
+    keywords=['spechomo', 'spectral homogenization', 'sensor fusion', 'remote sensing'],
+    name='spechomo',
+    packages=find_packages(include=['spechomo'], exclude=['tests']),
+    setup_requires=req_setup,
+    test_suite='tests',
+    tests_require=req_test,
+    url='https://git.gfz-potsdam.de/geomultisens/spechomo',
+    version=version['__version__'],
+    zip_safe=False,
+)
