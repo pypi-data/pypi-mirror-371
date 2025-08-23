3996ae2c853efda6557da8e29468dde7a5b7e029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 09:04:30 +0000

    Add package pyvider-rpcplugin/0.0.112/pyvider_rpcplugin-0.0.112-py3-none-any.whl

---
+++ packages/pyvider-rpcplugin/pyvider_rpcplugin-0.0.112-py3-none-any.whl/pyvider/__init__.py
@@ -0,0 +1,10 @@
+#
+# pyvider/__init__.py
+#
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+# 🐍🏗️🔌
+
+
+
+# 🐍🔌🚀🪄
---
+++ packages/pyvider-rpcplugin/pyvider_rpcplugin-0.0.112-py3-none-any.whl/pyvider/rpcplugin/protocol/grpc_controller_pb2.pyi
@@ -0,0 +1,8 @@
+from google.protobuf import descriptor as _descriptor  # type: ignore[import-untyped]
+from google.protobuf import message as _message  # type: ignore[import-untyped]
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Empty(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
