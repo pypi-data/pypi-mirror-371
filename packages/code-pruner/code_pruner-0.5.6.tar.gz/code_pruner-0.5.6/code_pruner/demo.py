e022082be3455042f615faa38f8ab4113a6d12da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 11:05:18 +0000

    Add package code-pruner/0.5.6/code_pruner-0.5.6.tar.gz

---
+++ packages/code-pruner/code_pruner-0.5.6.tar.gz/code_pruner-0.5.6/code_pruner/demo.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+from .prune import prune_code
+from .pruning.data_types import ExceptPatterns, Patterns
+
+python_file = f'{os.path.dirname(__file__)}/data/example.py'
+code = Path(python_file).read_text()
+
+patterns = [
+    *Patterns.ALL,
+    # *ExceptPatterns.IMPORT,
+    # *ExceptPatterns.FUNCTION_DOCSTRING,
+    *ExceptPatterns.CLASS_BODY,
+    *Patterns.FUNCTION_DEF,
+    *ExceptPatterns.FUNCTION_DOCSTRING
+]
+
+pruned_code = prune_code(code, patterns)
+
+with(open(python_file.replace('.py', '_pruned.py'), 'w')) as f:
+    f.write(pruned_code)
