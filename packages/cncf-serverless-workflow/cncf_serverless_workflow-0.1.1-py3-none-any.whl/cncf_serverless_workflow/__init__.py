138d42f85783e528474a31c27e52336d504b2c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 08:05:33 +0000

    Add package cncf-serverless-workflow/0.1.1/cncf_serverless_workflow-0.1.1-py3-none-any.whl

---
+++ packages/cncf-serverless-workflow/cncf_serverless_workflow-0.1.1-py3-none-any.whl/cncf_serverless_workflow/__init__.py
@@ -0,0 +1,9 @@
+"""CNCF Serverless Workflow v1.0 Python package.
+
+Provides validation and execution for CNCF Serverless Workflow v1.0 specifications.
+"""
+
+from .workflow_validator import load_yaml, validate_workflow
+from .workflow_engine import execute_workflow, validate_state_flow
+
+__version__ = "0.1.1"
