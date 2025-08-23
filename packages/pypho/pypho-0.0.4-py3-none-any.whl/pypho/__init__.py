65b308e4984f259ad544a9cdab6b75804fb51f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 07:05:05 +0000

    Add package pypho/0.0.4/pypho-0.0.4-py3-none-any.whl

---
+++ packages/pypho/pypho-0.0.4-py3-none-any.whl/pypho/__init__.py
@@ -0,0 +1,13 @@
+"""Photogrammetric Design
+
+This module proposes classes for designing a photogrammetric campaign.
+There are definitions of Cameras, Lenses, and Pictures (aka CameraShot),
+as well as basic Objects to be modelled and visualisation tools
+based on pyvista.
+
+Modules:
+- camera.py: defines camera registry, manipulation, and how to take pictures
+- target.py: defines different kinds of typical object to be digitised
+- graphics.py: to visualise configuration of the photography campaing
+"""
+__version__ = "0.0.4"
