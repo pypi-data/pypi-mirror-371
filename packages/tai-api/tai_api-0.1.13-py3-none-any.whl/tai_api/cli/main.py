c22e6e2f066b4c7da2d6db4614a146e87021b912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 07:04:56 +0000

    Add package tai-api/0.1.13/tai_api-0.1.13-py3-none-any.whl

---
+++ packages/tai-api/tai_api-0.1.13-py3-none-any.whl/tai_api/cli/main.py
@@ -0,0 +1,20 @@
+import click
+from .commands import (
+    generate,
+    init,
+    dev,
+    set_auth
+)
+
+@click.group()
+def cli():
+    """CLI para tai-api: Un framework para APIs basado en FastAPI."""
+    pass
+
+cli.add_command(generate)
+cli.add_command(init)
+cli.add_command(dev)
+cli.add_command(set_auth)
+
+if __name__ == '__main__':
+    cli()
