4073801663cf3cd8972b5e44a9e6738c38c54d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 11:05:07 +0000

    Add package mcdreforged/2.15.0b2/mcdreforged-2.15.0b2.tar.gz

---
+++ packages/mcdreforged/mcdreforged-2.15.0b2.tar.gz/mcdreforged-2.15.0b2/mcdreforged/utils/collection_utils.py
@@ -0,0 +1,21 @@
+import queue
+from typing import List, TypeVar, Iterable
+
+_T = TypeVar('_T')
+
+
+def unique_list(lst: Iterable[_T]) -> List[_T]:
+	return list(dict.fromkeys(lst).keys())
+
+
+def drain_iterate_queue(q: 'queue.Queue[_T]') -> Iterable[_T]:
+	while True:
+		try:
+			yield q.get(block=False)
+		except queue.Empty:
+			break
+
+
+def drain_queue(q: 'queue.Queue[_T]'):
+	for _ in drain_iterate_queue(q):
+		pass
