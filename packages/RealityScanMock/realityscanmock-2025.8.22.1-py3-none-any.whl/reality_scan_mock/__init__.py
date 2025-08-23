f021588855939e60eff3f10dd00baf1fa4891ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 05:05:43 +0000

    Add package RealityScanMock/2025.8.22.1/realityscanmock-2025.8.22.1-py3-none-any.whl

---
+++ packages/RealityScanMock/realityscanmock-2025.8.22.1-py3-none-any.whl/reality_scan_mock/__init__.py
@@ -0,0 +1,14 @@
+################################################################
+#                                                              #
+#  This file is part of RealityScanMock                        #
+#                                                              #
+#      https://github.com/basejumpa/RealityScanMock            #
+#                                                              #
+#  Copyright (c) 2025 Alexander Mann-Wahrenberg (basejumpa)    #
+#                                                              #
+#  License                                                     #
+#                                                              #
+#  - MIT for contents used as software                         #
+#                                                              #
+################################################################
+
