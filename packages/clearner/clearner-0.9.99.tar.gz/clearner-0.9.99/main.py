fe0d1d72f4c54e76ee2c02a7c3ebce43fc0a377a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 11:05:42 +0000

    Add package clearner/0.9.99/clearner-0.9.99.tar.gz

---
+++ packages/clearner/clearner-0.9.99.tar.gz/clearner-0.9.99/main.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+# Copyright (C) Prizmi, LLC - All Rights Reserved
+# Unauthorized copying or use of this file is strictly prohibited and subject to prosecution under applicable laws
+# Proprietary and confidential
+
+from learner.setup.main import main
+
+if __name__ == '__main__':
+    main()
