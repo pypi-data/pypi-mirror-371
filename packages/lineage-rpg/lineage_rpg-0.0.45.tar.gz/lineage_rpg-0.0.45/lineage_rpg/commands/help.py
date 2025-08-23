4cd0ebedf74ffcb182290a63d5dc3ddab0bd5c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 04:04:39 +0000

    Add package lineage-rpg/0.0.45/lineage_rpg-0.0.45.tar.gz

---
+++ packages/lineage-rpg/lineage_rpg-0.0.45.tar.gz/lineage_rpg-0.0.45/lineage_rpg/commands/help.py
@@ -0,0 +1,7 @@
+def execute(playerData):
+    print("Available commands:")
+    print("~ help - View all commands")
+    print("~ bank - Check your bank balance") 
+    print("~ deposit <amount> - Deposit money into your bank")
+    print("~ withdraw <amount> - Withdraw money from your bank")
+    print("~ exit - Exit the game")
