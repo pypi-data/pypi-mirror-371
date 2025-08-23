c2fee61fbe9fe57fbc47ed9984711d5aac9d5361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 06:04:58 +0000

    Add package django-user-status/0.1.0/django_user_status-0.1.0-py3-none-any.whl

---
+++ packages/django-user-status/django_user_status-0.1.0-py3-none-any.whl/django-user-status/middleware.py
@@ -0,0 +1,26 @@
+# middleware.py
+
+from django.conf import settings
+from django.core.cache import cache
+from datetime import datetime
+
+
+class UserStatusMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.cache_duration = getattr(
+            settings, "USER_STATUS_CACHE_DURATION", 300
+        )  # Default to 5 minutes
+        self.cache_key_prefix = getattr(
+            settings, "USER_STATUS_CACHE_KEY_PREFIX", "last_seen"
+        )
+
+    def __call__(self, request):
+        if request.user.is_authenticated:
+            cache.set(
+                f"{self.cache_key_prefix}_{request.user.id}",
+                datetime.now(),
+                self.cache_duration,
+            )
+        response = self.get_response(request)
+        return response
