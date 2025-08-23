6568c30372561d6c4a9204829e179de7881cdf9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 15:05:11 +0000

    Add package panchito-lib/0.1.0/panchito_lib-0.1.0.tar.gz

---
+++ packages/panchito-lib/panchito_lib-0.1.0.tar.gz/panchito_lib-0.1.0/panchito_lib/__init__.py
@@ -0,0 +1,16 @@
+"""
+My math library, panchito_lib.
+This is a library created for my youtube channel: PythonIsGod
+"""
+from primes import is_prime
+
+__version__ = "0.1.0"
+__author__ = "Pablo Soifer"
+__email__ = "pablojapo@proton.me"""
+__all__ = ["is_prime"]
+
+def get_version():
+    return __version__
+
+def list_functions():
+    return __all__
---
+++ packages/panchito-lib/panchito_lib-0.1.0.tar.gz/panchito_lib-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='panchito_lib',
+    version='0.1.0',
+    author='Pablo Soifer',
+    author_email='pablojapo@proton.me',
+    description='A library with some math functions',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url='https://www.youtube.com/@PythonIsGod',
+    license="MIT",
+)
