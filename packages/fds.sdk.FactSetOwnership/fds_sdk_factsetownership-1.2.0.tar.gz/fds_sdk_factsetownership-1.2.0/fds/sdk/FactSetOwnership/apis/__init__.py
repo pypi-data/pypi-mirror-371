7b3ca6fb29356add66afe2fbac13eeb3bacbd70f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 08:05:20 +0000

    Add package fds.sdk.FactSetOwnership/1.2.0/fds_sdk_factsetownership-1.2.0.tar.gz

---
+++ packages/fds.sdk.FactSetOwnership/fds_sdk_factsetownership-1.2.0.tar.gz/fds_sdk_factsetownership-1.2.0/fds/sdk/FactSetOwnership/apis/__init__.py
@@ -0,0 +1,19 @@
+
+# flake8: noqa
+
+# Import all APIs into this package.
+# If you have many APIs here with many many models used in each API this may
+# raise a `RecursionError`.
+# In order to avoid this, import only the API that you directly need like:
+#
+#   from .api.fund_holdings_api import FundHoldingsApi
+#
+# or import this package, but before doing it, use:
+#
+#   import sys
+#   sys.setrecursionlimit(n)
+
+# Import APIs into API package:
+from fds.sdk.FactSetOwnership.api.fund_holdings_api import FundHoldingsApi
+from fds.sdk.FactSetOwnership.api.security_holders_api import SecurityHoldersApi
+from fds.sdk.FactSetOwnership.api.transactions_api import TransactionsApi
---
+++ packages/fds.sdk.FactSetOwnership/fds_sdk_factsetownership-1.2.0.tar.gz/fds_sdk_factsetownership-1.2.0/fds/sdk/FactSetOwnership/models/__init__.py
@@ -0,0 +1,37 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from fds.sdk.FactSetOwnership.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from fds.sdk.FactSetOwnership.model.asset_type import AssetType
+from fds.sdk.FactSetOwnership.model.error_response import ErrorResponse
+from fds.sdk.FactSetOwnership.model.error_response_sub_errors import ErrorResponseSubErrors
+from fds.sdk.FactSetOwnership.model.frequency import Frequency
+from fds.sdk.FactSetOwnership.model.fund_holding import FundHolding
+from fds.sdk.FactSetOwnership.model.fund_holdings_request import FundHoldingsRequest
+from fds.sdk.FactSetOwnership.model.fund_holdings_response import FundHoldingsResponse
+from fds.sdk.FactSetOwnership.model.holder_type import HolderType
+from fds.sdk.FactSetOwnership.model.id_fund_holdings import IdFundHoldings
+from fds.sdk.FactSetOwnership.model.id_holders import IdHolders
+from fds.sdk.FactSetOwnership.model.id_transactions import IdTransactions
+from fds.sdk.FactSetOwnership.model.insider_transactions import InsiderTransactions
+from fds.sdk.FactSetOwnership.model.insider_transactions_request import InsiderTransactionsRequest
+from fds.sdk.FactSetOwnership.model.insider_transactions_request_data import InsiderTransactionsRequestData
+from fds.sdk.FactSetOwnership.model.insider_transactions_response import InsiderTransactionsResponse
+from fds.sdk.FactSetOwnership.model.institutional_transactions import InstitutionalTransactions
+from fds.sdk.FactSetOwnership.model.institutional_transactions_request import InstitutionalTransactionsRequest
+from fds.sdk.FactSetOwnership.model.institutional_transactions_request_data import InstitutionalTransactionsRequestData
+from fds.sdk.FactSetOwnership.model.institutional_transactions_response import InstitutionalTransactionsResponse
+from fds.sdk.FactSetOwnership.model.period_of_measure import PeriodOfMeasure
+from fds.sdk.FactSetOwnership.model.row_exclusion import RowExclusion
+from fds.sdk.FactSetOwnership.model.security_holders import SecurityHolders
+from fds.sdk.FactSetOwnership.model.security_holders_request import SecurityHoldersRequest
+from fds.sdk.FactSetOwnership.model.security_holders_response import SecurityHoldersResponse
+from fds.sdk.FactSetOwnership.model.top_n_holders import TopNHolders
+from fds.sdk.FactSetOwnership.model.transaction_type import TransactionType
