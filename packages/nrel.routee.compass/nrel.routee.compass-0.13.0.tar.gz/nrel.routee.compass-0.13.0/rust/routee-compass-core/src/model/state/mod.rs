3e2fb8c6674c4e628c2d8501ee68a9d6a599ed3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 09:05:07 +0000

    Add package nrel.routee.compass/0.13.0/nrel.routee.compass-0.13.0.tar.gz

---
+++ packages/nrel.routee.compass/nrel.routee.compass-0.13.0.tar.gz/nrel.routee.compass-0.13.0/rust/routee-compass-core/src/model/state/mod.rs
@@ -0,0 +1,17 @@
+mod custom_feature_format;
+mod input_feature;
+mod state_feature;
+mod state_model;
+mod state_model_error;
+mod state_variable;
+mod unit_codec_type;
+mod update_operation;
+
+pub use custom_feature_format::CustomFeatureFormat;
+pub use input_feature::InputFeature;
+pub use state_feature::StateFeature;
+pub use state_model::StateModel;
+pub use state_model_error::StateModelError;
+pub use state_variable::StateVariable;
+pub use unit_codec_type::UnitCodecType;
+pub use update_operation::UpdateOperation;
---
+++ packages/nrel.routee.compass/nrel.routee.compass-0.13.0.tar.gz/nrel.routee.compass-0.13.0/rust/routee-compass-core/src/model/traversal/default/mod.rs
@@ -0,0 +1,8 @@
+pub mod combined;
+pub mod custom;
+pub mod distance;
+pub mod elevation;
+pub mod fieldname;
+pub mod grade;
+pub mod speed;
+pub mod time;
