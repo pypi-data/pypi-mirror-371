81e90118e9a85596c844ad8a5c76db48cc1cc33f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 05:04:52 +0000

    Add package web-poet/0.19.2/web_poet-0.19.2-py3-none-any.whl

---
+++ packages/web-poet/web_poet-0.19.2-py3-none-any.whl/web_poet/page_inputs/__init__.py
@@ -0,0 +1,14 @@
+from .browser import BrowserHtml, BrowserResponse
+from .client import HttpClient
+from .http import (
+    HttpRequest,
+    HttpRequestBody,
+    HttpRequestHeaders,
+    HttpResponse,
+    HttpResponseBody,
+    HttpResponseHeaders,
+)
+from .page_params import PageParams
+from .response import AnyResponse
+from .stats import Stats
+from .url import RequestUrl, ResponseUrl
