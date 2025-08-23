52de10ad90f4f0b66c6ff41c35aec20635ee03ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 08:04:36 +0000

    Add package ol-openedx-chat-xblock/0.3.2/ol_openedx_chat_xblock-0.3.2.tar.gz

---
+++ packages/ol-openedx-chat-xblock/ol_openedx_chat_xblock-0.3.2.tar.gz/ol_openedx_chat_xblock-0.3.2/ol_openedx_chat_xblock/settings/devstack.py
@@ -0,0 +1,22 @@
+# noqa: INP001
+
+"""Settings to provide to edX"""
+
+
+def plugin_settings(settings):
+    """
+    Populate devstack settings
+    """
+    env_tokens = getattr(settings, "ENV_TOKENS", {})
+    settings.MIT_LEARN_AI_XBLOCK_CHAT_API_URL = env_tokens.get(
+        "MIT_LEARN_AI_XBLOCK_CHAT_API_URL", ""
+    )
+    settings.MIT_LEARN_AI_XBLOCK_TUTOR_CHAT_API_URL = env_tokens.get(
+        "MIT_LEARN_AI_XBLOCK_TUTOR_CHAT_API_URL", ""
+    )
+    settings.MIT_LEARN_AI_XBLOCK_PROBLEM_SET_LIST_URL = env_tokens.get(
+        "MIT_LEARN_AI_XBLOCK_PROBLEM_LIST_URL", ""
+    )
+    settings.MIT_LEARN_AI_XBLOCK_CHAT_API_TOKEN = env_tokens.get(
+        "MIT_LEARN_AI_XBLOCK_CHAT_API_TOKEN", ""
+    )
