78af7da120d7a60f05bbf79e2d47289e2b068dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 06:04:22 +0000

    Add package pyobs-core/1.28.1/pyobs_core-1.28.1.tar.gz

---
+++ packages/pyobs-core/pyobs_core-1.28.1.tar.gz/pyobs_core-1.28.1/pyobs/modules/camera/__init__.py
@@ -0,0 +1,12 @@
+"""
+Camera modules.
+TODO: write doc
+"""
+
+__title__ = "Cameras"
+
+from .basecamera import BaseCamera
+from .basevideo import BaseVideo
+from .basespectrograph import BaseSpectrograph
+from .dummycamera import DummyCamera
+from .dummyspectrograph import DummySpectrograph
