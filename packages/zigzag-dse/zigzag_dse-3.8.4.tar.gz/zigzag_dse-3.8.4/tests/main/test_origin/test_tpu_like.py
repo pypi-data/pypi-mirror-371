48736d3c18915030bf285371bb2199997709f4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 07:04:42 +0000

    Add package zigzag-dse/3.8.4/zigzag_dse-3.8.4.tar.gz

---
+++ packages/zigzag-dse/zigzag_dse-3.8.4.tar.gz/zigzag_dse-3.8.4/tests/main/test_origin/test_tpu_like.py
@@ -0,0 +1,33 @@
+import pytest
+
+from zigzag.api import get_hardware_performance_zigzag
+
+workloads = (
+    "zigzag/inputs/workload/resnet18.onnx",
+    "zigzag/inputs/workload/resnet18.yaml",
+)
+
+# Expected energy and latency for each workload defined above
+ens_lats = {
+    "zigzag/inputs/workload/resnet18.onnx": (1871436380.66, 3703960.5),
+    "zigzag/inputs/workload/resnet18.yaml": (2615172831.38, 5584991.0),
+}
+
+
+@pytest.fixture
+def mapping():
+    return "zigzag/inputs/mapping/tpu_like.yaml"
+
+
+@pytest.fixture
+def accelerator():
+    return "zigzag/inputs/hardware/tpu_like.yaml"
+
+
+@pytest.mark.parametrize("workload", workloads)
+def test_api(workload: str, accelerator: str, mapping: str):  # pylint: disable=W0621
+    energy, latency, _ = get_hardware_performance_zigzag(workload, accelerator, mapping)
+    (expected_energy, expected_latency) = ens_lats[workload]
+    print(f"'{workload}': ({energy}, {latency}),")
+    assert energy == pytest.approx(expected_energy)  # type: ignore
+    assert latency == pytest.approx(expected_latency)  # type: ignore
---
+++ packages/zigzag-dse/zigzag_dse-3.8.4.tar.gz/zigzag_dse-3.8.4/tests/main/test_with_mix_spatial_mapping/test_ascend_like.py
@@ -0,0 +1,39 @@
+import pytest
+
+from zigzag.api import get_hardware_performance_zigzag
+
+# Test case for when spatial_mapping is provided, while spatial_mapping_hint is not provided.
+
+workloads = (
+    "zigzag/inputs/workload/resnet18.onnx",
+    "zigzag/inputs/workload/resnet18.yaml",
+)
+
+# Expected energy and latency for each workload defined above
+ens_lats = {
+    "zigzag/inputs/workload/alexnet.onnx": (5664863934, 8479644),
+    "zigzag/inputs/workload/mobilenetv2.onnx": (920740293, 3814440),
+    "zigzag/inputs/workload/resnet18.onnx": (1811468387.52, 3119977.33),
+    "zigzag/inputs/workload/resnet18.yaml": (2349013378.88, 3718548.66),
+}
+
+
+@pytest.fixture
+def mapping():
+    return "zigzag/inputs/mapping/ascend_like_mixed.yaml"
+
+
+@pytest.fixture
+def accelerator():
+    return "zigzag/inputs/hardware/ascend_like.yaml"
+
+
+@pytest.mark.parametrize("workload", workloads)
+def test_api(workload: str, accelerator: str, mapping: str):  # pylint: disable=W0621
+    energy, latency, _ = get_hardware_performance_zigzag(
+        workload, accelerator, mapping, enable_mix_spatial_mapping=True
+    )
+    (expected_energy, expected_latency) = ens_lats[workload]
+    print(f"'{workload}': ({energy}, {latency}),")
+    assert energy == pytest.approx(expected_energy)  # type: ignore
+    assert latency == pytest.approx(expected_latency)  # type: ignore
