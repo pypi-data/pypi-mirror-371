1089bb97ff1bb6c47fbd2598275280533e28ed81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 14:04:56 +0000

    Add package canvas-sak/1.0.2/canvas_sak-1.0.2.tar.gz

---
+++ packages/canvas-sak/canvas_sak-1.0.2.tar.gz/canvas_sak-1.0.2/canvas_sak/commands/__init__.py
@@ -0,0 +1,17 @@
+__all__ = [
+    "code_similarity",
+    "collect_reference_info",
+    "download_canvas_course",
+    "download_submissions",
+    "export_letter_grade",
+    "grade_discussion",
+    "help_me_setup",
+    "list_courses",
+    "list_students",
+    "message_students",
+    "min_grade_analyzer",
+    "set_fudge_points",
+    "set_letter_grade",
+    "upload_canvas_course",
+    "quiz",
+]
