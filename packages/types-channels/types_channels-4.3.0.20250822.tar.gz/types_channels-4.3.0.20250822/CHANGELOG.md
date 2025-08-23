e4ce589bb52cbd52040bdbb4757d4c8e8322af8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 04:04:48 +0000

    Add package types-channels/4.3.0.20250822/types_channels-4.3.0.20250822.tar.gz

---
+++ packages/types-channels/types_channels-4.3.0.20250822.tar.gz/types_channels-4.3.0.20250822/CHANGELOG.md
@@ -0,0 +1,12 @@
+## 4.3.0.20250822 (2025-08-22)
+
+Add missing defaults to third-party stubs ([#14617](https://github.com/python/typeshed/pull/14617))
+
+## 4.3.0.20250730 (2025-07-30)
+
+[channels] Bump to `4.3.*` (#14490)
+
+## 4.2.0.20250712 (2025-07-12)
+
+Django channels stubs (#13939)
+
---
+++ packages/types-channels/types_channels-4.3.0.20250822.tar.gz/types_channels-4.3.0.20250822/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'channels-stubs': ['__init__.pyi', 'apps.pyi', 'auth.pyi', 'consumer.pyi', 'db.pyi', 'exceptions.pyi', 'generic/__init__.pyi', 'generic/http.pyi', 'generic/websocket.pyi', 'layers.pyi', 'management/__init__.pyi', 'management/commands/__init__.pyi', 'management/commands/runworker.pyi', 'middleware.pyi', 'routing.pyi', 'security/__init__.pyi', 'security/websocket.pyi', 'sessions.pyi', 'testing/__init__.pyi', 'testing/application.pyi', 'testing/http.pyi', 'testing/live.pyi', 'testing/websocket.pyi', 'utils.pyi', 'worker.pyi', 'METADATA.toml', 'py.typed']})
