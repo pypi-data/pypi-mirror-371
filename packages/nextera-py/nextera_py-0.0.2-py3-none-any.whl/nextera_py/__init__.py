f256ba8f513af5f96c8b4029d7826e9abb5dafe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 04:04:39 +0000

    Add package nextera-py/0.0.2/nextera_py-0.0.2-py3-none-any.whl

---
+++ packages/nextera-py/nextera_py-0.0.2-py3-none-any.whl/nextera_py/__init__.py
@@ -0,0 +1,12 @@
+
+good_bullins = None
+def lazy_import():
+    global good_bullins
+    from nextera_py import good_bullins
+modules = None
+def lazy_import():
+    global modules
+    from nextera_py import modules
+__version__ = '0.0.1'
+__author__ = 'jason'
+__all__ = ['good_builtins', 'modules']
