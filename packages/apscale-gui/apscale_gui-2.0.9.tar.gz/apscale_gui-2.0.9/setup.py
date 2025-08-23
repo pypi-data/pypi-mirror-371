02645f7896a7485527d49b2a6796aec4150aba79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 05:04:52 +0000

    Add package apscale-gui/2.0.9/apscale_gui-2.0.9.tar.gz

---
+++ packages/apscale-gui/apscale_gui-2.0.9.tar.gz/apscale_gui-2.0.9/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="apscale_gui",
+    version="2.0.9",
+    author="Till-Hendrik Macher",
+    author_email="macher@uni-trier.de",
+    description="Advanced Pipeline for Simple yet Comprehensive AnaLysEs of DNA metabarcoding data - Graphical User Interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TillMacher/apscale_gui",
+    packages=setuptools.find_packages(),
+    license = 'MIT',
+    install_requires = [
+                        'pandas',
+                        ],
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    entry_points = {
+        "console_scripts" : [
+            "apscale_gui = apscale_gui.__main__:main",
+        ]
+    },
+)
