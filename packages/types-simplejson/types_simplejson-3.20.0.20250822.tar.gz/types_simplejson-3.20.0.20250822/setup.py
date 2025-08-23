5dfbb81d8a41a3b38a2ff6bf5f5270ff23463ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 04:04:46 +0000

    Add package types-simplejson/3.20.0.20250822/types_simplejson-3.20.0.20250822.tar.gz

---
+++ packages/types-simplejson/types_simplejson-3.20.0.20250822.tar.gz/types_simplejson-3.20.0.20250822/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'simplejson-stubs': ['__init__.pyi', 'decoder.pyi', 'encoder.pyi', 'errors.pyi', 'raw_json.pyi', 'scanner.pyi', 'METADATA.toml', 'py.typed']})
