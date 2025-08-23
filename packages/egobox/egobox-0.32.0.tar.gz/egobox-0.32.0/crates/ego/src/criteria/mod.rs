789c78e69883f5f0e375254bb2f18425705ac513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 07:05:37 +0000

    Add package egobox/0.32.0/egobox-0.32.0.tar.gz

---
+++ packages/egobox/egobox-0.32.0.tar.gz/egobox-0.32.0/crates/ego/src/criteria/mod.rs
@@ -0,0 +1,49 @@
+//! Available infill criteria to be used by Egor solver
+mod ei;
+mod wb2;
+
+pub use ei::{ExpectedImprovement, LogExpectedImprovement, EI, LOG_EI};
+pub use wb2::{WB2Criterion, WB2, WB2S};
+
+use dyn_clonable::*;
+use egobox_moe::MixtureGpSurrogate;
+use ndarray::{Array1, ArrayView2};
+
+/// A trait for infill criterion which maximmum location will
+/// determine the next most promising point expected to be the
+/// optimum location of the objective function
+#[clonable]
+#[typetag::serde(tag = "type")]
+pub trait InfillCriterion: Clone + Sync {
+    /// Name of the infill criterion
+    fn name(&self) -> &'static str;
+
+    /// Criterion value at given point x with regards to given
+    /// surrogate of the objective function, the current found min
+    /// and an optional scaling factor
+    fn value(
+        &self,
+        x: &[f64],
+        obj_model: &dyn MixtureGpSurrogate,
+        fmin: f64,
+        scale: Option<f64>,
+    ) -> f64;
+
+    /// Derivatives wrt x components of the criterion value
+    fn grad(
+        &self,
+        x: &[f64],
+        obj_model: &dyn MixtureGpSurrogate,
+        fmin: f64,
+        scale: Option<f64>,
+    ) -> Array1<f64>;
+
+    /// Scaling factor computation
+    fn scaling(&self, x: &ArrayView2<f64>, obj_model: &dyn MixtureGpSurrogate, fmin: f64) -> f64;
+}
+
+impl std::fmt::Debug for dyn InfillCriterion {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{}", self.name())
+    }
+}
---
+++ packages/egobox/egobox-0.32.0.tar.gz/egobox-0.32.0/crates/ego/src/utils/mod.rs
@@ -0,0 +1,16 @@
+mod bounds;
+mod cstr_pof;
+mod find_result;
+mod hot_start;
+mod logei_helper;
+mod misc;
+mod sort_axis;
+mod start_points;
+
+pub use bounds::*;
+pub use cstr_pof::*;
+pub use find_result::*;
+pub use hot_start::*;
+pub use logei_helper::*;
+pub use misc::*;
+pub use start_points::*;
