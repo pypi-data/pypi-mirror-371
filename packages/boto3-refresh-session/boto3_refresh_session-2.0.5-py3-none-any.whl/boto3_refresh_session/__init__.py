f25a6c4917cf09b72f333da530331201fce30bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 14:05:05 +0000

    Add package boto3-refresh-session/2.0.5/boto3_refresh_session-2.0.5-py3-none-any.whl

---
+++ packages/boto3-refresh-session/boto3_refresh_session-2.0.5-py3-none-any.whl/boto3_refresh_session/__init__.py
@@ -0,0 +1,9 @@
+from .session import RefreshableSession
+
+__all__ = ["RefreshableSession"]
+__version__ = "2.0.5"
+__title__ = "boto3-refresh-session"
+__author__ = "Mike Letts"
+__maintainer__ = "Mike Letts"
+__license__ = "MIT"
+__email__ = "lettsmt@gmail.com"
