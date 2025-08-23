8ab18163141c3c61ab4f5eff19c1272a0966ce5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 08:05:59 +0000

    Add package cncf-serverless-workflow/0.1.2/cncf_serverless_workflow-0.1.2.tar.gz

---
+++ packages/cncf-serverless-workflow/cncf_serverless_workflow-0.1.2.tar.gz/cncf_serverless_workflow-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cncf-serverless-workflow",
+    version="0.1.2",
+    packages=["cncf_serverless_workflow"],
+    install_requires=["pyyaml", "jq", "jsonschema", "pytest"],
+    include_package_data=True,
+    package_data={
+        "cncf_serverless_workflow": ["workflow_schema.yaml", "workflow_example.yaml", "test_workflow_validator.py"]
+    },
+    author="Ron Vergis",
+    description="CNCF Serverless Workflow v1.0 validator and executor",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/rvergis/cncf-serverless-workflow-python-v1",
+    classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License", "Operating System :: OS Independent"],
+)
