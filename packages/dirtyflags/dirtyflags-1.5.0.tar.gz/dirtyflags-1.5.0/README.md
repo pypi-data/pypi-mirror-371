091ae34c23617f7185acda45ea4b7c628a597fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 11:05:09 +0000

    Add package dirtyflags/1.5.0/dirtyflags-1.5.0.tar.gz

---
+++ packages/dirtyflags/dirtyflags-1.5.0.tar.gz/dirtyflags-1.5.0/README.md
@@ -0,0 +1,57 @@
+# dirtyflags
+
+**dirtyflags** is a simple Python decorator that tracks when and which instance attributes have changed.
+
+```python
+>>> from dirtyflags import dirtyflag
+>>> @dirtyflag
+>>> class ChangingObject():
+>>>     def __init__(self, attr1: int, attr2: str = "Default Value"):
+>>>         self.attr1 = attr1
+>>>         self.attr2 = attr2
+>>> 
+>>>     def __str__(self):
+>>>         return (f"attr1 = {self.attr1}, attr2='{self.attr2}'")
+
+>>> # create an instance of the class
+instance_default = ChangingObject(attr1=1)
+
+>>> print(f"instance_default is: {instance_default}")
+instance_default is: attr1 = 1, attr2='Default Value'
+
+>>> # dirtyflags tracks whether a change has occurred - in this case it has not
+>>> print(f"Has this instance changed = {instance_default.is_dirty()}")
+Has this instance changed = False
+
+>>> # now change the value of an attribute
+>>> instance_default.attr1 = 234
+>>> # now dirty flag indicates the class has changed - and tells you what has changed
+>>> print(f"Has this instance changed = {instance_default.is_dirty()}")
+>>> print(f"The attribute(s) that have changed: {instance_default.dirty_attrs()}")
+Has this instance changed = True
+The attribute(s) that have changed: {'attr1'}
+
+>>> # dirtyflags even tracks changes when using __setter__
+>>> instance_default.__setattr__('attr2', 'changed the default')
+>>> print(f"Has this instance changed = {instance_default.is_dirty()}")
+>>> print(f"The attribute(s) that have changed: {instance_default.dirty_attrs()}")
+Has this instance changed = True
+The attribute(s) that have changed: {'attr2', 'attr1'}
+```
+
+## Installing dirtyflags
+
+
+```console
+pip install dirtyflags
+```
+dirtyflags officially supports Python 3.8+
+
+## Supported Features and Best Practices
+- Sinmply use the `@dirtyflag` decorator
+- Supports attributes of any datatype, built-in or custom
+- Works with Python dataclasses
+- Nested classes should have the '@dirtyflag' decorator applied as well
+
+[![linting: pylint](https://img.shields.io/badge/linting-pylint-yellowgreen)](https://github.com/PyCQA/pylint)
+---
---
+++ packages/dirtyflags/dirtyflags-1.5.0.tar.gz/dirtyflags-1.5.0/tests/test_dirtyflags.py
@@ -0,0 +1,98 @@
+#  SPDX-FileCopyrightText: Copyright (c) 2022-2024. James J. Johnson <james.x.johnson@gmail.com>
+#  SPDX-License-Identifier: BSD-3-Clause
+from dirtyflags.dirtyflags import dirtyflag
+
+
+def test_basic_object():
+    # create object
+    @dirtyflag
+    class BasicObj():
+        def __init__(self, aint, bfloat, cstr, dlist, fdict):
+            self.a = aint
+            self.b = bfloat
+            self.c = cstr
+            self.d = dlist
+            self.f = fdict
+
+    # test things
+    bo = BasicObj(5, 3.14, 'Test String', [1, 2], {'key1': 'val1'})
+    assert not bo.is_dirty
+
+
+def test_basic_object_isdirty():
+    # create object
+    @dirtyflag
+    class BasicObj():
+        def __init__(self, aint, bfloat, cstr, dlist, fdict):
+            self.a = aint
+            self.b = bfloat
+            self.c = cstr
+            self.d = dlist
+            self.f = fdict
+
+    # test things
+    bo = BasicObj(5, 3.14, 'Test String', [1, 2], {'key1': 'val1'})
+    bo.a = 99
+    bo.b = 88.88
+    assert bo.is_dirty
+
+
+def test_basic_object_dirtyattrs():
+    # create object
+    @dirtyflag
+    class BasicObj():
+        def __init__(self, aint, bfloat, cstr, dlist, fdict):
+            self.a = aint
+            self.b = bfloat
+            self.c = cstr
+            self.d = dlist
+            self.f = fdict
+
+    # test things
+    bo = BasicObj(5, 3.14, 'Test String', [1, 2], {'key1': 'val1'})
+    bo.a = 12
+    bo.c = "Changed String"
+    assert (
+            'a' in bo.dirty_attrs()
+            and 'b' not in bo.dirty_attrs()
+            and 'c' in bo.dirty_attrs()
+    )
+
+
+def test_list_and_dict_object_dirtyattrs():
+    # create object
+    @dirtyflag
+    class BasicObj():
+        def __init__(self, aint, bfloat, cstr, dlist, fdict):
+            self.a = aint
+            self.b = bfloat
+            self.c = cstr
+            self.d = dlist
+            self.f = fdict
+
+    # test things
+    bo = BasicObj(5, 3.14, 'Test String', [1, 2], {'key1': 'val1'})
+    bo.d.append(3)
+    bo.f['key1'] = "Changed Value 1"
+    assert (
+            'a' not in bo.dirty_attrs()
+            and 'b' not in bo.dirty_attrs()
+            and 'c' not in bo.dirty_attrs()
+            and 'd' in bo.dirty_attrs()
+            and 'f' in bo.dirty_attrs()
+    )
+
+
+def test_list_of_instances_for_scope():
+    @dirtyflag
+    class BasicObj():
+        def __init__(self, aint, bfloat):
+            self.a = aint
+            self.b = bfloat
+
+    # test this case
+    lst_objects = [BasicObj(a, a) for a in [1, 2]]
+    assert not lst_objects[1].is_dirty
+    lst_objects[1].a = 11
+    assert lst_objects[1].is_dirty
+    assert 'a' in lst_objects[1].dirty_attrs()
