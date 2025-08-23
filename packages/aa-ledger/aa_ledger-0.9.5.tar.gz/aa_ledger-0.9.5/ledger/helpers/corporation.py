c4c57439908c4444411e5de63818ca8acacbf1e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 05:04:50 +0000

    Add package aa-ledger/0.9.5/aa_ledger-0.9.5.tar.gz

---
+++ packages/aa-ledger/aa_ledger-0.9.5.tar.gz/aa_ledger-0.9.5/ledger/helpers/corporation.py
@@ -0,0 +1,404 @@
+"""PvE Views"""
+
+# Standard Library
+import json
+from decimal import Decimal
+
+# Django
+from django.core.handlers.wsgi import WSGIRequest
+from django.db.models import DecimalField, Q, QuerySet, Sum
+from django.utils.translation import gettext as _
+
+# Alliance Auth
+from allianceauth.authentication.models import UserProfile
+from allianceauth.eveonline.models import EveCharacter
+from allianceauth.services.hooks import get_extension_logger
+
+# Alliance Auth (External Libs)
+from app_utils.logging import LoggerAddTag
+
+# AA Ledger
+from ledger import __title__
+from ledger.helpers.core import LedgerCore, LedgerEntity
+from ledger.helpers.ref_type import RefTypeManager
+from ledger.models.corporationaudit import (
+    CorporationAudit,
+    CorporationWalletJournalEntry,
+)
+
+logger = LoggerAddTag(get_extension_logger(__name__), __title__)
+
+NPC_ENTITIES = [
+    1000125,  # Concord Bounties (Bounty Prizes, ESS
+    1000132,  # Secure Commerce Commission (Market Fees)
+    1000413,  # Air Laboratories (Daily Login Rewards, etc.)
+]
+
+
+class CorporationData(LedgerCore):
+    """Class to hold character data for the ledger."""
+
+    # pylint: disable=too-many-positional-arguments
+    def __init__(
+        self,
+        request: WSGIRequest,
+        corporation: CorporationAudit,
+        year=None,
+        month=None,
+        day=None,
+    ):
+        LedgerCore.__init__(self, year, month, day)
+        self.request = request
+        self.corporation = corporation
+        self.create_account_information()
+
+    def create_account_information(self):
+        """Create the account information for the corporation."""
+        # Get all Auth Accounts with main character
+        self.accounts = (
+            UserProfile.objects.filter(
+                main_character__isnull=False,
+            )
+            .prefetch_related(
+                "user__profile__main_character",
+            )
+            .order_by(
+                "user__profile__main_character__character_name",
+            )
+        )
+        self.account_char_ids = self.get_all_account_ids(accounts=self.accounts)
+
+    def setup_ledger(self, entity: LedgerEntity = None):
+        """Setup the Ledger Data for the Corporation."""
+
+        if entity is not None:
+            if (
+                self.request.GET.get("all", False)
+                and entity.entity_id == self.corporation.corporation.corporation_id
+            ):
+                self.journal = CorporationWalletJournalEntry.objects.filter(
+                    self.filter_date,
+                    division__corporation=self.corporation,
+                ).exclude(  # Filter by date and division
+                    first_party_id=self.corporation.corporation.corporation_id,
+                    second_party_id=self.corporation.corporation.corporation_id,
+                )  # exclude Transaction between the corporation itself
+            else:
+                character_ids = entity.get_alts_ids_or_self()
+                self.journal = (
+                    CorporationWalletJournalEntry.objects.filter(
+                        self.filter_date,
+                        division__corporation=self.corporation,
+                    )  # Filter by date and division
+                    .filter(
+                        Q(first_party_id__in=character_ids)
+                        | Q(second_party_id__in=character_ids)
+                    )  # Filter only needed Character IDs
+                    .exclude(
+                        first_party_id=self.corporation.corporation.corporation_id,
+                        second_party_id=self.corporation.corporation.corporation_id,
+                    )  # exclude Transaction between the corporation itself
+                )
+
+                if entity.entity_id == self.corporation.corporation.corporation_id:
+                    self.journal = self.journal.filter(
+                        Q(first_party_id__in=NPC_ENTITIES)
+                        | Q(second_party_id__in=NPC_ENTITIES)
+                    )
+
+                # If the entity is a corporation or alliance, we need to exclude the accounts Character IDs
+                # from the journal to prevent double counting
+                if entity.type in ["alliance", "corporation"]:
+                    exclude_ids = self.get_all_account_ids(
+                        accounts=self.accounts
+                    ) - set(character_ids)
+                    self.journal = self.journal.exclude(
+                        Q(first_party_id__in=exclude_ids)
+                        | Q(second_party_id__in=exclude_ids)
+                    )
+            # Get All Entities from the Journal
+            self.entities = set(
+                self.journal.values_list("second_party_id", flat=True)
+            ) | set(self.journal.values_list("first_party_id", flat=True))
+        else:
+            self.journal = CorporationWalletJournalEntry.objects.filter(
+                self.filter_date, division__corporation=self.corporation
+            ).exclude(  # Filter by date and division
+                first_party_id=self.corporation.corporation.corporation_id,
+                second_party_id=self.corporation.corporation.corporation_id,
+            )  # exclude Transaction between the corporation itself
+
+            # Evaluate the existing years for the view
+            self.existing_years = (
+                CorporationWalletJournalEntry.objects.filter(
+                    division__corporation=self.corporation
+                )
+                .exclude(date__year__isnull=True)
+                .values_list("date__year", flat=True)
+                .order_by("-date__year")
+                .distinct()
+            )
+
+            # Get All Entities from the Journal
+            self.entities = set(
+                self.journal.values_list("second_party_id", flat=True)
+            ) | set(self.journal.values_list("first_party_id", flat=True))
+
+    def get_all_account_ids(self, accounts: list[QuerySet]) -> set:
+        """Get all account Character IDs for the corporation."""
+        account_character_ids = set()
+        for account in accounts:
+            alts = account.user.character_ownerships.all()
+            account_character_ids.update(
+                alts.values_list("character__character_id", flat=True)
+            )
+        return account_character_ids
+
+    def create_entity_data(
+        self,
+        entity: LedgerEntity,
+        alts: EveCharacter = None,
+    ) -> dict:
+        """Create the URL for entity details based on the view type."""
+        ids = (
+            list(alts.values_list("character__character_id", flat=True))
+            if alts is not None
+            else [entity.entity_id]
+        )
+
+        # Create Alts Dictionary
+        alts_dict = {}
+        if alts is not None:
+            for alt in alts:
+                alts_dict[alt.character.character_id] = alt.character.character_name
+
+            # Remove the main character from the alts dictionary only one entry
+            if len(alts_dict) == 1:
+                alts_dict.pop(entity.entity_id, None)
+
+        used_pks = set()
+        bounty = Decimal(0)
+        ess = Decimal(0)
+        miscellaneous = Decimal(0)
+        costs = Decimal(0)
+
+        for pk, rows in list(self.entries.items()):
+            for row in rows:
+                if row["first_party_id"] in ids or row["second_party_id"] in ids:
+                    if RefTypeManager.special_cases(
+                        row, ids=ids, account_char_ids=self.account_char_ids
+                    ):
+                        continue
+                    bounty += row.get("bounty") or Decimal(0)
+                    ess += row.get("ess") or Decimal(0)
+                    miscellaneous += row.get("miscellaneous") or Decimal(0)
+                    costs += row.get("costs") or Decimal(0)
+                    used_pks.add(pk)
+
+        # Remove Used Pks from Entries
+        # This is to prevent the entries from being used in the future
+        for pk in used_pks:
+            self.entries.pop(pk, None)
+
+        total = sum([bounty, ess, miscellaneous, costs])
+
+        if total == 0:
+            return None
+
+        char_data = {
+            "entity": entity,
+            "alts": alts_dict,
+            "ledger": {
+                "bounty": bounty,
+                "ess": ess,
+                "miscellaneous": miscellaneous,
+                "costs": costs,
+                "total": total,
+            },
+            "type": entity.type,
+        }
+
+        # Create the chord data for the billboard
+        self.billboard.chord_add_data(
+            chord_from=entity.entity_name,
+            chord_to=_("Ratting (Wallet)"),
+            value=bounty,
+        )
+        self.billboard.chord_add_data(
+            chord_from=entity.entity_name,
+            chord_to=_("ESS (Wallet)"),
+            value=ess,
+        )
+        self.billboard.chord_add_data(
+            chord_from=entity.entity_name,
+            chord_to=_("Costs (Wallet)"),
+            value=abs(costs),
+        )
+        self.billboard.chord_add_data(
+            chord_from=entity.entity_name,
+            chord_to=_("Miscellaneous (Wallet)"),
+            value=abs(miscellaneous),
+        )
+
+        return char_data
+
+    def generate_ledger_data(self) -> dict:
+        """Generate the ledger data for the character and its alts."""
+        self.setup_ledger()
+
+        ledger = []
+        finished_entities = set()
+
+        journal = self.journal.values(
+            "first_party_id", "second_party_id", "pk", "ref_type"
+        ).annotate(
+            bounty=Sum(
+                "amount",
+                filter=Q(ref_type__in=RefTypeManager.BOUNTY_PRIZES),
+                output_field=DecimalField(),
+            ),
+            ess=Sum(
+                "amount",
+                filter=Q(ref_type__in=RefTypeManager.ESS_TRANSFER),
+                output_field=DecimalField(),
+            ),
+            costs=Sum(
+                "amount",
+                filter=Q(ref_type__in=RefTypeManager.all_ref_types(), amount__lt=0),
+                output_field=DecimalField(),
+            ),
+            miscellaneous=Sum(
+                "amount",
+                filter=Q(ref_type__in=RefTypeManager.all_ref_types(), amount__gt=0),
+                output_field=DecimalField(),
+            ),
+        )
+
+        self.entries = {}
+        for row in journal:
+            self.entries.setdefault(row["pk"], []).append(row)
+
+        for account in self.accounts:
+            alts = account.user.character_ownerships.all()
+
+            existing_alts = set(
+                alts.values_list("character__character_id", flat=True)
+            ).intersection(self.entities)
+
+            alts = alts.filter(character__character_id__in=existing_alts)
+
+            if not existing_alts:
+                continue
+
+            details_url = self.create_url(
+                viewname="corporation_details",
+                corporation_id=self.corporation.corporation.corporation_id,
+                entity_id=account.main_character.character_id,
+            )
+
+            # Create the LedgerEntity object for the character
+            entity_obj = LedgerEntity(
+                account.main_character.character_id,
+                character_obj=account.main_character,
+                details_url=details_url,
+            )
+
+            char_data = self.create_entity_data(
+                entity=entity_obj,
+                alts=alts,
+            )
+
+            if char_data is None:
+                continue
+
+            ledger.append(char_data)
+            finished_entities.update(existing_alts)
+
+        remaining_entities = self.entities - finished_entities
+        if remaining_entities:
+            for entity_id in remaining_entities:
+                # Skip NPC Entities like CONCORD, AIR Laboratories, etc.
+                if entity_id in NPC_ENTITIES:
+                    continue
+
+                # Skip the corporation itself to ensure it will be added last in #343
+                if entity_id == self.corporation.corporation.corporation_id:
+                    continue
+
+                # Create Details URL for the entity
+                details_url = self.create_url(
+                    viewname="corporation_details",
+                    corporation_id=self.corporation.corporation.corporation_id,
+                    entity_id=entity_id,
+                )
+
+                # Create the LedgerEntity object for the entity
+                entity_obj = LedgerEntity(
+                    entity_id,
+                    details_url=details_url,
+                )
+
+                char_data = self.create_entity_data(
+                    entity=entity_obj,
+                )
+
+                if char_data is None:
+                    continue
+
+                ledger.append(char_data)
+                finished_entities.add(entity_id)
+
+        # Create the last entity data for the corporation itself
+        corporation_entity = LedgerEntity(
+            self.corporation.corporation.corporation_id,
+            corporation_obj=self.corporation.corporation,
+            details_url=self.create_url(
+                viewname="corporation_details",
+                corporation_id=self.corporation.corporation.corporation_id,
+                entity_id=self.corporation.corporation.corporation_id,
+            ),
+        )
+        char_data = self.create_entity_data(
+            entity=corporation_entity,
+        )
+
+        if char_data is not None:
+            ledger.append(char_data)
+
+        # Create the billboard data
+        self.create_rattingbar(list(finished_entities), is_char_ledger=False)
+        # Prevent overflow in the chord data
+        self.billboard.chord_handle_overflow()
+
+        context = {
+            "title": f"Corporation Ledger - {self.corporation.corporation.corporation_name}",
+            "corporation_id": self.corporation.corporation.corporation_id,
+            "billboard": json.dumps(self.billboard.dict.asdict()),
+            "ledger": ledger,
+            "years": list(self.existing_years),
+            "totals": self._calculate_totals(ledger),
+            "view": self.create_view_data(
+                viewname="corporation_details",
+                corporation_id=self.corporation.corporation.corporation_id,
+                entity_id=self.corporation.corporation.corporation_id,
+            ),
+        }
+        return context
+
+    def create_rattingbar(
+        self, entities_ids: list = None, is_char_ledger: bool = False
+    ):
+        """Create the ratting bar for the view."""
+        if not entities_ids:
+            return
+
+        rattingbar_timeline = self.billboard.create_timeline(self.journal)
+        rattingbar = (
+            rattingbar_timeline.annotate_bounty_income()
+            .annotate_ess_income()
+            .annotate_miscellaneous()
+        )
+        self.billboard.create_or_update_results(
+            rattingbar, is_char_ledger=is_char_ledger
+        )
+        self.billboard.create_ratting_bar()
---
+++ packages/aa-ledger/aa_ledger-0.9.5.tar.gz/aa_ledger-0.9.5/ledger/managers/character_mining_manager.py
@@ -0,0 +1,192 @@
+# Standard Library
+from collections import defaultdict
+from typing import TYPE_CHECKING
+
+# Django
+from django.db import models, transaction
+from django.db.models import DecimalField, ExpressionWrapper, F, Q, Sum, Value
+from django.db.models.functions import Coalesce, Round
+from django.utils import timezone
+
+# Alliance Auth
+from allianceauth.services.hooks import get_extension_logger
+
+# Alliance Auth (External Libs)
+from app_utils.logging import LoggerAddTag
+from eveuniverse.models import EveSolarSystem, EveType
+
+# AA Ledger
+from ledger import __title__
+from ledger.decorators import log_timing
+from ledger.helpers.etag import etag_results
+from ledger.providers import esi
+
+if TYPE_CHECKING:
+    # AA Ledger
+    from ledger.models.characteraudit import (
+        CharacterAudit,
+    )
+    from ledger.models.general import UpdateSectionResult
+
+logger = LoggerAddTag(get_extension_logger(__name__), __title__)
+
+
+class CharacterMiningLedgerEntryQueryset(models.QuerySet):
+    def annotate_pricing(self) -> models.QuerySet:
+        """Annotate price and total columns."""
+        return self.annotate(price=F("type__market_price__average_price")).annotate(
+            total=ExpressionWrapper(
+                F("type__market_price__average_price") * F("quantity"),
+                output_field=models.DecimalField(),
+            ),
+        )
+
+    def annotate_mining(self) -> models.QuerySet:
+        """Annotate mining columns."""
+        return (
+            self.annotate_pricing()
+            .values(
+                "character__eve_character__character_id",
+                "character__eve_character__character_name",
+            )
+            .annotate(
+                total_amount=Round(
+                    Coalesce(
+                        Sum(F("total")),
+                        Value(0),
+                        output_field=DecimalField(),
+                    ),
+                    precision=2,
+                )
+            )
+        )
+
+    def aggregate_mining(self):
+        """Aggregate mining amounts."""
+        qs = self.annotate_pricing()
+        return qs.aggregate(
+            total_amount=Round(
+                Coalesce(
+                    Sum(F("total")),
+                    Value(0),
+                    output_field=DecimalField(),
+                ),
+                precision=2,
+            )
+        )["total_amount"]
+
+    def aggregate_amounts_information_modal(
+        self, amounts: defaultdict, chars_list: list, filter_date: timezone.datetime
+    ) -> dict:
+        """Generate data template for the ledger character information view."""
+        qs = self.filter(Q(character__eve_character__character_id__in=chars_list))
+        qs = qs.annotate_pricing()
+        qs = qs.aggregate(
+            total_amount=Round(
+                Coalesce(
+                    Sum(F("total"), filter=Q(date__year=filter_date.year)),
+                    Value(0),
+                    output_field=DecimalField(),
+                ),
+                precision=2,
+            ),
+            total_amount_day=Round(
+                Coalesce(
+                    Sum(F("total"), filter=Q(date__day=filter_date.day)),
+                    Value(0),
+                    output_field=DecimalField(),
+                ),
+                precision=2,
+            ),
+        )
+
+        amounts["mining"]["total_amount"] = qs["total_amount"]
+        amounts["mining"]["total_amount_day"] = qs["total_amount_day"]
+
+        return amounts
+
+    def annotate_billboard(self, chars_list: list) -> models.QuerySet:
+        """Annotate billboard columns."""
+        qs = self.filter(Q(character__eve_character__character_id__in=chars_list))
+        return qs.annotate(
+            total_amount=Round(
+                Coalesce(
+                    Sum(F("total")),
+                    Value(0),
+                    output_field=DecimalField(),
+                ),
+                precision=2,
+            )
+        )
+
+
+class CharacterMiningLedgerEntryManagerBase(models.Manager):
+    @log_timing(logger)
+    def update_or_create_esi(
+        self, character: "CharacterAudit", force_refresh: bool = False
+    ) -> "UpdateSectionResult":
+        """Update or Create a mining ledger entry from ESI data."""
+        return character.update_section_if_changed(
+            section=character.UpdateSection.MINING_LEDGER,
+            fetch_func=self._fetch_esi_data,
+            force_refresh=force_refresh,
+        )
+
+    def _fetch_esi_data(
+        self, character: "CharacterAudit", force_refresh: bool = False
+    ) -> None:
+        """Fetch mining ledger entries from ESI data."""
+        req_scopes = ["esi-industry.read_character_mining.v1"]
+
+        token = character.get_token(scopes=req_scopes)
+        mining_obj = esi.client.Industry.get_characters_character_id_mining(
+            character_id=character.eve_character.character_id
+        )
+        mining_items = etag_results(mining_obj, token, force_refresh=force_refresh)
+        self._update_or_create_objs(character, mining_items)
+
+    @transaction.atomic()
+    def _update_or_create_objs(self, character: "CharacterAudit", objs: list) -> None:
+        """Update or Create mining ledger entries from objs data."""
+        existings_pks = set(
+            self.filter(
+                character=character,
+                date__gte=timezone.now() - timezone.timedelta(days=30),
+            ).values_list("id", flat=True)
+        )
+        type_ids = set()
+        system_ids = set()
+        new_events = []
+        old_events = []
+
+        for entry in objs:
+            type_ids.add(entry.get("type_id"))
+            system_ids.add(entry.get("solar_system_id"))
+            pk = self.model.create_primary_key(character.pk, entry)
+            _e = self.model(
+                character=character,
+                id=pk,
+                date=entry.get("date"),
+                type_id=entry.get("type_id"),
+                system_id=entry.get("solar_system_id"),
+                quantity=entry.get("quantity"),
+            )
+            if pk in existings_pks:
+                old_events.append(_e)
+            else:
+                new_events.append(_e)
+
+        # Ensure both EveType and EveSolarSystem objects exist before creating mining entries
+        EveType.objects.bulk_get_or_create_esi(ids=list(type_ids))
+        EveSolarSystem.objects.bulk_get_or_create_esi(ids=list(system_ids))
+
+        if new_events:
+            self.bulk_create(new_events, ignore_conflicts=True)
+
+        if old_events:
+            self.bulk_update(old_events, fields=["quantity"])
+
+
+CharacterMiningLedgerEntryManager = CharacterMiningLedgerEntryManagerBase.from_queryset(
+    CharacterMiningLedgerEntryQueryset
+)
