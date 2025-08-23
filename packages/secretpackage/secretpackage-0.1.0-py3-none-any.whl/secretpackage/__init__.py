25a85a303a60bf06e17ea3316024a89234cf3ff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 14:05:09 +0000

    Add package secretpackage/0.1.0/secretpackage-0.1.0-py3-none-any.whl

---
+++ packages/secretpackage/secretpackage-0.1.0-py3-none-any.whl/secretpackage/__init__.py
@@ -0,0 +1,10 @@
+from .math_utils import add, multiply, power
+from .string_utils import reverse_string, capitalize_words, remove_spaces
+from .data_utils import process_list, find_max_min, calculate_average
+
+__version__ = "0.1.0"
+__all__ = [
+    'add', 'multiply', 'power',
+    'reverse_string', 'capitalize_words', 'remove_spaces',
+    'process_list', 'find_max_min', 'calculate_average'
+]
