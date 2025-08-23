879fb45c6a467399ad0970c05b63728fd2ea13e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 03:18:41 +0000

    Add package dycw-utilities/0.165.1/dycw_utilities-0.165.1.tar.gz

---
+++ packages/dycw-utilities/dycw_utilities-0.165.1.tar.gz/dycw_utilities-0.165.1/src/tests/test_typing_funcs/no_future.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from typing import Literal
+
+TrueOrFalseNoFutureLit = Literal["true", "false"]
+type TrueOrFalseNoFutureTypeLit = Literal["true", "false"]
+
+
+@dataclass(order=True, unsafe_hash=True, kw_only=True)
+class DataClassNoFutureInt:
+    int_: int
+
+
+@dataclass(order=True, unsafe_hash=True, kw_only=True)
+class DataClassNoFutureIntDefault:
+    int_: int = 0
+
+
+@dataclass(kw_only=True)
+class DataClassNoFutureNestedInnerFirstInner:
+    int_: int
+
+
+@dataclass(kw_only=True)
+class DataClassNoFutureNestedInnerFirstOuter:
+    inner: DataClassNoFutureNestedInnerFirstInner
+
+
+@dataclass(kw_only=True)
+class DataClassNoFutureNestedOuterFirstOuter:
+    inner: "DataClassNoFutureNestedOuterFirstInner"
+
+
+@dataclass(kw_only=True)
+class DataClassNoFutureNestedOuterFirstInner:
+    int_: int
