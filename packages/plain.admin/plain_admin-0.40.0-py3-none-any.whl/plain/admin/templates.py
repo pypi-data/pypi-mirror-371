97fb341dde65f39d53d0968b70e22ea3c783a15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 11:04:37 +0000

    Add package plain.admin/0.40.0/plain_admin-0.40.0-py3-none-any.whl

---
+++ packages/plain.admin/plain_admin-0.40.0-py3-none-any.whl/plain/admin/templates.py
@@ -0,0 +1,26 @@
+from plain.runtime import settings
+from plain.templates import register_template_extension, register_template_filter
+from plain.templates.jinja.extensions import InclusionTagExtension
+from plain.utils.module_loading import import_string
+
+from .views.registry import registry
+
+
+@register_template_extension
+class ToolbarExtension(InclusionTagExtension):
+    tags = {"toolbar"}
+    template_name = "toolbar/toolbar.html"
+
+    def get_context(self, context, *args, **kwargs):
+        if isinstance(settings.ADMIN_TOOLBAR_CLASS, str):
+            cls = import_string(settings.ADMIN_TOOLBAR_CLASS)
+        else:
+            cls = settings.ADMIN_TOOLBAR_CLASS
+        context.vars["toolbar"] = cls(request=context["request"])
+        context.vars["app_name"] = settings.APP_NAME
+        return context
+
+
+@register_template_filter
+def get_admin_model_detail_url(obj):
+    return registry.get_model_detail_url(obj)
