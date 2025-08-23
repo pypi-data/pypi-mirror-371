52f223f60082ad0007e17fd27e774156fc60c2ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 07:04:45 +0000

    Add package tai-api/0.1.12/tai_api-0.1.12.tar.gz

---
+++ packages/tai-api/tai_api-0.1.12.tar.gz/tai_api-0.1.12/tai_api/cli/commands/__init__.py
@@ -0,0 +1,11 @@
+from .cmd_generate import generate
+from .cmd_init import init
+from .cmd_dev import dev
+from .cmd_auth import set_auth
+
+__all__ = [
+    "generate",
+    "init",
+    "dev",
+    "set_auth"
+]
