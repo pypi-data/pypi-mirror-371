383eae7c1f4b49ae49534b856f2a9de192b74ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 04:04:42 +0000

    Add package browsr/1.22.1/browsr-1.22.1.tar.gz

---
+++ packages/browsr/browsr-1.22.1.tar.gz/browsr-1.22.1/.releaserc.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const fs = require("fs");
+
+const semantic_release_dir = path.resolve(
+    __dirname,
+    ".github/semantic_release",
+);
+const release_note_path = path.join(semantic_release_dir, "release_notes.hbs");
+const release_note_template = fs.readFileSync(release_note_path, "utf-8");
+
+module.exports = {
+    branches: [
+        "main",
+        "master",
+        "next",
+        "next-major",
+        "+([0-9])?(.{+([0-9]),x}).x",
+        {
+            name: "beta",
+            prerelease: true,
+        },
+        {
+            name: "alpha",
+            prerelease: true,
+        },
+    ],
+    plugins: [
+        [
+            "semantic-release-gitmoji",
+            {
+                releaseNotes: {
+                    template: release_note_template,
+                },
+            },
+        ],
+        [
+            "@semantic-release/exec",
+            {
+                prepareCmd: "uv version ${nextRelease.version} && uv build",
+            },
+        ],
+        [
+            "@semantic-release/git",
+            {
+                assets: ["pyproject.toml", "uv.lock"],
+                message:
+                    "🔖 browsr ${nextRelease.version}\n\n${nextRelease.notes}\n[skip ci]",
+            },
+        ],
+        [
+            "@semantic-release/github",
+            {
+                assets: [
+                    {
+                        path: "dist/*.whl",
+                    },
+                    {
+                        path: "dist/*.tar.gz",
+                    },
+                ],
+            },
+        ],
+    ],
+};
---
+++ packages/browsr/browsr-1.22.1.tar.gz/browsr-1.22.1/browsr/__main__.py
@@ -0,0 +1,8 @@
+"""
+browsr __main__ hook
+"""
+
+from browsr.cli import browsr
+
+if __name__ == "__main__":
+    browsr()
