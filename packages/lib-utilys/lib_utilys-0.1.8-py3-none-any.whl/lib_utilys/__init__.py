5318d7b7c520d10082df237a912ab57b6c844d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 05:05:49 +0000

    Add package lib-utilys/0.1.8/lib_utilys-0.1.8-py3-none-any.whl

---
+++ packages/lib-utilys/lib_utilys-0.1.8-py3-none-any.whl/lib_utilys/__init__.py
@@ -0,0 +1,9 @@
+from .utils import clean_special_characters, read_json, write_json, extract_business, clean_xml
+
+__all__ = [
+    'clean_special_characters',
+    'read_json',
+    'write_json',
+    'extract_business',
+    'clean_xml',
+]
