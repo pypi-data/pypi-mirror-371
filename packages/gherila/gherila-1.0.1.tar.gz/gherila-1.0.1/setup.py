963cba83bfc4062a48f0a9b62f3372de993e330a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 05:04:56 +0000

    Add package gherila/1.0.1/gherila-1.0.1.tar.gz

---
+++ packages/gherila/gherila-1.0.1.tar.gz/gherila-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+  name='gherila',
+  author='s4nica',
+  url='https://github.com/br4nch/gherila',
+  version='1.0.1',
+  license='MIT',
+  description='An async package destioned to fetch information from different platforms',
+  python_requires='>=3.8.0',
+  install_requires=['munch', 'aiohttp', 'pydantic'],
+  packages=['gherila'],
+  classifiers=[
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+  ],
+  include_package_data=True,
+)
