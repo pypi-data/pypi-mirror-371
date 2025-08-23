358e149d01034b9780094ac1cc11f7dc41247fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 15:05:11 +0000

    Add package agentkit-llm/0.1.9/agentkit_llm-0.1.9.tar.gz

---
+++ packages/agentkit-llm/agentkit_llm-0.1.9.tar.gz/agentkit_llm-0.1.9/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+import pkg_resources
+import pathlib
+
+PKG_NAME = "agentkit-llm"
+VERSION = "0.1.9"
+EXTRAS = {
+    "logging": ["wandb"],
+    "proprietary": ["wandb", "openai", "anthropic", "tiktoken"],
+    "all": ["wandb", "openai", "anthropic", "tiktoken", "llama"],
+}
+
+setuptools.setup(
+    name=PKG_NAME,
+    version=VERSION,
+    author="AgentKit Team",
+    description='A LLM prompting framework for LLM agents',
+    url="https://github.com/rhyswynn/AgentKit",
+    long_description=pathlib.Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_namespace_packages('src'),
+    package_dir={'': 'src'},
+    entry_points={'console_scripts': ['agentkit=agentkit.run_gui:main']},
+    install_requires = ['colorama', 'numpy','ruamel.yaml'],
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license="CC-BY-4.0-Attribution",
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
---
+++ packages/agentkit-llm/agentkit_llm-0.1.9.tar.gz/agentkit_llm-0.1.9/src/agentkit/after_query.py
@@ -0,0 +1,81 @@
+from .utils import extract_json_objects
+from .exceptions import AfterQueryError
+
+class BaseAfterQuery:
+    """Base class for after query postprocessing.
+
+    Each after query instance performs postprocessing after the LLM query.
+
+    Attributes:
+        node (SimpleDBNode): The Node object that this after-query instance is associated with.
+    """
+
+    def __init__(self):
+        self.node = None
+    
+    def set_node(self, node):
+        """Set the node for the after query.
+
+        This function will be called automatically upon initialization of a SimpleDBNode
+
+        Args:
+            node (SimpleDBNode): Node object.
+        """
+        self.node = node
+        return self
+    
+    def post_process(self):
+        """Post process the result of the LLM query.
+        
+        This method can be overridden by the derived class to perform postprocessing.
+        """
+        pass
+
+    def __call__(self):
+        if self.node is None:
+            raise Exception("Node is not set")
+
+        self.post_process()
+
+class JsonAfterQuery(BaseAfterQuery):
+    """Class for after query postprocessing of Json objects.
+
+    Attributes:
+        type (type): Type of the Json object.
+        required_keys (list): List of required keys in the Json object.
+        length (int): Required length of the Json object.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.type = dict
+        self.required_keys = []
+        self.length = None
+
+    def parse_json(self):
+        """Parse the result of the LLM query.
+
+        This method parses self.node.result (str) and returns the parsed Json object.
+        The method checks the length of the parsed Json object, and if the Json object contains the required keys.
+
+        Raises:
+            AfterQueryError: If the answer is invalid.
+
+        Returns:
+            parsed_answer (list of dict): List of Json objects, where the last Json object is the answer.
+        """
+        parsed_answer, error_msg = extract_json_objects(self.node.result)
+        if parsed_answer is None:
+            raise AfterQueryError("Failed to parse answer", error_msg)
+        elif parsed_answer[-1] is None:
+            raise AfterQueryError("No answer", "Invalid Json: It seems that the last Json object in the output above is invalid.")
+        elif type(parsed_answer[-1]) != self.type:
+            raise AfterQueryError("Invalid answer", "Invalid Type: Expecting the last Json object to be {}, got {} instead.".format(self.type, type(parsed_answer[-1])))
+        if self.length is not None and len(parsed_answer[-1]) != self.length:
+            raise AfterQueryError("Invalid answer", "Expecting length {}, got {} instead.".format(self.length, len(parsed_answer[-1])))
+
+        for k in self.required_keys:
+            if k not in parsed_answer[-1].keys():
+                raise AfterQueryError("Invalid answer", "Expecting '{}' in the keys.".format(k))
+        
+        return parsed_answer
