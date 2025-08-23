84b932c73edeb73704a7befbd389a5fd0cab20a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 15:05:05 +0000

    Add package jpreprocess-mod/0.1.7/jpreprocess_mod-0.1.7-cp312-cp312-win32.whl

---
+++ packages/jpreprocess-mod/jpreprocess_mod-0.1.7-cp312-cp312-win32.whl/jpreprocess_mod/__init__.py
@@ -0,0 +1,35 @@
+import os
+from .jpreprocess_mod import (
+    __version__,
+    JPreprocess,
+    build_dictionary,
+    JPREPROCESS_VERSION,
+)
+from .dictionary import download_dictionary, dictionary_path
+
+
+__all__ = [
+    "JPreprocess",
+    "build_dictionary",
+    "download_dictionary",
+    "JPREPROCESS_VERSION",
+]
+
+
+def jpreprocess(dictionary_version: str = "v0.1.18", user_dictionary: str | None = None) -> JPreprocess:
+    """
+    Create jpreprocess instance with naist-jdic dictionary.
+
+    If the system dictionary is not present, this function will download it.
+
+    Arguments:
+    - dictionary_version (str): Version of dictionary to download.
+      We don't recommend specifying this argument unless you are aware of what you are doing.
+    - user_dictionary (str | None): Path to user dictionary. The extionsion must be ".csv" or ".bin".
+    """
+    dict_path = dictionary_path(dictionary_version)
+
+    if not os.path.exists(dict_path):
+        download_dictionary(dictionary_version)
+
+    return JPreprocess(dict_path, user_dictionary)
