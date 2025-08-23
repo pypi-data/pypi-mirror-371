98d8da099e8f6e5b9822f824d434b4713d355f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-23 05:05:50 +0000

    Add package sqlccc/1.1.1/sqlccc-1.1.1-py3-none-any.whl

---
+++ packages/sqlccc/sqlccc-1.1.1-py3-none-any.whl/sqlcc.py
@@ -0,0 +1,30 @@
+import sqlglot as sg
+
+"""
+Calculate the cyclomatic complexity of a SQL query.
+
+Args:
+    sql (str): The SQL query string.
+    dialect (str): The SQL dialect to use for parsing. See sqlglot documentation for supported dialects.
+
+Returns:
+    int: The calculated cyclomatic complexity.
+"""
+def calc(sql: str, dialect='snowflake') -> int:
+    result = 1
+    parsed = sg.parse_one(sql,dialect=dialect)
+    cyclomatics = [
+        # Logic
+        sg.exp.And, sg.exp.Or, sg.exp.In, sg.exp.Between, sg.exp.Like, sg.exp.Group,
+        # Columns
+        sg.exp.Case, sg.exp.Func,
+        # Sets
+        sg.exp.Join
+    ]
+    for cyclom in cyclomatics:
+        for item in parsed.find_all(cyclom):
+            if item.key == sg.exp.Join.key:
+                result = result + 2
+            else:
+                result += 1
+    return result
