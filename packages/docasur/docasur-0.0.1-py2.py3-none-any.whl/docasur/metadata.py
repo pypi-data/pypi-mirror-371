e6eb0f162fa8213b8ecef6175d37d7a2d52a547a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 04:09:01 +0000

    Add package docasur/0.0.1/docasur-0.0.1-py2.py3-none-any.whl

---
+++ packages/docasur/docasur-0.0.1-py2.py3-none-any.whl/docasur/metadata.py
@@ -0,0 +1,8 @@
+# --------------------------------------------------------------------------------------------------
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2024 Jayesh Badwaik <jayesh@ambhora.com>
+# --------------------------------------------------------------------------------------------------
+
+import semver as SemVer
+__version__ = "0.0.1"
+semver = SemVer.VersionInfo.parse(__version__)
