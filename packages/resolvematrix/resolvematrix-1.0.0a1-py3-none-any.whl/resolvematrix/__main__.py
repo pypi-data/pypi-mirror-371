6a2be65052e8b653a7966918fd4720a3e330648e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 03:28:23 +0000

    Add package resolvematrix/1.0.0a1/resolvematrix-1.0.0a1-py3-none-any.whl

---
+++ packages/resolvematrix/resolvematrix-1.0.0a1-py3-none-any.whl/resolvematrix/__main__.py
@@ -0,0 +1,17 @@
+import argparse
+import logging
+from .client import ClientResolver
+from .server import ServerResolver
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Resolve a matrix server name")
+    parser.add_argument("server_name", type=str, help="The matrix server name to resolve")
+    args = parser.parse_args()
+
+    cr = ClientResolver()
+    sr = ServerResolver()
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger("resolvematrix").setLevel(logging.DEBUG)
+    print("Client resolution:", cr.resolve(args.server_name))
+    print("Server resolution:", sr.resolve(args.server_name))
