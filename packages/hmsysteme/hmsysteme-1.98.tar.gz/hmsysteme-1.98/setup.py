f7983550927a452f035ba83c914776d71ff908a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 03:28:40 +0000

    Add package hmsysteme/1.98/hmsysteme-1.98.tar.gz

---
+++ packages/hmsysteme/hmsysteme-1.98.tar.gz/hmsysteme-1.98/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(name='hmsysteme',
+version='1.98',
+description='Connection to HM01',
+author='stan',
+author_email='stan3@gmx.net',
+license='MIT',
+url='https://github.com/simbabali/hmsysteme',
+download_url = 'https://github.com/simbabali/hmsysteme/archive/v0.2.tar.gz',
+packages=['hmsysteme'],
+zip_safe=False,
+install_requires=['pygame','pymunk', 'remi', 'smbus2', 'pyserial', 'matplotlib','Shared-Memory-Dict','pyopengl','setuptools'],)
