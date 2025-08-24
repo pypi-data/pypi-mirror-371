f8ee7eb1871f73c6d4e68950fd9b30c4c5c41ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 14:06:52 +0000

    Add package pyhesaff/2.1.2/pyhesaff-2.1.2-cp39-cp39-manylinux_2_28_x86_64.whl

---
+++ packages/pyhesaff/pyhesaff-2.1.2-cp39-cp39-manylinux_2_28_x86_64.whl/pyhesaff/__init__.py
@@ -0,0 +1,60 @@
+__version__ = '2.1.2'
+__author__ = 'Jon Crall'
+__author_email__ = 'erotemic@gmail.com'
+__url__ = 'https://github.com/Erotemic/pyhesaff'
+
+
+from pyhesaff import ctypes_interface
+from pyhesaff import _pyhesaff
+from pyhesaff._pyhesaff import (DESC_DIM, HESAFF_CLIB,
+                                HESAFF_PARAM_DICT, HESAFF_PARAM_TYPES,
+                                HESAFF_TYPED_PARAMS, KPTS_DIM,
+                                adapt_scale, alloc_kpts,
+                                alloc_patches, alloc_vecs,
+                                argparse_hesaff_params,
+                                detect_feats, detect_feats2,
+                                detect_feats_in_image, detect_feats_list,
+                                detect_num_feats_in_image,
+                                extract_desc_from_patches, extract_patches,
+                                extract_vecs, get_cpp_version,
+                                get_hesaff_default_params,
+                                get_is_debug_mode,
+                                hesaff_kwargs_docstr_block, img32_dtype,
+                                test_rot_invar,
+                                vtool_adapt_rotation,)
+
+__all__ = [
+    # modules
+    "ctypes_interface",
+    "_pyhesaff",
+
+    # constants
+    "DESC_DIM",
+    "HESAFF_CLIB",
+    "HESAFF_PARAM_DICT",
+    "HESAFF_PARAM_TYPES",
+    "HESAFF_TYPED_PARAMS",
+    "KPTS_DIM",
+
+    # functions
+    "adapt_scale",
+    "alloc_kpts",
+    "alloc_patches",
+    "alloc_vecs",
+    "argparse_hesaff_params",
+    "detect_feats",
+    "detect_feats2",
+    "detect_feats_in_image",
+    "detect_feats_list",
+    "detect_num_feats_in_image",
+    "extract_desc_from_patches",
+    "extract_patches",
+    "extract_vecs",
+    "get_cpp_version",
+    "get_hesaff_default_params",
+    "get_is_debug_mode",
+    "hesaff_kwargs_docstr_block",
+    "img32_dtype",
+    "test_rot_invar",
+    "vtool_adapt_rotation",
+]
