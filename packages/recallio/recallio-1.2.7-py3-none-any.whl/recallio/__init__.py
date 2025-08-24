16cab5de25cfca4b2631d435a58630e96db47ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 05:05:43 +0000

    Add package recallio/1.2.7/recallio-1.2.7-py3-none-any.whl

---
+++ packages/recallio/recallio-1.2.7-py3-none-any.whl/recallio/__init__.py
@@ -0,0 +1,51 @@
+"""Recallio API client package."""
+
+from .client import RecallioClient
+from .models import (
+    MemoryWriteRequest,
+    MemoryRecallRequest,
+    MemoryDeleteRequest,
+    MemoryDto,
+    MemoryWithScoreDto,
+    RecallSummaryRequest,
+    SummarizedMemoriesDto,
+    DocumentIngestRequest,
+    RecallTopicsRequest,
+    TopicsDto,
+    TopicItemDto,
+    SubTopicItemDto,
+    GraphAddRequest,
+    GraphSearchRequest,
+    GraphSearchResult,
+    GraphEntity,
+    GraphRelationship,
+    GraphAddResponse,
+    MemoryExportRequest,
+    ErrorReturnClass,
+)
+from .errors import RecallioAPIError
+
+__all__ = [
+    "RecallioClient",
+    "MemoryWriteRequest",
+    "MemoryRecallRequest",
+    "MemoryDeleteRequest",
+    "MemoryDto",
+    "MemoryWithScoreDto",
+    "RecallSummaryRequest",
+    "SummarizedMemoriesDto",
+    "DocumentIngestRequest",
+    "RecallTopicsRequest",
+    "TopicsDto",
+    "TopicItemDto",
+    "SubTopicItemDto",
+    "GraphAddRequest",
+    "GraphSearchRequest",
+    "GraphSearchResult",
+    "GraphEntity",
+    "GraphRelationship",
+    "GraphAddResponse",
+    "MemoryExportRequest",
+    "ErrorReturnClass",
+    "RecallioAPIError",
+]
