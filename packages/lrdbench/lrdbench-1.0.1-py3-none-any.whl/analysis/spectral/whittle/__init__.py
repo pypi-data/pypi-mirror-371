4049a03f1020a4292c03df34fd4327baaea2ca60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 04:09:01 +0000

    Add package lrdbench/1.0.1/lrdbench-1.0.1-py3-none-any.whl

---
+++ packages/lrdbench/lrdbench-1.0.1-py3-none-any.whl/analysis/spectral/whittle/__init__.py
@@ -0,0 +1,22 @@
+"""
+Whittle estimator subpackage.
+
+Exposes the `WhittleEstimator` for (approximate) Whittle likelihood
+estimation of fractional differencing parameter d and Hurst parameter.
+"""
+
+from .whittle_estimator import WhittleEstimator
+
+__all__ = ["WhittleEstimator"]
+
+
+
+
+
+
+
+
+
+
+
+
