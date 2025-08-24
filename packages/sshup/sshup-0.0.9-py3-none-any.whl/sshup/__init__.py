a111e0915ce347f843d4bb7c196897ef3fe2bb20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 04:08:33 +0000

    Add package sshup/0.0.9/sshup-0.0.9-py3-none-any.whl

---
+++ packages/sshup/sshup-0.0.9-py3-none-any.whl/sshup/__init__.py
@@ -0,0 +1,11 @@
+"""sshup"""
+
+######################################################################
+# Main app information.
+__author__     = "Wael Ramadan"
+__copyright__  = "Copyright 2025, Wael Ramadan"
+__credits__    = [ "Wael Ramadan" ]
+__maintainer__ = "Wael Ramadan"
+__email__      = "wamramadan@gmail.com"
+__version__    = "0.0.9"
+__licence__    = "MIT"
