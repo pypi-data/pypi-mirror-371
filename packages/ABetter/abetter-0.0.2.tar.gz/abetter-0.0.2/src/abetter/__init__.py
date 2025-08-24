5380538f7164df86cc65ba8a4a9c9895b7086bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 15:05:14 +0000

    Add package ABetter/0.0.2/abetter-0.0.2.tar.gz

---
+++ packages/ABetter/abetter-0.0.2.tar.gz/abetter-0.0.2/src/abetter/__init__.py
@@ -0,0 +1,8 @@
+from .ab import AB
+from .sample_mean import SampleMean
+from .sample_prop import SampleProp
+from .io.plot import plot_two_mean, plot_diff_mean, plot_mde_mean_sample, plot_mde_prop_sample
+
+__version__ = "0.0.2"
+
+__all__ = ['AB', 'SampleMean', 'SampleProp', 'plot_two_mean', 'plot_diff_mean', 'plot_mde_mean_sample', 'plot_mde_prop_sample']
