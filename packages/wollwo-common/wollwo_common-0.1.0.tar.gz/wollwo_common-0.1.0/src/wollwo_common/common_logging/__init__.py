6312ff47c6d662698da8df4ce181ecb7b8f300e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 15:05:30 +0000

    Add package wollwo-common/0.1.0/wollwo_common-0.1.0.tar.gz

---
+++ packages/wollwo-common/wollwo_common-0.1.0.tar.gz/wollwo_common-0.1.0/src/wollwo_common/common_logging/__init__.py
@@ -0,0 +1,8 @@
+"""
+Copyright (c) 2025 by Michal Perzel. All rights reserved.
+
+License: MIT
+"""
+
+from .common_log_line import CommonLogLineBase
+from .common_logger import common_logger
---
+++ packages/wollwo-common/wollwo_common-0.1.0.tar.gz/wollwo_common-0.1.0/src/wollwo_common/wollwo_decorators/__init__.py
@@ -0,0 +1,8 @@
+"""
+Copyright (c) 2025 by Michal Perzel. All rights reserved.
+
+License: MIT
+"""
+
+from .check_return_value_type import CheckReturnValueType
+from .except_base_exception import ExceptBaseException
