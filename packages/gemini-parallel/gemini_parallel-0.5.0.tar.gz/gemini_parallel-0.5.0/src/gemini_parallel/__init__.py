13cdd42da9f60acbe7b3a5b03435dc2c07cefc8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 04:08:59 +0000

    Add package gemini-parallel/0.5.0/gemini_parallel-0.5.0.tar.gz

---
+++ packages/gemini-parallel/gemini_parallel-0.5.0.tar.gz/gemini_parallel-0.5.0/src/gemini_parallel/__init__.py
@@ -0,0 +1,25 @@
+# src/gemini_parallel/__init__.py
+
+from .gemini_parallel import (
+    GeminiParallelProcessor,
+    GeminiStreamingProcessor,
+    AdvancedApiKeyManager,
+)
+from .gemini_media_processor import prepare_media_contents
+from .gemini_tts import (
+    GeminiTTSProcessor,
+    text_to_speech,
+    TTS_VOICES,
+    TTS_MODELS,
+)
+
+__all__ = [
+    "GeminiParallelProcessor",
+    "GeminiStreamingProcessor",
+    "AdvancedApiKeyManager",
+    "prepare_media_contents",
+    "GeminiTTSProcessor",
+    "text_to_speech",
+    "TTS_VOICES",
+    "TTS_MODELS",
+]
