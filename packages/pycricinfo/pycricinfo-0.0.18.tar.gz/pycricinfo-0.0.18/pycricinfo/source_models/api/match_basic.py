6ba2a5e63b0198bd663e4e36e62e54f043622f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 03:28:12 +0000

    Add package pycricinfo/0.0.18/pycricinfo-0.0.18.tar.gz

---
+++ packages/pycricinfo/pycricinfo-0.0.18.tar.gz/pycricinfo-0.0.18/pycricinfo/source_models/api/match_basic.py
@@ -0,0 +1,53 @@
+from typing import Optional
+
+from pydantic import AliasChoices, Field
+
+from pycricinfo.source_models.api.common import CCBaseModel, RefMixin
+from pycricinfo.source_models.api.venue import Venue
+
+
+class MatchCompetitorBasic(CCBaseModel):
+    id: int
+    winner: bool
+    home_or_away: str = Field(validation_alias=AliasChoices("home_or_away", "homeAway"))
+    team: RefMixin
+    score: RefMixin
+    linescores: RefMixin
+    roster: RefMixin
+    leaders: RefMixin
+    statistics: RefMixin
+    record: RefMixin
+
+
+class MatchClass(CCBaseModel):
+    name: str
+    event_type: str
+    general_class_id: int
+    international_class_id: int
+    international_class_card: Optional[str] = None
+    general_class_card: Optional[str] = None
+
+
+class MatchCompetitionBasic(CCBaseModel):
+    id: int
+    description: str
+    date: str
+    end_date: str
+    day_night: bool
+    limited_overs: bool
+    reduced_overs: bool
+    match_class: MatchClass = Field(validation_alias=AliasChoices("match_class", "class"))
+    venue: Venue
+    competitors: list[MatchCompetitorBasic]
+
+
+class MatchBasic(CCBaseModel):
+    id: int
+    name: str
+    description: str
+    short_name: str
+    short_description: str
+    season: RefMixin
+    season_type: RefMixin
+    venues: list[RefMixin]
+    competitions: list[MatchCompetitionBasic]
