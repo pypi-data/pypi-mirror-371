80ce84ac77a3a94f2c9d11f0336950b3ade14189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 14:06:40 +0000

    Add package bluer-ugv/6.709.1/bluer_ugv-6.709.1.tar.gz

---
+++ packages/bluer-ugv/bluer_ugv-6.709.1.tar.gz/bluer_ugv-6.709.1/bluer_ugv/README/consts.py
@@ -0,0 +1,13 @@
+algo_docs = algo_docs = (
+    "https://github.com/kamangir/bluer-algo/blob/main/bluer_algo/docs"
+)
+
+assets = "https://github.com/kamangir/assets/raw/main"
+
+assets2 = "https://github.com/kamangir/assets2/raw/main"
+
+assets2_bluer_eagle = f"{assets2}/bluer-eagle"
+assets2_bluer_fire = f"{assets2}/bluer-fire"
+assets2_bluer_robin = f"{assets2}/bluer-robin"
+assets2_bluer_swallow = f"{assets2}/bluer-swallow"
+assets2_bluer_sparrow = f"{assets2}/bluer-sparrow"
---
+++ packages/bluer-ugv/bluer_ugv-6.709.1.tar.gz/bluer_ugv-6.709.1/bluer_ugv/help/swallow/env.py
@@ -0,0 +1,40 @@
+from typing import List
+
+from bluer_options.terminal import show_usage, xtra
+
+
+def help_cp(
+    tokens: List[str],
+    mono: bool,
+) -> str:
+    return show_usage(
+        [
+            "@swallow",
+            "env",
+            "cp",
+            "[<env-name>]",
+        ],
+        "cp swallow swallow-raspbian-<env-name>.",
+        mono=mono,
+    )
+
+
+def help_list(
+    tokens: List[str],
+    mono: bool,
+) -> str:
+    return show_usage(
+        [
+            "@swallow",
+            "env",
+            "list",
+        ],
+        "list swallow envs.",
+        mono=mono,
+    )
+
+
+help_functions = {
+    "cp": help_cp,
+    "list": help_list,
+}
