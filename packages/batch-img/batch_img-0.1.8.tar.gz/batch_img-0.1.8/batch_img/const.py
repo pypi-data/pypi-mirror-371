a816ade3f2539163e09a09dc5ba8bda3b1bba93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 05:06:10 +0000

    Add package batch-img/0.1.8/batch_img-0.1.8.tar.gz

---
+++ packages/batch-img/batch_img-0.1.8.tar.gz/batch_img-0.1.8/batch_img/const.py
@@ -0,0 +1,28 @@
+"""const.py - define constants
+Copyright © 2025 John Liu
+"""
+
+PKG_NAME = "batch_img"
+VER = "version"
+EXPIRE_HOUR = 48
+UNKNOWN = "unknown"
+
+MSG_OK = "✅ Processed the image file(s)"
+MSG_BAD = "❌ Failed to process image file(s)."
+
+TS_FORMAT = "%Y-%m-%d_%H-%M-%S"
+PATTERNS = (
+    "*.HEIC",
+    "*.heic",
+    "*.JPG",
+    "*.jpg",
+    "*.JPEG",
+    "*.jpeg",
+    "*.PNG",
+    "*.png",
+)
+EXIF = "exif"
+MAX_LENGTH = 1280
+BD_WIDTH = 5
+BD_COLOR = "black"
+REPLACE = "replace"
