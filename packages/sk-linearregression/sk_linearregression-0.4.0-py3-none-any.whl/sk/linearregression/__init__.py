3380afde21c54789838e847d2289320d448e4b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 15:05:15 +0000

    Add package sk-linearregression/0.4.0/sk_linearregression-0.4.0-py3-none-any.whl

---
+++ packages/sk-linearregression/sk_linearregression-0.4.0-py3-none-any.whl/sk/linearregression/__init__.py
@@ -0,0 +1,6 @@
+from .LinearRegression import LinearRegression
+from .HandleMissingValue import HandleMissingValue
+from .TrainTestSplit import TrainTestSplit
+from .MinMaxScaler import MinMaxScaler  
+
+__all__ = ["LinearRegression", "HandleMissingValue", "TrainTestSplit", "MinMaxScaler"]
