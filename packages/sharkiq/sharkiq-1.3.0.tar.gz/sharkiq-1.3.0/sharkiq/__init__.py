555b61492fca1ea21e3e98fedbf98379c0bb575d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 05:06:04 +0000

    Add package sharkiq/1.3.0/sharkiq-1.3.0.tar.gz

---
+++ packages/sharkiq/sharkiq-1.3.0.tar.gz/sharkiq-1.3.0/sharkiq/__init__.py
@@ -0,0 +1,13 @@
+"""Unofficial SDK for Shark IQ robot vacuums, designed primarily to support an integration for Home Assistant."""
+
+from .ayla_api import get_ayla_api, AylaApi
+from .exc import (
+    SharkIqError,
+    SharkIqAuthExpiringError,
+    SharkIqNotAuthedError,
+    SharkIqAuthError,
+    SharkIqReadOnlyPropertyError,
+)
+from .sharkiq import OperatingModes, PowerModes,  Properties, SharkIqVacuum
+
+__version__ = '1.3.0'
