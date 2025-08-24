b1e7ecb0f84eb2fa851a7248574a869ec9162c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 04:08:49 +0000

    Add package django-simple-ordering/1.0.1/django_simple_ordering-1.0.1-py3-none-any.whl

---
+++ packages/django-simple-ordering/django_simple_ordering-1.0.1-py3-none-any.whl/django_simple_ordering/__init__.py
@@ -0,0 +1,11 @@
+"""
+Simple Integer-based Ordering for Django Models
+"""
+
+__version__ = "1.0.1"
+
+from .admin import SimpleOrderingModelAdminMixin
+
+__all__ = [
+    "SimpleOrderingModelAdminMixin",
+]
