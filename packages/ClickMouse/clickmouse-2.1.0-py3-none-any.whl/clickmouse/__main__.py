20bdb746123169fd99d16603fc2617206ea8ff07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 14:06:35 +0000

    Add package ClickMouse/2.1.0/clickmouse-2.1.0-py3-none-any.whl

---
+++ packages/ClickMouse/clickmouse-2.1.0-py3-none-any.whl/clickmouse/__main__.py
@@ -0,0 +1,19 @@
+"""
+ClickMouse命令行工具
+"""
+
+import argparse
+from . import click_mouse
+from .version import version
+
+# 常量
+__version__ = version
+
+def main():
+    """
+    ClickMouse命令行工具的函数
+    """
+    print('ClickMouse命令行工具未实现，敬请期待')
+
+if __name__ == '__main__':
+    main()
