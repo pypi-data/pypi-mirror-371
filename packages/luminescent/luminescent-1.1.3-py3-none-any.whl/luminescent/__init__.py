fcf6fba2d48c6346857b091d5c9f450bbcbd0886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 04:08:52 +0000

    Add package luminescent/1.1.3/luminescent-1.1.3-py3-none-any.whl

---
+++ packages/luminescent/luminescent-1.1.3-py3-none-any.whl/luminescent/__init__.py
@@ -0,0 +1,27 @@
+# import gplugins.luminescent as gl
+from gdsfactory.generic_tech import *
+from .utils import *
+from .constants import *
+from .materials import *
+from .sol import *
+
+from .pic.gcells import *
+from .pic.setup import *
+from .pic.inverse_design import *
+from .pic.sparams import *
+
+print("running luminescent python frontend")
+
+
+# __all__ = [
+#     "pic_design_problem",
+#     "sparams_problem",
+#     "solve",
+#     # "apply_design",
+#     "load",
+#     "finetune",
+#     "load_prob",
+#     # "make_training_movie",
+#     # "make_simulation_movie",
+#     "mimo",
+# ]
