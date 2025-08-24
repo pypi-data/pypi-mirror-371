4a279e37add5e128b00ece170c9663551ad0df20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 14:07:28 +0000

    Add package superset-showtime/0.2.3/superset_showtime-0.2.3.tar.gz

---
+++ packages/superset-showtime/superset_showtime-0.2.3.tar.gz/superset_showtime-0.2.3/showtime/__init__.py
@@ -0,0 +1,19 @@
+"""
+🎪 Superset Showtime - Smart ephemeral environment management
+
+Circus tent emoji state tracking for Apache Superset ephemeral environments.
+"""
+
+__version__ = "0.2.3"
+__author__ = "Maxime Beauchemin"
+__email__ = "maximebeauchemin@gmail.com"
+
+from .core.circus import PullRequest, Show
+from .core.github import GitHubInterface
+
+__all__ = [
+    "__version__",
+    "Show",
+    "PullRequest",
+    "GitHubInterface",
+]
