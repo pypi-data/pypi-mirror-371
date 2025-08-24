41b4a94c3d6765d5ba136ec10bfd3569a5723dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 05:06:07 +0000

    Add package rlottie-python/1.3.8/rlottie_python-1.3.8-pp37-pypy37_pp73-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/rlottie-python/rlottie_python-1.3.8-pp37-pypy37_pp73-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/rlottie_python/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""rlottie-python"""
+
+__version__ = "1.3.8"
+
+from .rlottie_wrapper import LottieAnimation  # type: ignore # noqa: F401
+from .rlottie_wrapper import LottieAnimationProperty  # type: ignore # noqa: F401
