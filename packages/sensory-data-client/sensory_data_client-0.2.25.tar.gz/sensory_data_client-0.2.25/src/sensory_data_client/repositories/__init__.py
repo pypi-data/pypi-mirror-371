a6fc1d72d1de50d3a6c1ce7b00d36cadabcb5732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 14:06:20 +0000

    Add package sensory-data-client/0.2.25/sensory_data_client-0.2.25.tar.gz

---
+++ packages/sensory-data-client/sensory_data_client-0.2.25.tar.gz/sensory_data_client-0.2.25/src/sensory_data_client/repositories/__init__.py
@@ -0,0 +1,30 @@
+from .minio_repository import MinioRepository
+from .pg_repositoryMeta import MetaDataRepository
+from .pg_repositoryObj import ObjectRepository
+from .lines_repo.pg_repositoryImage import ImageRepository
+from .lines_repo.pg_repositoryAudio import AudioRepository
+from .lines_repo.pg_repositoryDoc import DocumentDetailsRepository
+from .lines_repo.pg_repositoryLine import LineRepository
+from .auth.pg_repositoryUser import UserRepository
+from .auth.pg_repositoryGroup import GroupRepository
+from .auth.pg_repositoryPermission import PermissionRepository
+from .pg_repositoryTag import TagRepository
+from .auth.pg_repositoryBilling import BillingRepository
+from .es_repository import ElasticsearchRepository
+
+__all__ = [
+    "MinioRepository", 
+    "LineRepository", 
+    "MetaDataRepository", 
+    "ObjectRepository", 
+    "DocumentDetailsRepository",
+    "AudioRepository",
+    "ImageRepository", 
+    "UserRepository",
+    "GroupRepository",  
+    "PermissionRepository",  
+    "TagRepository",    
+    "BillingRepository",
+    "ElasticsearchRepository",
+    
+]
