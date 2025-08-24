0959eb3e76507139b24bd0fad2d1c0e4feb2c868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 03:27:39 +0000

    Add package inverse-projections/0.1.2/inverse_projections-0.1.2.tar.gz

---
+++ packages/inverse-projections/inverse_projections-0.1.2.tar.gz/inverse_projections-0.1.2/src/inverse_projections/__init__.py
@@ -0,0 +1,26 @@
+"""
+Inverse Projections Package
+
+A collection of inverse projection techniques for dimensionality reduction.
+Provides methods to map points from low-dimensional spaces back to high-dimensional spaces.
+"""
+
+from .nninv import NNinv
+from .lamp import ILAMP
+from .rbf import RBFinv
+from .multilateration import MDSinv
+from .gradient_map import get_gradient_map
+
+__author__ = "Yu Wang"
+
+__all__ = [
+    "NNinv",
+    "ILAMP", 
+    "RBFinv",
+    "MDSinv",
+    "get_gradient_map"
+]
+
+
+def main() -> None:
+    print("Hello from inverse-projections!")
