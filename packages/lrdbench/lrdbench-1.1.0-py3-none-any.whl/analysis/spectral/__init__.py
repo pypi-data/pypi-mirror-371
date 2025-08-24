eab2205359416164d575a59e6f0fc885e47ebc58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 14:06:23 +0000

    Add package lrdbench/1.1.0/lrdbench-1.1.0-py3-none-any.whl

---
+++ packages/lrdbench/lrdbench-1.1.0-py3-none-any.whl/analysis/spectral/__init__.py
@@ -0,0 +1,24 @@
+"""
+Spectral estimators package.
+
+Contains frequency-domain estimators such as Periodogram, Whittle, and GPH.
+"""
+
+__all__ = [
+    "periodogram",
+    "whittle",
+    "gph",
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
