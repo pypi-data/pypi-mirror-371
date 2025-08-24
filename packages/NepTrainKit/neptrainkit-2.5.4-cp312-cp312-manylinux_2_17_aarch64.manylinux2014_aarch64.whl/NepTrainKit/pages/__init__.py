27487fd542df5aceb3c638732f664c0ffa43c89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 14:06:48 +0000

    Add package NepTrainKit/2.5.4/neptrainkit-2.5.4-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/NepTrainKit/neptrainkit-2.5.4-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/NepTrainKit/pages/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2024/10/17 17:21
+# @Author  : 兵
+# @email    : 1747193328@qq.com
+from .makedata import MakeDataWidget
+from .settings import SettingsWidget
+from .show_nep import ShowNepWidget
