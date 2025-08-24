aae5e9626e322c0978a05e0a440a168a7102b8c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 05:05:44 +0000

    Add package briefcasebrain-sdk/1.0.0/briefcasebrain_sdk-1.0.0-py3-none-any.whl

---
+++ packages/briefcasebrain-sdk/briefcasebrain_sdk-1.0.0-py3-none-any.whl/briefcasebrain_sdk/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from briefcasebrain_sdk.api.contracts_api import ContractsApi
+from briefcasebrain_sdk.api.legal_research_api import LegalResearchApi
+from briefcasebrain_sdk.api.queries_api import QueriesApi
+from briefcasebrain_sdk.api.query_enhancement_api import QueryEnhancementApi
+from briefcasebrain_sdk.api.user_api import UserApi
+from briefcasebrain_sdk.api.workflows_api import WorkflowsApi
+
