ac8db499105824f8683fed9fcb6441a6b5f81bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 04:08:52 +0000

    Add package tide-sdk/0.1.7/tide_sdk-0.1.7.tar.gz

---
+++ packages/tide-sdk/tide_sdk-0.1.7.tar.gz/tide_sdk-0.1.7/tide/__init__.py
@@ -0,0 +1,35 @@
+"""
+tide - A Zenoh-based framework for robotics with opinionated namespacing
+
+This framework provides a lightweight, strongly-typed layer
+on top of Zenoh for building robot control systems.
+"""
+
+__version__ = "0.1.7"
+
+# Import core components
+from tide.core.node import BaseNode
+from tide.components import PIDNode
+
+# Reserved namespace enums and helpers
+from tide.namespaces import (
+    Group,
+    CmdTopic,
+    StateTopic,
+    SensorTopic,
+    sensor_camera_rgb,
+    sensor_camera_depth,
+    robot_topic,
+)
+
+__all__ = [
+    "BaseNode",
+    "Group",
+    "CmdTopic",
+    "StateTopic",
+    "SensorTopic",
+    "sensor_camera_rgb",
+    "sensor_camera_depth",
+    "robot_topic",
+    "PIDNode",
+]
