9eb89cff187c44d6bcd0fa0d849f0bb4082cfdd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 14:06:40 +0000

    Add package fbusl/0.5.21/fbusl-0.5.21.tar.gz

---
+++ packages/fbusl/fbusl-0.5.21.tar.gz/fbusl-0.5.21/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    
+]
+
+setup(
+    name="fbusl",
+    version='0.05.21',
+    packages=find_packages(),
+    license="MIT",
+    author="Oliver Morrison",
+    long_description_content_type="text/markdown",
+    install_requires=requirements
+)
