b32b220b0ad25224fb7e339fb311c5738d701b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 03:28:30 +0000

    Add package supertuxkart.py/0.1.7/supertuxkart_py-0.1.7.tar.gz

---
+++ packages/supertuxkart.py/supertuxkart_py-0.1.7.tar.gz/supertuxkart_py-0.1.7/supertuxkart/__init__.py
@@ -0,0 +1,8 @@
+from .client import SuperTuxKartClient, AsyncSuperTuxKartClient
+from .relationships import Friend
+from .account import SessionInfo
+from .addons import AddonVote, SetAddonVote
+from .users import User
+from .ranking import Ranking, TopPlayer
+from .server import Server, ServerPlayer
+from .errors import *
