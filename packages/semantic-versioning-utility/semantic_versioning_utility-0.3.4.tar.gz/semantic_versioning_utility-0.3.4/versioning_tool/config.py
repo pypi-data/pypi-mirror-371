45553f5928c9066dd821449d652711ee0443b32a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 04:08:47 +0000

    Add package semantic-versioning-utility/0.3.4/semantic_versioning_utility-0.3.4.tar.gz

---
+++ packages/semantic-versioning-utility/semantic_versioning_utility-0.3.4.tar.gz/semantic_versioning_utility-0.3.4/versioning_tool/config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+PROJ_ROOT = Path(__file__).parent.parent.resolve()
+CHANGELOG_FILE = PROJ_ROOT / "CHANGELOG.md"
+PYPROJECT_FILE = PROJ_ROOT / "pyproject.toml"
+VERSIONING_CONFIG = PROJ_ROOT / "versioning.yaml"
