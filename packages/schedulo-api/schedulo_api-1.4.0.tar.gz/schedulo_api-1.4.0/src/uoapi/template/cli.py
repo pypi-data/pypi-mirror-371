c1c1fb6f503ca0888ce9a7097e28918111096b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 03:27:57 +0000

    Add package schedulo-api/1.4.0/schedulo_api-1.4.0.tar.gz

---
+++ packages/schedulo-api/schedulo_api-1.4.0.tar.gz/schedulo_api-1.4.0/src/uoapi/template/cli.py
@@ -0,0 +1,25 @@
+import argparse
+
+from uoapi.cli_tools import make_parser, make_cli
+
+description = ""
+help = ""
+epilog = ""
+
+
+@make_parser(description=description, epilog=epilog)
+def parser(default):
+    raise NotImplementedError()
+
+
+@make_cli(parser)
+def cli(args=None):
+    raise NotImplementedError()
+
+
+def main():
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    cli()
