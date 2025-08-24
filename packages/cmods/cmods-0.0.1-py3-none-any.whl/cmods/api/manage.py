784445856a4bb10d651edf1c3d16b1ee3a0ae6e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 04:08:54 +0000

    Add package cmods/0.0.1/cmods-0.0.1-py3-none-any.whl

---
+++ packages/cmods/cmods-0.0.1-py3-none-any.whl/cmods/api/manage.py
@@ -0,0 +1,56 @@
+import json
+
+def cmods_repo_add(name, url):
+    cmods_config = None
+    
+    with open("cmods.json", "r") as f:
+        cmods_config = json.load(f)
+    
+    cmods_config["repositories"][name]=url
+    
+    with open("cmods.json", "w") as f:
+        json.dump(cmods_config, f)
+
+def cmods_repo_del(name):
+    cmods_config = None
+    
+    with open("cmods.json", "r") as f:
+        cmods_config = json.load(f)
+    
+    del cmods_config["repositories"][name]
+    
+    with open("cmods.json", "w") as f:
+        json.dump(cmods_config, f)
+
+def cmods_mod_add(name, repo):
+    cmods_config = None
+    
+    with open("cmods.json", "r") as f:
+        cmods_config = json.load(f)
+    
+    cmods_config["dependencies"][name]=repo
+    
+    with open("cmods.json", "w") as f:
+        json.dump(cmods_config, f)
+
+def cmods_mod_add(name):
+    cmods_config = None
+    
+    with open("cmods.json", "r") as f:
+        cmods_config = json.load(f)
+    
+    del cmods_config["dependencies"][name]
+    
+    with open("cmods.json", "w") as f:
+        json.dump(cmods_config, f)
+
+def cmods_set_cflags(cflags):
+    cmods_config = None
+    
+    with open("cmods.json", "r") as f:
+        cmods_config = json.load(f)
+    
+    del cmods_config["CFLAGS"]=cflags
+    
+    with open("cmods.json", "w") as f:
+        json.dump(cmods_config, f)
