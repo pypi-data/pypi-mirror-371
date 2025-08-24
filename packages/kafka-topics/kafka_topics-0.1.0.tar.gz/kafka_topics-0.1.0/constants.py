e3ce86ba41db344f993fefaeba30b6408d801404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 05:05:48 +0000

    Add package kafka-topics/0.1.0/kafka_topics-0.1.0.tar.gz

---
+++ packages/kafka-topics/kafka_topics-0.1.0.tar.gz/kafka_topics-0.1.0/constants.py
@@ -0,0 +1,10 @@
+class KafkaTopics:
+    USER_REGISTERED = "user.registered"
+    USER_VERIFICATION_REQUEST = "user.verification_request"
+    USER_PASSWORD_RESET_REQUEST = "user.password_reset_request"
+    USER_EMAIL_VERIFICATION_REQUEST = "user.email_verification_request"
+    USER_KYC_VERIFIED = "user.kyc_verified"
+    RESERVATION_RECORD = "reservation.record"
+    RESERVATION_SUCCEEDED = "reservation.suceeded"
+    SLOT_RESERVED_NOTIFICATION = "slot.reserved_notification"
+    # Add other topics as needed
