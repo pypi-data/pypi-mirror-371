0c3705b11034781a6cb6fa23f3634d8ec7f5e342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 04:08:54 +0000

    Add package cmods/1.0.0/cmods-1.0.0-py3-none-any.whl

---
+++ packages/cmods/cmods-1.0.0-py3-none-any.whl/cmods/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2025-present Guille-ux <guilleleiratemes@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "1.0.0"
---
+++ packages/cmods/cmods-1.0.0-py3-none-any.whl/cmods/api/__init__.py
@@ -0,0 +1,5 @@
+# API
+from . import download
+from . import make
+from . import init
+from . import manage
