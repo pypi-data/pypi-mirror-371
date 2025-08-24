fba420e428c12cc80db3daf14f5114999a61f430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 15:05:13 +0000

    Add package clipassgen/0.7.5/clipassgen-0.7.5-py3-none-any.whl

---
+++ packages/clipassgen/clipassgen-0.7.5-py3-none-any.whl/clipassgen/__init__.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------
+# Licensed under the terms of the BSD 3-Clause License
+# (see LICENSE for details).
+# Copyright © 2018-2024, A.A. Suvorov
+# All rights reserved.
+# --------------------------------------------------------
+# https://github.com/smartlegionlab/
+# --------------------------------------------------------
+"""
+Console Smart Passwords Generator. Cross-platform console utility for
+generating cryptographically strong, recoverable, smart passwords.
+"""
+__version__ = '0.7.5'
---
+++ packages/clipassgen/clipassgen-0.7.5-py3-none-any.whl/clipassgen/config.py
@@ -0,0 +1,14 @@
+# --------------------------------------------------------
+# Licensed under the terms of the BSD 3-Clause License
+# (see LICENSE for details).
+# Copyright © 2018-2024, A.A. Suvorov
+# All rights reserved.
+# --------------------------------------------------------
+# https://github.com/smartlegionlab/
+# --------------------------------------------------------
+
+
+class Config:
+    name = 'Smart Password Generator'
+    url = 'https://github.com/smartlegionlab/clipassgen/'
+    copyright_ = 'Copyright © 2018-2024, A.A. Suvorov. All rights reserved.'
