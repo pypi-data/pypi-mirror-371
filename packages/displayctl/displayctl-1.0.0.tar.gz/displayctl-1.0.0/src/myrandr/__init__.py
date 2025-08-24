8acef4518b6f6a74ed003489a733988ed7454b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 05:05:51 +0000

    Add package displayctl/1.0.0/displayctl-1.0.0.tar.gz

---
+++ packages/displayctl/displayctl-1.0.0.tar.gz/displayctl-1.0.0/src/myrandr/__init__.py
@@ -0,0 +1,5 @@
+"""MyRandr - Monitor Configuration Manager for GNOME."""
+
+__version__ = "1.0.0"
+__author__ = "MyRandr Contributors"
+__description__ = "Monitor Configuration Manager for GNOME"
---
+++ packages/displayctl/displayctl-1.0.0.tar.gz/displayctl-1.0.0/src/myrandr/__main__.py
@@ -0,0 +1,6 @@
+"""Allow myrandr to be run as a module with python -m myrandr."""
+
+from myrandr.cli import main
+
+if __name__ == '__main__':
+    main()
