664980b393e06cd96640f504c89685f322cd2d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 03:28:12 +0000

    Add package celeryviz/0.0.4/celeryviz-0.0.4.tar.gz

---
+++ packages/celeryviz/celeryviz-0.0.4.tar.gz/celeryviz-0.0.4/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+def get_requirements():
+    return open('./requirements.txt').read().splitlines()
+
+classes = """
+    Development Status :: 1 - Planning
+    Intended Audience :: Developers
+    Topic :: System :: Monitoring
+    Programming Language :: Python
+    Programming Language :: Python :: 3
+    Programming Language :: Python :: 3 :: Only
+    Programming Language :: Python :: 3.6
+    Programming Language :: Python :: 3.7
+    Programming Language :: Python :: 3.8
+    Programming Language :: Python :: 3.9
+    Programming Language :: Python :: Implementation :: CPython
+    Programming Language :: Python :: Implementation :: PyPy
+    Operating System :: OS Independent
+"""
+classifiers = [s.strip() for s in classes.split('\n') if s]
+
+setup(
+    name='celeryviz',
+    description= "A UI centric tool for visualising Celery task execution.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    version='0.0.4',
+    classifiers=classifiers,
+    author = "Bhavya Peshavaria",
+    packages=find_packages(),
+    package_dir={'celeryviz': 'celeryviz'},
+    include_package_data=True,
+    package_data={'celeryviz': ['static/*']},
+    install_requires=get_requirements(),
+    entry_points={
+        'celery.commands': [
+            'celeryviz = celeryviz.command:celeryviz',
+        ],
+    },
+)
