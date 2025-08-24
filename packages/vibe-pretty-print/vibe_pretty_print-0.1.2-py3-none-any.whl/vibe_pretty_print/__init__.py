d7b07ffeb2206664ef4a3b102aa4e71a1ce2b404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 14:06:23 +0000

    Add package vibe-pretty-print/0.1.2/vibe_pretty_print-0.1.2-py3-none-any.whl

---
+++ packages/vibe-pretty-print/vibe_pretty_print-0.1.2-py3-none-any.whl/vibe_pretty_print/__init__.py
@@ -0,0 +1,10 @@
+"""
+Clean Architecture implementation of vibe-pretty-print
+"""
+from .runner import vibe_pprint
+
+__version__ = "0.1.2"
+__author__ = "Khanh Ngo"
+__email__ = "akatekhanh0212@gmail.com"
+
+__all__ = ["vibe_pprint"]
