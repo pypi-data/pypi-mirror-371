fc67ce7846f3cf15748b71fce1663df94deecc22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 04:08:29 +0000

    Add package schedulo-api/2.0.3/schedulo_api-2.0.3.tar.gz

---
+++ packages/schedulo-api/schedulo_api-2.0.3.tar.gz/schedulo_api-2.0.3/src/uoapi/dates/cli.py
@@ -0,0 +1,27 @@
+import json
+
+from uoapi.cli_tools import make_parser, make_cli
+from uoapi.dates import scrape_dates
+
+description = ""
+help = ""
+epilog = ""
+
+
+@make_parser(description=description, epilog=epilog)
+def parser(default):
+    return default
+
+
+@make_cli(parser)
+def cli(args=None):
+    for x in main():
+        print(json.dumps(x))
+
+
+def main():
+    yield from scrape_dates()
+
+
+if __name__ == "__main__":
+    cli()
