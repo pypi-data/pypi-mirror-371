29717bdf3daa8c5395e25c08884ff3fb94be107f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 14:06:19 +0000

    Add package python-ecobee-api/0.3.1/python_ecobee_api-0.3.1.tar.gz

---
+++ packages/python-ecobee-api/python_ecobee_api-0.3.1.tar.gz/python_ecobee_api-0.3.1/pyecobee/errors.py
@@ -0,0 +1,20 @@
+"""Errors used in this library."""
+
+
+class EcobeeError(Exception):
+    """Base class for all ecobee exceptions."""
+
+    pass
+
+
+class ExpiredTokenError(EcobeeError):
+    """Raised when ecobee API returns a code indicating expired credentials."""
+
+    pass
+
+
+class InvalidTokenError(EcobeeError):
+    """Raised when ecobee API returns a code indicating invalid credentials."""
+
+class InvalidSensorError(EcobeeError):
+    """Raised when remote sensor not present on thermostat."""
