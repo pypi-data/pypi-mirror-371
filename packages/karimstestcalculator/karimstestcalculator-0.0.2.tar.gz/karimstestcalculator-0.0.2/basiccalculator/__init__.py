0e572a2acea1cde3cfef4aa9f70b9086512a9cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-24 05:05:44 +0000

    Add package karimstestcalculator/0.0.2/karimstestcalculator-0.0.2.tar.gz

---
+++ packages/karimstestcalculator/karimstestcalculator-0.0.2.tar.gz/karimstestcalculator-0.0.2/basiccalculator/__init__.py
@@ -0,0 +1,11 @@
+def add_numbers(num1, num2):
+    num1 + num2
+
+def sub_numbers(num1, num2):
+    num1 - num2
+
+def mult_numbers(num1, num2):
+    num1 * num2
+
+def divi_numbers(num1, num2):
+    num1 / num2
