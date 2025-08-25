e2508bfda1bc76a5c32ed552a302eac0a285a111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 15:12:14 +0000

    Add package gllm-inference-binary/0.5.1/gllm_inference_binary-0.5.1-cp312-cp312-win_amd64.whl

---
+++ packages/gllm-inference-binary/gllm_inference_binary-0.5.1-cp312-cp312-win_amd64.whl/gllm_inference/constants.pyi
@@ -0,0 +1,7 @@
+from _typeshed import Incomplete
+
+DEFAULT_AZURE_OPENAI_API_VERSION: str
+DOCUMENT_MIME_TYPES: Incomplete
+GOOGLE_SCOPES: Incomplete
+HEX_REPR_LENGTH: int
+HTTP_STATUS_CODE_PATTERNS: Incomplete
