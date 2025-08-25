a8e7e9e5da2597237263e6e79052ff1e00ed0e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 04:10:23 +0000

    Add package testnewsdkwebdock/1.0.8/testnewsdkwebdock-1.0.8-py3-none-any.whl

---
+++ packages/testnewsdkwebdock/testnewsdkwebdock-1.0.8-py3-none-any.whl/test/test_account.py
@@ -0,0 +1,16 @@
+import os
+import unittest
+
+from dotenv import load_dotenv
+from webdock import Webdock
+load_dotenv()
+
+
+class TestAccount(unittest.TestCase):
+    def __init__(self, methodName = "runTest"):
+        super().__init__(methodName)
+        self.client = Webdock(os.getenv("TOKEN"))
+
+    def test_account_info(self):
+        account = self.client.account.info()
+        self.assertIsInstance(account.get("body").get("userId"), int, "userId should be of type int")
