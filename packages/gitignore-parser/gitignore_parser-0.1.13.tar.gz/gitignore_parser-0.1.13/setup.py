1ab4d34d62c17918a8775933df4d790386674c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 15:11:55 +0000

    Add package gitignore-parser/0.1.13/gitignore_parser-0.1.13.tar.gz

---
+++ packages/gitignore-parser/gitignore_parser-0.1.13.tar.gz/gitignore_parser-0.1.13/setup.py
@@ -0,0 +1,45 @@
+"""A spec-compliant gitignore parser for Python 3.5+
+
+See:
+https://github.com/mherrmann/gitignore_parser
+"""
+
+from setuptools import setup
+
+description = 'A spec-compliant gitignore parser for Python 3.5+'
+setup(
+    name='gitignore_parser',
+    version='0.1.13',
+    description=description,
+    long_description=
+        description + '\n\nhttps://github.com/mherrmann/gitignore_parser',
+    author='Michael Herrmann',
+    author_email='michael+removethisifyouarehuman@herrmann.io',
+    url='https://github.com/mherrmann/gitignore_parser',
+    py_modules=['gitignore_parser'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Operating System :: OS Independent',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    license='MIT',
+    keywords='gitignore',
+    platforms=['MacOS', 'Windows', 'Debian', 'Fedora', 'CentOS']
+)
