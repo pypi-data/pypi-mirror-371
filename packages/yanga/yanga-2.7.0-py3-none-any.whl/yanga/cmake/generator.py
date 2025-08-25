adb123efc783f0e785f13b37070908711a544cd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 03:29:59 +0000

    Add package yanga/2.7.0/yanga-2.7.0-py3-none-any.whl

---
+++ packages/yanga/yanga-2.7.0-py3-none-any.whl/yanga/cmake/generator.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Any, Optional
+
+from yanga.domain.execution_context import ExecutionContext
+
+from .cmake_backend import CMakeElement
+
+
+class CMakeGenerator(ABC):
+    """Base class for CMake generators."""
+
+    def __init__(self, execution_context: ExecutionContext, output_dir: Path, config: Optional[dict[str, Any]] = None) -> None:
+        self.execution_context = execution_context
+        self.config = config
+        self.output_dir = output_dir
+
+    @abstractmethod
+    def generate(self) -> list[CMakeElement]:
+        pass
