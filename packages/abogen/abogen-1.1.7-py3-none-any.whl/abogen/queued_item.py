47563f9b1620e268ba28ebae5f532cf0890f2275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 14:06:28 +0000

    Add package abogen/1.1.7/abogen-1.1.7-py3-none-any.whl

---
+++ packages/abogen/abogen-1.1.7-py3-none-any.whl/abogen/queued_item.py
@@ -0,0 +1,16 @@
+# represents a queued item - book, chapters, voice, etc.
+from dataclasses import dataclass
+
+
+@dataclass
+class QueuedItem:
+    file_name: str
+    lang_code: str
+    speed: float
+    voice: str
+    save_option: str
+    output_folder: str
+    subtitle_mode: str
+    output_format: str
+    total_char_count: int
+    replace_single_newlines: bool = False
