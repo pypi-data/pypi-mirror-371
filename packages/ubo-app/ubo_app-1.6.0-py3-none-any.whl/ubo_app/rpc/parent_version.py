34ac37ce9eb3f4344790eb32d9c5ae7653b5aef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 14:06:04 +0000

    Add package ubo-app/1.6.0/ubo_app-1.6.0-py3-none-any.whl

---
+++ packages/ubo-app/ubo_app-1.6.0-py3-none-any.whl/ubo_app/rpc/parent_version.py
@@ -0,0 +1,26 @@
+"""Get the version of the parent package using hatch."""
+
+import subprocess
+from pathlib import Path
+
+
+def get_version() -> str:
+    """Return the version of the parent package."""
+    root = Path().absolute().parent
+    while not any(i.name == 'pyproject.toml' for i in root.iterdir()):
+        root = root.parent
+    result = subprocess.run(
+        [
+            '/usr/bin/env',
+            'uvx',
+            '--with',
+            'pip',
+            'hatch',
+            'version',
+        ],
+        check=True,
+        text=True,
+        cwd=root,
+        capture_output=True,
+    )
+    return result.stdout.strip()
