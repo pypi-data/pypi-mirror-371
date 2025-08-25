f528d9c8c23ceb75a2a3a0e096c4a045401843d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 14:05:40 +0000

    Add package memory-graph/0.3.51/memory_graph-0.3.51.tar.gz

---
+++ packages/memory-graph/memory_graph-0.3.51.tar.gz/memory_graph-0.3.51/images/create_images.sh
@@ -0,0 +1,42 @@
+# intro
+python many_types.py
+
+# debugging
+python debugging.py
+bash create_gif.sh debugging
+
+# data model
+python immutable.py
+python mutable.py
+python copy_mutable.py
+python copy_method.py
+python copy_immutable.py
+python copy_mix.py
+python name_rebinding.py
+
+# call stack
+python add_one.py
+python factorial.py
+bash create_gif.sh factorial
+python binary.py
+bash create_gif.sh binary
+python power_set.py
+bash create_gif.sh power_set
+
+# datastructures
+python linked_list.py
+python bin_tree.py
+python hash_set.py
+
+# configuration
+python not_node_types.py
+
+# introspection
+python avltree.py
+python bin_search.py
+python introspect_depth.py
+python hidden_edges.py
+
+# extensions
+python extension_numpy.py
+python extension_pandas.py
