d59b0e6938b5fe853fa93c101a48b1a94e703d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 15:12:33 +0000

    Add package ibauth/0.0.6/ibauth-0.0.6-py3-none-any.whl

---
+++ packages/ibauth/ibauth-0.0.6-py3-none-any.whl/ibauth/const.py
@@ -0,0 +1,14 @@
+CLIENT_ASSERTION_TYPE = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer"
+SCOPE = "sso-sessions.write"
+GRANT_TYPE = "client_credentials"
+
+VALID_DOMAINS = [
+    "api.ibkr.com",
+    "1.api.ibkr.com",
+    "2.api.ibkr.com",
+    "3.api.ibkr.com",
+    "4.api.ibkr.com",
+    "5.api.ibkr.com",
+    "6.api.ibkr.com",
+    "7.api.ibkr.com",
+]
