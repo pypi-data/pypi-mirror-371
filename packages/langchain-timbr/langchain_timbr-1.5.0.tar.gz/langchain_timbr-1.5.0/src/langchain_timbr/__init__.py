5e2dd7f6e58f6157ed392164e8c4cbc5bf1c14fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 04:09:19 +0000

    Add package langchain-timbr/1.5.0/langchain_timbr-1.5.0.tar.gz

---
+++ packages/langchain-timbr/langchain_timbr-1.5.0.tar.gz/langchain_timbr-1.5.0/src/langchain_timbr/__init__.py
@@ -0,0 +1,17 @@
+#
+#             *###              .,              @%             
+#       *%##  `#// %%%*  *@     ``              @%             
+#        #*.    * .%%%`  @@@@*  @@   @@@@,@@@@  @&@@@@   .&@@@*
+#            #%%#   ..   *@     @@  @`  @@` ,@  @%   #@  @@  
+#      ,, .,%(##/./%%#,  *@     @@  @`  @@` ,@  @%   #@  @@   
+#    ,%##%          ``   `/@@*  @@  @`  @@` ,@  (/@@@#/  @@   
+#      ``                                                     
+#  ``````````````````````````````````````````````````````````````
+#  Copyright (C) 2018-2025 timbr.ai
+
+__version__ = "1.4.3"
+from .timbr_llm_connector import TimbrLlmConnector
+from .llm_wrapper.llm_wrapper import LlmWrapper, LlmTypes
+
+from .langchain import *
+from .langgraph import *
