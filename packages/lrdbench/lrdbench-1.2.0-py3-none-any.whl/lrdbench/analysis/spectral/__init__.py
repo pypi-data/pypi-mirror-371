0d08fa402c9edd8d77f2c0b4ac5d9bcbbd6fb295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 03:29:30 +0000

    Add package lrdbench/1.2.0/lrdbench-1.2.0-py3-none-any.whl

---
+++ packages/lrdbench/lrdbench-1.2.0-py3-none-any.whl/lrdbench/analysis/spectral/__init__.py
@@ -0,0 +1,25 @@
+"""
+Spectral estimators package.
+
+Contains frequency-domain estimators such as Periodogram, Whittle, and GPH.
+"""
+
+__all__ = [
+    "periodogram",
+    "whittle",
+    "gph",
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ packages/lrdbench/lrdbench-1.2.0-py3-none-any.whl/lrdbench/models/data_models/__init__.py
@@ -0,0 +1,10 @@
+"""
+Data models package containing implementations of stochastic processes.
+
+This package provides classes for generating synthetic data from various
+stochastic models including ARFIMA, fBm, fGn, and MRW.
+"""
+
+from .base_model import BaseModel
+
+__all__ = ['BaseModel']
