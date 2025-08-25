6d74f5519959fa3b88170ab0dfb1bd6d94d33389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 14:06:15 +0000

    Add package frccontrol/2025.6/frccontrol-2025.6-py3-none-any.whl

---
+++ packages/frccontrol/frccontrol-2025.6-py3-none-any.whl/frccontrol/__init__.py
@@ -0,0 +1,14 @@
+"""The WPILib state-space control library."""
+
+from .ctrlutil import *
+from .discretization import *
+from .extended_kalman_filter import *
+from .kalman_filter import *
+from .linear_plant_inversion_feedforward import *
+from .linear_quadratic_regulator import *
+from .models import *
+from .numerical_integration import *
+from .numerical_jacobian import *
+from .plotutil import *
+from .profiles import *
+from .trajectory import *
