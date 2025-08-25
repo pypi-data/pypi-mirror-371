c3ec7755858f971e9b3fe1f771b6d69dcf14e30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 14:05:39 +0000

    Add package jsonschema-diff/0.1.1/jsonschema_diff-0.1.1.tar.gz

---
+++ packages/jsonschema-diff/jsonschema_diff-0.1.1.tar.gz/jsonschema_diff-0.1.1/jsonschema_diff/core/__init__.py
@@ -0,0 +1,7 @@
+from .abstraction import Statuses, ToCompare
+from .config import Config
+from .parameter_base import Compare
+from .parameter_combined import CompareCombined
+from .property import Property
+
+__all__ = ["Statuses", "ToCompare", "Compare", "CompareCombined", "Property", "Config"]
