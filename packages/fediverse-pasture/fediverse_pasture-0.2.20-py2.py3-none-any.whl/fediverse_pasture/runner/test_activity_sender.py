c6c0a2cad01f3663bbb0b930173a8f013babfdbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 04:10:09 +0000

    Add package fediverse-pasture/0.2.20/fediverse_pasture-0.2.20-py2.py3-none-any.whl

---
+++ packages/fediverse-pasture/fediverse_pasture-0.2.20-py2.py3-none-any.whl/fediverse_pasture/runner/test_activity_sender.py
@@ -0,0 +1,81 @@
+# SPDX-FileCopyrightText: 2023 Helge
+#
+# SPDX-License-Identifier: MIT
+
+import pytest
+import json
+from datetime import datetime
+from unittest.mock import AsyncMock
+
+from fediverse_pasture.data_provider import DataProvider, bovine_actor_for_actor_data
+
+from . import ActivitySender
+
+
+@pytest.fixture
+def activity_sender():
+    dp = DataProvider.generate(with_possible_actors=False)
+
+    bovine_actor, actor_object = bovine_actor_for_actor_data(
+        "http://localhost/actor", dp.one_actor
+    )
+
+    yield ActivitySender.for_actor(bovine_actor, actor_object)
+
+
+def test_activity_sender(activity_sender):
+    activity_sender.init_create_note(lambda x: {**x, "content": "text"})
+
+    assert isinstance(activity_sender.published, datetime)
+    assert isinstance(activity_sender.note, dict)
+
+    obj = activity_sender.note
+
+    assert obj.get("content") == "text"
+
+    text = json.dumps(activity_sender.note)
+
+    assert len(text) > 20
+
+
+async def test_activity_sender_modifying_to(activity_sender):
+    activity_sender.init_create_note(lambda x: {**x})
+    assert activity_sender.note.get("to") == ["as:Public"]
+
+    activity_sender.init_create_note(lambda x: {**x, "to": ["Public"]})
+    assert activity_sender.note.get("to") == ["Public"]
+
+    activity_sender.bovine_actor.post = AsyncMock()
+    activity_sender.bovine_actor.get = AsyncMock(
+        return_value={"inbox": "https://remote.example/inbox"}
+    )
+
+    await activity_sender.send("https://remote.example")
+
+    activity_sender.bovine_actor.get.assert_awaited_once()
+    assert activity_sender.bovine_actor.get.await_args == [("https://remote.example",)]
+    activity_sender.bovine_actor.post.assert_awaited_once()
+
+    data = activity_sender.bovine_actor.post.await_args[0][1]
+
+    assert set(data["to"]) == {"https://remote.example", "Public"}
+
+
+async def test_activity_sender_mention(activity_sender):
+    activity_sender.include_mention = True
+    activity_sender.init_create_note(lambda x: {**x})
+    assert activity_sender.note.get("to") == ["as:Public"]
+
+    activity_sender.bovine_actor.post = AsyncMock()
+    activity_sender.bovine_actor.get = AsyncMock(
+        return_value={"inbox": "https://remote.example/inbox"}
+    )
+
+    await activity_sender.send("https://remote.example")
+    data = activity_sender.bovine_actor.post.await_args[0][1]
+
+    assert set(data["to"]) == {"https://remote.example", "as:Public"}
+
+    obj = data.get("object")
+    assert len(obj["tag"]) == 1
+    assert obj["tag"][0] == {"type": "Mention", "href": "https://remote.example"}
---
+++ packages/fediverse-pasture/fediverse_pasture-0.2.20-py2.py3-none-any.whl/fediverse_pasture/send/test_modifier.py
@@ -0,0 +1,29 @@
+import pytest
+
+from .modifier import ModifierBuilder
+
+
+@pytest.mark.parametrize(
+    ["text", "input_name", "input_number"],
+    [("text", "name", 1), (None, None, 1), (None, "name", None)],
+)
+def test_invalid_arguments(text, input_name, input_number):
+    with pytest.raises(ValueError):
+        ModifierBuilder(text=text, input_name=input_name, input_number=input_number)
+
+
+def test_build_with_text():
+    builder = ModifierBuilder(text="Hello, World!")
+    modifier = builder.build()
+
+    assert modifier({}) == {"content": "Hello, World!"}
+
+
+def test_with_input():
+    builder = ModifierBuilder(input_name="hashtags", input_number=1)
+    modifier = builder.build()
+
+    assert modifier({}) == {
+        "content": "text",
+        "tag": {"name": "nohash", "type": "Hashtag"},
+    }
