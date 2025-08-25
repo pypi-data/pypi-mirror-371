aea35e09d9923177f1766936a3f7d1d9bd6571c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 06:06:00 +0000

    Add package listpick/0.1.14.6/listpick-0.1.14.6.tar.gz

---
+++ packages/listpick/listpick-0.1.14.6.tar.gz/listpick-0.1.14.6/src/listpick/ui/keycodes.py
@@ -0,0 +1,70 @@
+# Define constants for alt+a to alt+z
+META_A = 1000
+META_B = 1001
+META_C = 1002
+META_D = 1003
+META_E = 1004
+META_F = 1005
+META_G = 1006
+META_H = 1007
+META_I = 1008
+META_J = 1009
+META_K = 1010
+META_L = 1011
+META_M = 1012
+META_N = 1013
+META_O = 1014
+META_P = 1015
+META_Q = 1016
+META_R = 1017
+META_S = 1018
+META_T = 1019
+META_U = 1020
+META_V = 1021
+META_W = 1022
+META_X = 1023
+META_Y = 1024
+META_Z = 1025
+
+# Define constants for alt+A to alt+Z (using uppercase)
+META_a = 1050
+META_b = 1051
+META_c = 1052
+META_d = 1053
+META_e = 1054
+META_f = 1055
+META_g = 1056
+META_h = 1057
+META_i = 1058
+META_j = 1059
+META_k = 1060
+META_l = 1061
+META_m = 1062
+META_n = 1063
+META_o = 1064
+META_p = 1065
+META_q = 1066
+META_r = 1067
+META_s = 1068
+META_t = 1069
+META_u = 1070
+META_v = 1071
+META_w = 1072
+META_x = 1073
+META_y = 1074
+META_z = 1075
+
+# Dictionary to map character to its constant value
+META_KEY_MAP = {
+    'a': META_a, 'b': META_b, 'c': META_c, 'd': META_d, 'e': META_e,
+    'f': META_f, 'g': META_g, 'h': META_h, 'i': META_i, 'j': META_j,
+    'k': META_k, 'l': META_l, 'm': META_m, 'n': META_n, 'o': META_o,
+    'p': META_p, 'q': META_q, 'r': META_r, 's': META_s, 't': META_t,
+    'u': META_u, 'v': META_v, 'w': META_w, 'x': META_x, 'y': META_y,
+    'z': META_z, 'A': META_A, 'B': META_B, 'C': META_C, 'D': META_D,
+    'E': META_E, 'F': META_F, 'G': META_G, 'H': META_H, 'I': META_I,
+    'J': META_J, 'K': META_K, 'L': META_L, 'M': META_M, 'N': META_N,
+    'O': META_O, 'P': META_P, 'Q': META_Q, 'R': META_R, 'S': META_S,
+    'T': META_T, 'U': META_U, 'V': META_V, 'W': META_W, 'X': META_X,
+    'Y': META_Y, 'Z': META_Z
+}
