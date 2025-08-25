00903af9873303997ab1add9c20e4a31f484be40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 04:09:46 +0000

    Add package jk_prettyprintobj/0.2025.8.14/jk_prettyprintobj-0.2025.8.14-py3-none-any.whl

---
+++ packages/jk_prettyprintobj/jk_prettyprintobj-0.2025.8.14-py3-none-any.whl/jk_prettyprintobj/__init__.py
@@ -0,0 +1,53 @@
+
+
+
+__author__ = "Jürgen Knauth"
+__version__ = "0.2025.8.14"
+__email__ = "pubsrc@binary-overflow.de"
+__license__ = "Apache2"
+__copyright__ = "Copyright (c) 2020-2025, Jürgen Knauth"
+
+
+__all__ = (
+	"DumperSettings",
+	"RawValue",
+	"DumpMixin",
+	"Dumper",
+	"DumpCtx",
+	"DEFAULT_DUMPER_SETTINGS",
+	"pprint",
+	"NamedTupleDumpMixinMeta",
+)
+
+
+
+
+
+from .DumperSettings import DumperSettings
+from .RawValue import RawValue
+from .dumper import DumpMixin, Dumper, DumpCtx, DEFAULT_DUMPER_SETTINGS
+from .NamedTupleDumpMixinMeta import NamedTupleDumpMixinMeta
+
+from builtins import print as _print
+
+
+
+
+#
+# Print any value in a human readable way.
+#
+def pprint(something, printFunc = None):
+	if printFunc is None:
+		printFunc = _print
+	else:
+		assert callable(printFunc)
+
+	dumper = Dumper()
+	with dumper.createContext(None, "") as dumper2:
+		dumper2._dumpX("", something)
+	dumper.print(printFunc)
+#
+
+
+
+
