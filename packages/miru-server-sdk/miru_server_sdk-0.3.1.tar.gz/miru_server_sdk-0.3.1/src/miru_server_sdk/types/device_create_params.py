9be31df88454047820c917c2020fdc9b235372f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 15:12:35 +0000

    Add package miru-server-sdk/0.3.1/miru_server_sdk-0.3.1.tar.gz

---
+++ packages/miru-server-sdk/miru_server_sdk-0.3.1.tar.gz/miru_server_sdk-0.3.1/src/miru_server_sdk/types/device_create_params.py
@@ -0,0 +1,16 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Required, TypedDict
+
+__all__ = ["DeviceCreateParams"]
+
+
+class DeviceCreateParams(TypedDict, total=False):
+    name: Required[str]
+    """The name of the device.
+
+    Use $HOSTNAME to postpone device naming until activation, where the device's
+    hostname will be used.
+    """
