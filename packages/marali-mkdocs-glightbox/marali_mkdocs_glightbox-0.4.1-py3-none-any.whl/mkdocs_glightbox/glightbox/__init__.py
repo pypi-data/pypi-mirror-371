521fda2763ee9545bd44c0a37ba7b8808ae2bb92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 16:07:06 +0000

    Add package marali-mkdocs-glightbox/0.4.1/marali_mkdocs_glightbox-0.4.1-py3-none-any.whl

---
+++ packages/marali-mkdocs-glightbox/marali_mkdocs_glightbox-0.4.1-py3-none-any.whl/mkdocs_glightbox/glightbox/__init__.py
@@ -0,0 +1,6 @@
+"""Asset subpackage for mkdocs_glightbox.
+
+Contains the vendored GLightbox JavaScript/CSS distributed as package data.
+This module intentionally exposes no Python API; it only ensures the directory
+is treated as a proper package to silence setuptools discovery warnings.
+"""
