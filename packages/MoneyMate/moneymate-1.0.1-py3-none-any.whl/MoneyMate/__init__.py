eba0ee40928a76d4b809cdcc83821fa5349efefe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 04:09:42 +0000

    Add package MoneyMate/1.0.1/moneymate-1.0.1-py3-none-any.whl

---
+++ packages/MoneyMate/moneymate-1.0.1-py3-none-any.whl/MoneyMate/__init__.py
@@ -0,0 +1,16 @@
+import logging
+
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger('MoneyMate')
+
+# this is the initial module of your app
+# this is executed whenever some client-code is calling `import MoneyMate` or `from MoneyMate import ...`
+# put your main classes here, eg:
+class MyClass:
+    def my_method(self):
+        return "Hello World"
+
+
+# let this be the last line of this file
+logger.info("MoneyMate loaded")
