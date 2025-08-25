96d2a2031937f0e9b46553cadd6b54b68197d789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 14:06:11 +0000

    Add package wai-logging/0.0.5/wai_logging-0.0.5.tar.gz

---
+++ packages/wai-logging/wai_logging-0.0.5.tar.gz/wai_logging-0.0.5/CHANGES.rst
@@ -0,0 +1,35 @@
+Changelog
+=========
+
+0.0.5 (2025-08-25)
+------------------
+
+- `add_logging_level` method now allows specifying the `destination` in the parsed namespace
+- `add_logger_name` method now allows specifying the `destination` in the parse namespace,
+  and either short or long option flag can be specified
+
+
+0.0.4 (2025-08-25)
+------------------
+
+- `add_logging_level` method no longer requires short and long option flag to be specified together,
+  one of them can be omitted (ie None)
+
+
+0.0.3 (2025-01-14)
+------------------
+
+- using underscore in project name now
+
+
+0.0.2 (2025-01-14)
+------------------
+
+- `init_logging` now supports specifying `stream` or `filename` or `handlers` as well as the `log_format`
+
+
+0.0.1 (2023-11-30)
+------------------
+
+- initial release
+
---
+++ packages/wai-logging/wai_logging-0.0.5.tar.gz/wai_logging-0.0.5/src/wai/logging/_argparse.py
@@ -0,0 +1,69 @@
+import argparse
+from typing import Optional
+
+from ._core import LOGGING_LEVELS, LOGGING_WARN
+
+
+DEFAULT_LOGGING_LEVEL_HELP = "The logging level to use."
+DEFAULT_LOGGER_NAME_HELP = "The custom name to use for the logger."
+
+
+def add_logging_level(parser: argparse.ArgumentParser, short_opt: Optional[str] = "-l",
+                      long_opt: Optional[str] = "--logging_level", dest: str = "logging_level",
+                      help_str: str = None):
+    """
+    Adds an option for the logging level to the parser.
+    Only one of the two must be supplied: short_opt or long_opt.
+
+    :param parser: the parser to append.
+    :type parser: argparse.ArgumentParser
+    :param short_opt: the short option flag to use (eg -l), ignored if None
+    :type short_opt: str
+    :param long_opt: the long option flag to use (eg --logging_level), ignored if None
+    :type long_opt: str
+    :param dest: the name to use in the parsed namespace
+    :type dest: str
+    :param help_str: the help string to use
+    :type help_str: str
+    """
+    if help_str is None:
+        help_str = DEFAULT_LOGGING_LEVEL_HELP
+    if (short_opt is None) and (long_opt is None):
+        raise Exception("Either short or long option must be supplied at least!")
+    elif (short_opt is not None) and (long_opt is not None):
+        parser.add_argument(short_opt, long_opt, dest=dest, choices=LOGGING_LEVELS, default=LOGGING_WARN, help=help_str)
+    elif short_opt is not None:
+        parser.add_argument(short_opt, dest=dest, choices=LOGGING_LEVELS, default=LOGGING_WARN, help=help_str)
+    else:
+        parser.add_argument(long_opt, dest=dest, choices=LOGGING_LEVELS, default=LOGGING_WARN, help=help_str)
+
+
+def add_logger_name(parser: argparse.ArgumentParser, short_opt: str = "-N", long_opt: str = "--logger_name",
+                    dest: str = "logger_name", default_name: str = None, help_str: str = None):
+    """
+    Adds an option for the logger name to the parser.
+    Only one of the two must be supplied: short_opt or long_opt.
+
+    :param parser: the parser to append.
+    :type parser: argparse.ArgumentParser
+    :param short_opt: the short option flag to use (eg -N), ignored if None
+    :type short_opt: str
+    :param long_opt: the long option flag to use (eg --logger_name), ignored if None
+    :type long_opt: str
+    :param dest: the name to use in the parsed namespace
+    :type dest: str
+    :param default_name: the default name for the logger
+    :type default_name: str
+    :param help_str: the help string to use
+    :type help_str: str
+    """
+    if help_str is None:
+        help_str = DEFAULT_LOGGER_NAME_HELP
+    if (short_opt is None) and (long_opt is None):
+        raise Exception("Either short or long option must be supplied at least!")
+    elif (short_opt is not None) and (long_opt is not None):
+        parser.add_argument(short_opt, long_opt, type=str, dest=dest, default=default_name, help=help_str, required=False)
+    elif short_opt is not None:
+        parser.add_argument(short_opt, type=str, dest=dest, default=default_name, help=help_str, required=False)
+    else:
+        parser.add_argument(long_opt, type=str, dest=dest, default=default_name, help=help_str, required=False)
