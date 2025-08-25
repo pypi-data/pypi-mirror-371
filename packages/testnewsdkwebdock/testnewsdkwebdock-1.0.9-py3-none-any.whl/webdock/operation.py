fb5610a790cf71ededecc306410967d88d5ebf17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 04:09:21 +0000

    Add package testnewsdkwebdock/1.0.9/testnewsdkwebdock-1.0.9-py3-none-any.whl

---
+++ packages/testnewsdkwebdock/testnewsdkwebdock-1.0.9-py3-none-any.whl/webdock/operation.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from typing import List, Literal, TypedDict
+
+from webdock.utils import RequestOptions, req
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from webdock import Webdock
+
+class EventLog(TypedDict):
+    id: int
+    startTime: str
+    endTime: str | None
+    callbackId: str
+    serverSlug: str
+    eventType: str
+    action: str
+    actionData: str
+    status: Literal["waiting", "working", "finished", "error"]
+    message: str
+
+
+class EventLogResponse(TypedDict):
+    body: List[EventLog]
+
+
+class Operation:
+    def __init__(self, parent: "Webdock") -> None:
+        self.parent = parent
+
+    def fetch(self, callbackId: str):
+        return req(
+            RequestOptions(
+                token=self.parent.string_token,
+                endpoint=f"/events?callbackId={callbackId}",
+                method="GET",
+            ),
+            EventLogResponse
+        )
+
+
