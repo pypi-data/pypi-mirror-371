2a092d64c886a2a629c610d9699aeee37202e4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 14:06:43 +0000

    Add package srctools/2.6.1/srctools-2.6.1-cp310-cp310-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/srctools/srctools-2.6.1-cp310-cp310-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/srctools/property_parser.py
@@ -0,0 +1,36 @@
+"""Deprecated, this was renamed to srctools.keyvalues."""
+from typing import TYPE_CHECKING
+import warnings
+
+
+__all__ = ['KeyValError', 'NoKeyError', 'Property']
+
+warnings.warn(
+    'srctools.property_parser module was deprecated, import srctools.keyvalues instead.',
+    DeprecationWarning,
+    stacklevel=2,
+)
+
+if TYPE_CHECKING:
+    from srctools.keyvalues import (
+        KeyValError as KeyValError, Keyvalues as Property, NoKeyError as NoKeyError,
+    )
+else:
+    from srctools import keyvalues
+
+    def __getattr__(name: str) -> object:
+        if name == 'Property':
+            warnings.warn(
+                'srctools.property_parser.Property is renamed to srctools.keyvalues.Keyvalues',
+                DeprecationWarning,
+                stacklevel=2,
+            )
+            return keyvalues.Keyvalues
+        elif name in ('KeyValError', 'NoKeyError'):
+            warnings.warn(
+                f'srctools.property_parser.{name} is renamed to srctools.keyvalues.{name}',
+                DeprecationWarning,
+                stacklevel=2,
+            )
+            return getattr(keyvalues, name)
+        raise AttributeError(name)
