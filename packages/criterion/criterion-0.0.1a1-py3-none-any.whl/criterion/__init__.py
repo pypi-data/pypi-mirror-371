57bf1b15066f5115bae650e22605c18acf74ecc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 03:29:21 +0000

    Add package criterion/0.0.1a1/criterion-0.0.1a1-py3-none-any.whl

---
+++ packages/criterion/criterion-0.0.1a1-py3-none-any.whl/criterion/__init__.py
@@ -0,0 +1,10 @@
+"""Criterion - A Python package for Criterion"""
+
+__version__ = "0.0.1a1"
+__author__ = "Criterion Team"
+__email__ = "criterion@criterion.org"
+
+
+def hello():
+    """A simple hello function"""
+    return f"Hello from Criterion v{__version__}!"
