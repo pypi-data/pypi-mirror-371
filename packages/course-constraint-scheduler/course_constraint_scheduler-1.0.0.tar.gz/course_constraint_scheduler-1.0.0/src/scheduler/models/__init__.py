c97f51ebde8a74d082cd52182a04d8a5e4a19119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 14:05:39 +0000

    Add package course-constraint-scheduler/1.0.0/course_constraint_scheduler-1.0.0.tar.gz

---
+++ packages/course-constraint-scheduler/course_constraint_scheduler-1.0.0.tar.gz/course_constraint_scheduler-1.0.0/src/scheduler/models/__init__.py
@@ -0,0 +1,15 @@
+from .identifiable import Identifiable
+from .day import Day
+from .course import Course, CourseInstance
+from .time_slot import TimeSlot, TimeInstance, TimePoint, Duration
+
+__all__ = [
+    "Identifiable",
+    "Day",
+    "Course",
+    "CourseInstance",
+    "TimeSlot",
+    "TimeInstance",
+    "TimePoint",
+    "Duration",
+]
