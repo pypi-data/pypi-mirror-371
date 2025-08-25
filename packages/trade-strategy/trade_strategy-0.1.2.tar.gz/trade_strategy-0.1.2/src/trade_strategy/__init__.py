16072275af0b3af45388cc8050a5b1d9f8aab432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 04:09:20 +0000

    Add package trade-strategy/0.1.2/trade_strategy-0.1.2.tar.gz

---
+++ packages/trade-strategy/trade_strategy-0.1.2.tar.gz/trade_strategy-0.1.2/src/trade_strategy/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2025-present zerubabeldinsa <zerubabel.dinsa@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from .portfolio_construction.return_allocations.efficient_frontier import risk_return
