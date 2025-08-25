7208ae4d6ab4a10551a6f774a78408398961cc4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 14:06:43 +0000

    Add package srctools/2.6.1/srctools-2.6.1-pp310-pypy310_pp73-macosx_10_15_x86_64.whl

---
+++ packages/srctools/srctools-2.6.1-pp310-pypy310_pp73-macosx_10_15_x86_64.whl/srctools/logger.py
@@ -0,0 +1,517 @@
+"""
+Wrapper around logging to provide our own functionality.
+
+This adds the ability to log using str.format() instead of %.
+"""
+from typing import Literal, TYPE_CHECKING, Any, ClassVar, Optional, TextIO, Union, cast, overload
+from collections.abc import Callable, Generator, Iterable, Mapping
+from io import StringIO
+from pathlib import Path
+from types import TracebackType
+import contextlib
+import contextvars
+import logging
+import os
+import sys
+import traceback
+
+from srctools import StringPath
+
+
+__all__ = ['LoggerAdapter', 'get_handler', 'get_logger', 'init_logging', 'context']
+# Only generic in stubs!
+CTX_STACK: 'contextvars.ContextVar[list[str]]' = contextvars.ContextVar('srctools_logger')
+
+
+class LogMessage:
+    """Allow using str.format() in logging messages.
+
+    The __str__() method performs the joining.
+    """
+    fmt: str
+    args: tuple[object, ...]
+    kwargs: dict[str, object]
+    has_args: bool
+
+    def __init__(
+        self,
+        fmt: str,
+        args: tuple[object, ...],
+        kwargs: dict[str, object],
+    ) -> None:
+        self.fmt = fmt
+        self.args = args
+        self.kwargs = kwargs
+        self.has_args = bool(kwargs or args)
+
+    def format_msg(self) -> str:
+        """Format using str.format."""
+        # Only format if we have arguments!
+        # That way { or } can be used in regular messages.
+        if self.has_args:
+            f = self.fmt = str(self.fmt).format(*self.args, **self.kwargs)
+
+            # Don't repeat the formatting, and don't keep refs to the args.
+            del self.args, self.kwargs
+            self.has_args = False
+            return f
+        else:
+            return str(self.fmt)
+
+    def __str__(self) -> str:
+        """Format the string, and add an ASCII indent."""
+        msg = self.format_msg()
+
+        if '\n' not in msg:
+            return msg
+
+        # For multi-line messages, add an indent so that they're associated
+        # with the logging tag.
+        lines = msg.split('\n')
+        if lines[-1].isspace():
+            # Strip last line if it's blank
+            del lines[-1]
+        # [I] A multi-line message looks like the following:
+        # | blah
+        # | blah
+        # |___
+        #
+        return '\n | '.join(lines[:]) + '\n |___\n'
+
+
+_SysExcInfoType = Union[
+    tuple[type[BaseException], BaseException, Optional[TracebackType]],
+    tuple[None, None, None]
+]
+if TYPE_CHECKING:  # Only generic in stubs.
+    _AdapterBase = logging.LoggerAdapter[logging.Logger]
+else:
+    _AdapterBase = logging.LoggerAdapter
+
+
+class LoggerAdapter(_AdapterBase):
+    """Fix loggers to use str.format().
+
+    """
+    logger: logging.Logger  #: The logger this wraps.
+    alias: Optional[str]  #: If set, this is a replacement module name for log messages.
+    def __init__(self, logger: logging.Logger, alias: Optional[str] = None) -> None:
+        self.alias = alias
+        self.logger = logger
+        logging.LoggerAdapter.__init__(self, logger, extra={})
+
+    def log(
+        self,
+        level: int,
+        msg: Any,
+        *args: Any,
+        exc_info: Union[bool, _SysExcInfoType, BaseException, None] = None,
+        stack_info: bool = False,
+        extra: Optional[Mapping[str, object]] = None,
+        stacklevel: int = 0,
+        **kwargs: Any,
+    ) -> None:
+        """This version of :external:py:meth:`~logging.Logger.log()` is for :external:py:meth:`str.format()` compatibility.
+
+        The message is wrapped in a :py:class:`LogMessage` object, which is given the
+        ``args`` and ``kwargs``.
+        """
+        if self.isEnabledFor(level):
+            try:
+                ctx = ', '.join(CTX_STACK.get())
+            except LookupError:
+                ctx = ''
+
+            new_extra = {} if extra is None else dict(extra)
+            new_extra['_srctools_alias'] = self.alias
+            new_extra['srctools_context'] = f' ({ctx})' if ctx else ''
+
+            # Handle some extra indirection in 3.10+
+            if sys.version_info >= (3, 10):
+                stacklevel += 2
+
+            # noinspection PyProtectedMember
+            self.logger._log(
+                level,
+                LogMessage(str(msg), args, kwargs),
+                (),  # No positional arguments, we do the formatting through LogMessage.
+                # Pull out of kwargs, so log can handle them specially.
+                extra=new_extra,
+                exc_info=exc_info,
+                stack_info=stack_info,
+                stacklevel=stacklevel,
+            )
+
+    def __getattr__(self, attr: str) -> Any:
+        """Delegate unknown methods to the logger."""
+        return getattr(self.logger, attr)
+
+
+class Formatter(logging.Formatter):
+    """Override exception handling."""
+    SKIP_LIBS: ClassVar[list[str]] = ['importlib', 'cx_freeze', 'PyInstaller']
+    def __init__(
+        self,
+        template: str,
+        *,
+        expand_exc_groups: bool=False,
+        style: Literal['{']='{',  # Here only for backwards compat.
+    ) -> None:
+        super().__init__(template, style=style)
+        self.expand_exc_groups = expand_exc_groups
+
+    def formatException(self, ei: _SysExcInfoType) -> str:
+        """Ignore importlib, cx_freeze and PyInstaller."""
+        exc_type, exc_value, exc_tb = ei
+        buffer = StringIO()
+
+        trace: Optional[TracebackType] = exc_tb
+
+        try:
+            while trace is not None:
+                filename = trace.tb_frame.f_code.co_filename.casefold()
+                if all(keyword not in filename for keyword in self.SKIP_LIBS):
+                    break
+                trace = trace.tb_next
+
+            if trace is None:
+                # It's to do with these libraries themselves? Show the full
+                # traceback.
+                trace = exc_tb
+        except Exception:  # Something failed, keep the original.
+            trace = exc_tb
+
+        if exc_type is not None and exc_value is not None:
+            # Include much more of exception groups in the log files.
+            # Mypy needs version check first.
+            if sys.version_info >= (3, 11) and self.expand_exc_groups:
+                tb_exc = traceback.TracebackException(
+                    exc_type, exc_value, trace,
+                    max_group_depth=64,
+                    max_group_width=100,
+                )
+            else:
+                tb_exc = traceback.TracebackException(exc_type, exc_value, trace)
+            for line in tb_exc.format():
+                buffer.write(line)
+
+        return buffer.getvalue().rstrip('\n')
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Ensure a default context is set in the record."""
+        record.__dict__.setdefault('srctools_context', '')
+        return super().format(record)
+
+
+def get_handler(filename: Union[str, os.PathLike[str]]) -> logging.FileHandler:
+    """Cycle log files, then give the required file handler."""
+    path = Path(filename)
+    ext = ''.join(path.suffixes)
+    suffixes = ('.5', '.4', '.3', '.2', '.1', '')
+
+    try:
+        # Remove the oldest one.
+        path.with_suffix(suffixes[0] + ext).unlink(missing_ok=True)
+
+        # Go in reverse, moving each file over to give us space.
+        for frm, to in zip(suffixes[1:], suffixes):
+            try:
+                path.with_suffix(frm + ext).rename(path.with_suffix(to + ext))
+            except FileNotFoundError:
+                pass
+
+        try:
+            return logging.FileHandler(path, mode='x', encoding='utf8')
+        except FileExistsError:
+            pass
+    except PermissionError:
+        pass
+
+    # On windows, we can't touch files opened by other programs (ourselves).
+    # If another copy of us is open, it'll hold access.
+    # In that case, just keep trying suffixes until we find an empty file.
+    ind = 1
+    while True:
+        try:
+            return logging.FileHandler(path.with_suffix(f'.{ind}{ext}'), mode='x', encoding='utf8')
+        except (FileExistsError, PermissionError):
+            pass
+        ind += 1
+
+
+class NullStream(TextIO):
+    """A stream object that discards all data.
+
+    This is needed for multiprocessing, since it tries to flush stdout.
+    That'll fail if it is None.
+    """
+    def __enter__(self) -> 'NullStream':
+        return self
+
+    def __exit__(
+        self,
+        typ: Optional[type[BaseException]],
+        value: Optional[BaseException],
+        tback: Optional[TracebackType],
+    ) -> None:
+        return None
+
+    def __next__(self) -> str:
+        raise StopIteration
+
+    def __iter__(self) -> 'NullStream':
+        return self
+
+    def close(self) -> None:
+        """Closing does nothing."""
+
+    def isatty(self) -> bool:
+        """We are not a TTY."""
+        return False
+
+    def readable(self) -> bool:
+        """Pretend we are readable."""
+        return True
+
+    def seekable(self) -> bool:
+        """Pretend we are seekable."""
+        return True
+
+    def writable(self) -> bool:
+        """Pretend we are writable."""
+        return True
+
+    def fileno(self) -> int:
+        """We do not have a file number."""
+        raise OSError('No file number.')
+
+    def flush(self) -> None:
+        """Flushing does nothing."""
+
+    def read(self, size: Optional[int] = None) -> str:
+        """We never have data."""
+        return ''
+
+    def readline(self, limit: int = -1) -> str:
+        """We never have data."""
+        return ''
+
+    def readlines(self, hint: int = -1) -> list[str]:
+        """We never have data."""
+        return []
+
+    def write(self, text: str) -> int:
+        """Write nothing to the file."""
+        return 0
+
+    def writelines(self, lines: Iterable[str]) -> None:
+        """Write nothing to the file."""
+        pass
+
+    def seek(self, offset: int, whence: int = 0) -> int:
+        """Seeking does nothing."""
+        return 0
+
+    def tell(self) -> int:
+        """We are always at the start position."""
+        return 0
+
+    def truncate(self, size: Optional[int] = None) -> int:
+        """Truncation does nothing."""
+        return 0
+
+
+class NewLogRecord(logging.LogRecord):
+    """Allow passing an alias and context for log modules."""
+    _srctools_alias: Optional[str] = None
+    # Can be used by formatters.
+    srctools_context: str = ''
+    module: str
+
+    def getMessage(self) -> str:
+        """We have to hook here to change the value of .module.
+
+        It's called just before the formatting call is made.
+        """
+        if self._srctools_alias is not None:
+            self.module = self._srctools_alias
+        # If this is one of our logs it {}-formats, otherwise it %-formats.
+        return super().getMessage()
+
+
+@overload
+def init_logging(
+    filename: Optional[StringPath] = None,
+    main_logger: str = '',
+    on_error: Optional[Callable[
+        [type[BaseException], BaseException, Optional[TracebackType]],
+        None,
+    ]] = None,
+) -> logging.Logger: ...
+
+
+@overload
+def init_logging(
+    filename: Optional[StringPath] = None,
+    main_logger: str = '',
+    *,
+    error: Callable[[BaseException], object],
+) -> logging.Logger: ...
+
+
+def init_logging(
+    filename: Optional[StringPath] = None,
+    main_logger: str = '',
+    on_error: Optional[Callable[
+        [type[BaseException], BaseException, Optional[TracebackType]],
+        None,
+    ]] = None,
+    *,
+    error: Optional[Callable[[BaseException], object]] = None,
+) -> logging.Logger:
+    """Set up the logger and logging handlers.
+
+    This also sets :py:func:`sys.excepthook`, so uncaught exceptions are captured.
+
+    If an unhandled :external:py:class:`BaseException` is raised, this will not log or call the
+    callback.
+
+    :param filename: If this is set, all logs will be written to this file as well.
+    :param error: should be a function to call when uncaught exceptions are thrown.
+    :param on_error: Deprecated version of ``error`` with the old-style exception tuple \
+      representation from Python 3.10 and below.
+    :param main_logger: Specify the name of the logger to produce under the `srctools` hierachy.
+    """
+    if logging.getLogRecordFactory() is not logging.LogRecord:
+        raise ValueError('Unknown record factory: ', logging.getLogRecordFactory())
+    logging.setLogRecordFactory(NewLogRecord)
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    if on_error is not None:
+        if error is not None:
+            raise TypeError('Cannot pass both on_error and error!')
+
+        def error_closure(exc: BaseException) -> None:
+            """Call the old error handler function."""
+            if on_error is not None:  # Mypy can't infer this is constant.
+                on_error(type(exc), exc, exc.__traceback__)
+
+        error = error_closure
+
+    # Put more info in the log file, since it's not onscreen.
+    long_log_format = Formatter(
+        '[{levelname}]{srctools_context} {module}.{funcName}(): {message}',
+        expand_exc_groups=True,
+    )
+    # Console messages, etc.
+    short_log_format = Formatter(
+        # One letter for level name
+        '[{levelname[0]}]{srctools_context} {module}.{funcName}(): {message}',
+        expand_exc_groups=False,
+    )
+
+    if filename is not None:
+        # Make the directories the logs are in, if needed.
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+        # The log contains DEBUG and above logs.
+        log_handler = get_handler(filename)
+        log_handler.setLevel(logging.DEBUG)
+        log_handler.setFormatter(long_log_format)
+        logger.addHandler(log_handler)
+
+    if sys.stdout is not None:
+        stdout_loghandler = logging.StreamHandler(sys.stdout)
+        stdout_loghandler.setLevel(
+            logging.DEBUG
+            if os.environ.get('SRCTOOLS_DEBUG', '0') == '1' else
+            logging.INFO
+        )
+        stdout_loghandler.setFormatter(short_log_format)
+        logger.addHandler(stdout_loghandler)
+
+        if sys.stderr is not None:
+            def ignore_warnings(record: logging.LogRecord) -> bool:
+                """Filter out messages higher than WARNING.
+
+                Those are handled by stdError, and we don't want duplicates.
+                """
+                return record.levelno < logging.WARNING
+            stdout_loghandler.addFilter(ignore_warnings)
+    else:
+        sys.stdout = NullStream()
+
+    if sys.stderr is not None:
+        stderr_loghandler = logging.StreamHandler(sys.stderr)
+        stderr_loghandler.setLevel(logging.WARNING)
+        stderr_loghandler.setFormatter(short_log_format)
+        logger.addHandler(stderr_loghandler)
+    else:
+        sys.stderr = NullStream()
+
+    # Use the exception hook to report uncaught exceptions, and finalise the
+    # logging system.
+    old_except_handler = sys.excepthook
+
+    def except_handler(
+        exc_type: type[BaseException],
+        exc_value: BaseException,
+        exc_tb: Optional[TracebackType],
+    ) -> None:
+        """Log uncaught exceptions."""
+        if isinstance(exc_value, SystemExit):
+            # Suppress messages for this, it's not an error.
+            # We can't ignore BaseException, MultiError is one of those.
+            return
+
+        logger.error(
+            'Uncaught Exception:',
+            exc_info=(exc_type, exc_value, exc_tb),
+        )
+        if error is not None:
+            error(exc_value)
+        # Call the original handler - that prints to the normal console.
+        if old_except_handler is not sys.__excepthook__:
+            old_except_handler(exc_type, exc_value, exc_tb)
+
+    sys.excepthook = except_handler
+
+    if main_logger:
+        return get_logger(main_logger)
+    else:
+        return cast(logging.Logger, LoggerAdapter(logger))
+
+
+def get_logger(name: str = '', alias: Optional[str] = None) -> logging.Logger:
+    """Get the named logger object.
+
+    This puts the logger into the ``srctools`` namespace, and wraps it to
+    use :external:py:meth:`str.format()` instead of ``%`` formatting.
+    If set, ``alias`` is the name to show for the module.
+    """
+    if name:
+        log = logging.getLogger('srctools.' + name)
+    else:  # Allow retrieving the main logger.
+        log = logging.getLogger('srctools')
+    return cast(logging.Logger, LoggerAdapter(log, alias))
+
+
+@contextlib.contextmanager
+def context(name: str) -> Generator[str, None, None]:
+    """Context manager to allow specifying additional information for any logs contained in this block.
+
+    The specified string gets included in the log messages.
+    """
+    try:
+        stack = CTX_STACK.get()
+    except LookupError:
+        stack = []
+        CTX_STACK.set(stack)
+    stack.append(name)
+    try:
+        yield name
+    finally:
+        popped = stack.pop()
+        assert popped is name, f'Popped incorrect value: pop({popped!r}) != ctx({name!r})!'
---
+++ packages/srctools/srctools-2.6.1-pp310-pypy310_pp73-macosx_10_15_x86_64.whl/srctools/surfaceprop.py
@@ -0,0 +1,263 @@
+"""Parse surfaceproperties files, to determine surface physics.
+"""
+from typing import TYPE_CHECKING, Optional, TypeVar
+from enum import Enum
+
+import attrs
+
+from srctools.filesys import File, FileSystem
+from srctools.keyvalues import Keyvalues
+
+
+__all__ = ['SurfChar', 'SurfaceProp']
+
+
+class SurfChar(Enum):
+    """Code classification for this material.
+
+    This is a single ASCII character.
+    """
+    ANTLION = 'A'
+    BLOODYFLESH = 'B'
+    CONCRETE = 'C'
+    DIRT = 'D'
+    EGGSHELL = 'E'  #: The egg sacs in the tunnels in EP2.
+    FLESH = 'F'
+    GRATE = 'G'
+    ALIENFLESH = 'H'
+    CLIP = 'I'
+    GRASS = 'J'  #: L4D addition
+    #: In ASW, this is mud. In CSGO it's snow.
+    MUD_ASW = SNOW = 'K'
+    PLASTIC = 'L'
+    METAL = 'M'
+    SAND = 'N'
+    FOLIAGE = 'O'
+    COMPUTER = 'P'
+    ASPHALT = 'Q'  #: L4D addition
+    #: 2013 and P2 assigns this to reflective, brick in L4D+
+    REFLECTIVE = BRICK = 'R'
+    SLOSH = 'S'
+    TILE = 'T'
+    CARDBOARD = 'U'  #: L4D addition
+    VENT = 'V'
+    WOOD = 'W'
+    NOFX = 'X'  #: "fake" materials use this (ladders, wading, clips, etc)
+    GLASS = 'Y'
+    WARPSHIELD = 'Z'  #: Weird-looking jello effect for advisor shield.
+
+    #: L4D adds these:
+    CLAY = '1'
+    PLASTER = '2'
+    ROCK = '3'
+    RUBBER = '4'
+    SHEETROCK = '5'
+    CLOTH = '6'
+    CARPET = '7'
+    PAPER = '8'
+    UPHOLSTERY = '9'
+    PUDDLE = '10'
+    MUD_L4D = '11'
+
+    #: ASW, shoots out steam
+    STEAM_PIPE = '11'
+
+    #: CSGO
+    SANDBARREL = '12'
+
+
+T = TypeVar('T')
+
+
+@attrs.define(init=False)
+class SurfaceProp:
+    """A material surface type."""
+    name: str
+    density: float = 2000
+    elasticity: float = 0.25
+    friction: float = 0.8
+    dampening: float = 0.0
+    thickness: float = -1.0
+
+    snd_stepleft: str = "Default.StepLeft"
+    snd_stepright: str = "Default.StepRight"
+    snd_bulletimpact: str = "Default.BulletImpact"
+    snd_scraperough: str = "Default.ScrapeRough"
+    snd_scrapesmooth: str = "Default.ScrapeSmooth"
+    snd_impacthard: str = "Default.ImpactHard"
+    snd_impactsoft: str = "Default.ImpactSoft"
+    snd_break: str = ""
+    snd_strain: str = ""
+    snd_shake: str = ""
+    snd_roll: str = ""
+
+    audio_reflectivity: float = 0.66
+    audio_hardness_factor: float = 1.0
+    audio_roughness_factor: float = 1.0
+
+    scrape_rough_threshold: float = 0.0
+    impact_hard_threshold: float = 0.5
+    hard_velocity_threshold: float = 0.0
+
+    gamematerial: SurfChar = SurfChar.CONCRETE
+    jump_factor: float = 1.0
+    max_speed_factor: float = 1.0
+    climbable: bool = False
+
+    if TYPE_CHECKING:
+        def __init__(
+            self,
+            name: str,
+            parent: Optional['SurfaceProp'] = None,
+            *,
+            density: Optional[float] = None,
+            elasticity: Optional[float] = None,
+            friction: Optional[float] = None,
+            dampening: Optional[float] = None,
+            thickness: Optional[float] = None,
+
+            snd_stepleft: Optional[str] = None,
+            snd_stepright: Optional[str] = None,
+            snd_bulletimpact: Optional[str] = None,
+            snd_scraperough: Optional[str] = None,
+            snd_scrapesmooth: Optional[str] = None,
+            snd_impacthard: Optional[str] = None,
+            snd_impactsoft: Optional[str] = None,
+            snd_strain: Optional[str] = None,
+            snd_break: Optional[str] = None,
+            snd_roll: Optional[str] = None,
+            snd_shake: Optional[str] = None,
+
+            audio_reflectivity: Optional[float] = None,
+            audio_hardness_factor: Optional[float] = None,
+            audio_roughness_factor: Optional[float] = None,
+            scrape_rough_threshold: Optional[float] = None,
+            impact_hard_threshold: Optional[float] = None,
+            hard_velocity_threshold: Optional[float] = None,
+
+            gamematerial: Optional[SurfChar] = None,
+            jump_factor: Optional[float] = None,
+            max_speed_factor: Optional[float] = None,
+            climbable: bool = False,
+        ) -> None:
+            """Create a surfaceprop definition.
+
+            If parent is passed, it will be used for any unset values.
+            """
+    else:
+        def __init__(self, name: str, parent: Optional['SurfaceProp'] = None, **kwargs: object) -> None:
+            """Create a surfaceprop definition.
+
+            If parent is passed, it will be used for any unset values.
+            """
+            self.name = name
+            for field in attrs.fields(type(self)):
+                field_name = field.name
+                if field_name == 'name':
+                    continue
+                value = kwargs.get(field_name)
+                if value is not None:
+                    if not isinstance(value, field.type):
+                        # Special case, int->float is fine, everything else is not.
+                        if field.type is float and isinstance(value, int):
+                            value = float(value)
+                        else:
+                            raise TypeError(f'{field_name} must be of type {field.type.__name__}, not {value!r}')
+                    setattr(self, field_name, value)
+                elif parent is not None:
+                    setattr(self, field_name, getattr(parent, field_name))
+                else:
+                    setattr(self, field_name, field.default)
+
+    def copy(self) -> 'SurfaceProp':
+        """Duplicate this surfaceprop."""
+        return attrs.evolve(self)
+
+    __copy__ = copy
+
+    @staticmethod
+    def parse_file(
+        props: Keyvalues,
+        prev: Optional[dict[str, 'SurfaceProp']] = None,
+    ) -> dict[str, 'SurfaceProp']:
+        """Parse surfaceproperties from a file.
+
+        :param props: The keyvalues block to parse.
+        :param prev: If passed, this is used to read parent properties from.
+
+        A blank "default" surfaceprop  will be generated if not already present.
+        """
+        if prev is None:
+            prev = {}
+
+        try:
+            default = prev['default']
+        except KeyError:
+            default = prev['default'] = SurfaceProp('default')
+
+        for prop in props:
+            try:
+                base = prev[prop['base'].casefold()]
+            except KeyError:
+                raise ValueError(f'Missing base surface "{prop["base"]}"') from None
+            except IndexError:  # Not in keyvalues.
+                base = default
+
+            game_mat: Optional[SurfChar]
+            try:
+                game_mat = SurfChar(prop['gamematerial'])
+            except (LookupError, ValueError):
+                game_mat = None
+
+            prev[prop.name] = SurfaceProp(
+                prop.real_name or '',
+                base,
+                density=prop.float('density', None),
+                elasticity=prop.float('elasticity', None),
+                friction=prop.float('friction', None),
+                dampening=prop.float('dampening', None),
+
+                snd_stepleft=prop['stepleft', None],
+                snd_stepright=prop['stepright', None],
+                snd_bulletimpact=prop['bulletimpact', None],
+                snd_scraperough=prop['scraperough', None],
+                snd_scrapesmooth=prop['scrapesmooth', None],
+                snd_impacthard=prop['impacthard', None],
+                snd_impactsoft=prop['impactsoft', None],
+                snd_strain=prop['strain', None],
+                snd_break=prop['break', None],
+                snd_roll=prop['roll', None],
+
+                audio_reflectivity=prop.float('audioreflectivity', None),
+                audio_hardness_factor=prop.float('audiohardnessfactor', None),
+                audio_roughness_factor=prop.float('audioroughnessfactor', None),
+                scrape_rough_threshold=prop.float('scrapeRoughThreshold', None),
+                impact_hard_threshold=prop.float('impactHardThreshold', None),
+                hard_velocity_threshold=prop.float('audioHardMinVelocity', None),
+
+                gamematerial=game_mat,
+                jump_factor=prop.float('jump_factor', None),
+                climbable=prop.bool('climbable', False),
+            )
+
+        return prev
+
+    @staticmethod
+    def parse_manifest(fsys: FileSystem[T], file: Optional[File[FileSystem[T]]] = None) -> dict[str, 'SurfaceProp']:
+        """Load surfaceproperties from a manifest.
+
+        :file:`scripts/surfaceproperties_manifest.txt` will be used if a file is
+        not specified.
+        """
+        if not file:
+            file = fsys['scripts/surfaceproperties_manifest.txt']
+
+        with file.open_str() as f:
+            manifest = Keyvalues.parse(f, file.path)
+
+        surf: dict[str, SurfaceProp] = {}
+
+        for prop in manifest.find_all('surfaceproperties_manifest', 'file'):
+            surf = SurfaceProp.parse_file(fsys.read_kv1(prop.value), surf)
+
+        return surf
