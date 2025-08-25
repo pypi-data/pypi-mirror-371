7afae7bba12196b752478a961ebc22f12be38c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 15:12:51 +0000

    Add package maleo-identity/0.0.90/maleo_identity-0.0.90-py3-none-any.whl

---
+++ packages/maleo-identity/maleo_identity-0.0.90-py3-none-any.whl/maleo/identity/mixins/user_organization_role.py
@@ -0,0 +1,6 @@
+from maleo.soma.mixins.parameter import Expand as BaseExpand
+from maleo.identity.enums.user_organization_role import ExpandableField
+
+
+class Expand(BaseExpand[ExpandableField]):
+    pass
