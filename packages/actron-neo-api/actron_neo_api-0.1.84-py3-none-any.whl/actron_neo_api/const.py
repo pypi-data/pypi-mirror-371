d550bac00292f72e04c2fbdc4c8c0e547d006d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 03:29:32 +0000

    Add package actron-neo-api/0.1.84/actron_neo_api-0.1.84-py3-none-any.whl

---
+++ packages/actron-neo-api/actron_neo_api-0.1.84-py3-none-any.whl/actron_neo_api/const.py
@@ -0,0 +1,1 @@
+DEFAULT_BASE_URL = "https://nimbus.actronair.com.au"
---
+++ packages/actron-neo-api/actron_neo_api-0.1.84-py3-none-any.whl/actron_neo_api/exceptions.py
@@ -0,0 +1,7 @@
+class ActronNeoAuthError(Exception):
+    """Exception raised for authentication-related errors."""
+    pass
+
+class ActronNeoAPIError(Exception):
+    """Exception raised for general API errors."""
+    pass
