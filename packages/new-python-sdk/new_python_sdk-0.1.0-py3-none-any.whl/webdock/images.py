f141279bd2210d4a83c816b8543e25c11ce3446f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 04:09:43 +0000

    Add package new-python-sdk/0.1.0/new_python_sdk-0.1.0-py3-none-any.whl

---
+++ packages/new-python-sdk/new_python_sdk-0.1.0-py3-none-any.whl/webdock/images.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from typing import List, Literal, TypedDict
+
+from utils.req import RequestOptions, req
+
+class ServerImage(TypedDict):
+    slug: str
+    name: str
+    webServer: Literal["Apache", "Nginx", None]
+    phpVersion: str | None
+
+
+class ListImageTypeResponse(TypedDict):
+    body: List[ServerImage]
+
+
+class Images:
+    def __init__(self, parent: "Webdock") -> None:
+        self.parent = parent
+
+    def list(self):
+        return req(
+            RequestOptions(
+                token=self.parent.string_token,
+                endpoint="/images",
+                headers=[],
+                method="GET",
+            ),
+            ListImageTypeResponse
+        )
+
+
