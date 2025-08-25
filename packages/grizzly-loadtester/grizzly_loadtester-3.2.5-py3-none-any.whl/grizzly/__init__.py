9e6eba1d35efff6ebe021c7681f9b206126485c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 15:12:33 +0000

    Add package grizzly-loadtester/3.2.5/grizzly_loadtester-3.2.5-py3-none-any.whl

---
+++ packages/grizzly-loadtester/grizzly_loadtester-3.2.5-py3-none-any.whl/grizzly/__init__.py
@@ -0,0 +1,16 @@
+"""Any import from a grizzly module should intialize version (grizzly and locust) variables."""
+
+from gevent import monkey
+
+monkey.patch_all()
+
+from importlib.metadata import PackageNotFoundError, version
+
+from .__version__ import __version__
+
+try:
+    __locust_version__ = version('locust')
+except PackageNotFoundError:  # pragma: no coverage
+    __locust_version__ = '<unknown>'
+
+__all__ = ['__version__']
