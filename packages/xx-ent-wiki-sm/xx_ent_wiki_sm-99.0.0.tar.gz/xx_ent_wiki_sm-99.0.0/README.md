370d00f81afe56fe15c104e5d099a7cb4bca9c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 15:12:27 +0000

    Add package xx-ent-wiki-sm/99.0.0/xx_ent_wiki_sm-99.0.0.tar.gz

---
+++ packages/xx-ent-wiki-sm/xx_ent_wiki_sm-99.0.0.tar.gz/xx_ent_wiki_sm-99.0.0/README.md
@@ -0,0 +1,1 @@
+# xx_ent_wiki_sm\nA dummy Python package version of xx_ent_wiki_sm.
---
+++ packages/xx-ent-wiki-sm/xx_ent_wiki_sm-99.0.0.tar.gz/xx_ent_wiki_sm-99.0.0/src/ca_certificates/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "99.0.0"
+
+def info():
+    return "This is a dummy xx_ent_wiki_sm package for PyPI."
