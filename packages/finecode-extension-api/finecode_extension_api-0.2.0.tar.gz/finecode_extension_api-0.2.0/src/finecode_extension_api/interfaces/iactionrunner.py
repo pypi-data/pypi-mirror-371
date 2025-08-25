141a425e5c4bce30779cffa366547e78b0ad9327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 15:12:40 +0000

    Add package finecode-extension-api/0.2.0/finecode_extension_api-0.2.0.tar.gz

---
+++ packages/finecode-extension-api/finecode_extension_api-0.2.0.tar.gz/finecode_extension_api-0.2.0/src/finecode_extension_api/interfaces/iactionrunner.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from typing import Any, Protocol
+
+
+class IActionRunner(Protocol):
+    async def run_action(
+        self, name: str, payload: dict[str, Any]
+    ) -> dict[str, Any]: ...
+
+
+class BaseRunActionException(Exception):
+    def __init__(self, message: str) -> None:
+        self.message = message
+
+
+class ActionNotFound(BaseRunActionException): ...
+
+
+class InvalidActionRunPayload(BaseRunActionException): ...
+
+
+class ActionRunFailed(BaseRunActionException):
+    pass
