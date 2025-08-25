13eeb51baccc70f264af6359b24e078284abf303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 03:29:24 +0000

    Add package academia-mcp/1.3.0/academia_mcp-1.3.0.tar.gz

---
+++ packages/academia-mcp/academia_mcp-1.3.0.tar.gz/academia_mcp-1.3.0/academia_mcp/tools/__init__.py
@@ -0,0 +1,30 @@
+from .arxiv_search import arxiv_search
+from .anthology_search import anthology_search
+from .arxiv_download import arxiv_download
+from .hf_datasets_search import hf_datasets_search
+from .s2_citations import s2_get_references, s2_get_citations
+from .document_qa import document_qa
+from .md_to_pdf import md_to_pdf
+from .web_search import web_search, tavily_web_search, exa_web_search, brave_web_search
+from .visit_webpage import visit_webpage
+from .bitflip import extract_bitflip_info, generate_research_proposal, score_research_proposals
+
+
+__all__ = [
+    "arxiv_search",
+    "arxiv_download",
+    "anthology_search",
+    "s2_get_references",
+    "s2_get_citations",
+    "hf_datasets_search",
+    "document_qa",
+    "md_to_pdf",
+    "web_search",
+    "tavily_web_search",
+    "exa_web_search",
+    "brave_web_search",
+    "visit_webpage",
+    "extract_bitflip_info",
+    "generate_research_proposal",
+    "score_research_proposals",
+]
