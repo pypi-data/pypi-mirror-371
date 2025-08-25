bd13adabf9f5a3cb00b5eda8e86da769c65f1d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 14:05:42 +0000

    Add package cli-lite/0.11.2/cli_lite-0.11.2.tar.gz

---
+++ packages/cli-lite/cli_lite-0.11.2.tar.gz/cli_lite-0.11.2/clilte/__init__.py
@@ -0,0 +1,6 @@
+from .core import BasePlugin as BasePlugin
+from .core import PluginMetadata as PluginMetadata
+from .core import CommandLine as CommandLine
+from .core import register as register
+from .builtin import Version as Version
+from .builtin import Cache as Cache
