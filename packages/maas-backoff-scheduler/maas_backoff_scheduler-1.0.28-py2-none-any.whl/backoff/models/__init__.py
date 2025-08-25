0eba6eec5ff564f90ae6660c10a944efa313a6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 15:12:24 +0000

    Add package maas-backoff-scheduler/1.0.28/maas_backoff_scheduler-1.0.28-py2-none-any.whl

---
+++ packages/maas-backoff-scheduler/maas_backoff_scheduler-1.0.28-py2-none-any.whl/backoff/models/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""models模块初始化文件"""
+
+# 从backoff_threadpool.py导入
+from .backoff_threadpool import BackoffThreadPool
+
+# 从redis_client.py导入
+from .redis_client import RedisClient, get_redis_client, init_redis_client
+
+# 从repository子模块导入
+from backoff.repository.storage_adapter import StorageAdapter, StorageType
+from backoff.repository.redis_adapter import RedisStorageAdapter
+from backoff.repository.storage_adapter import StorageAdapterFactory
+
+# 导出所有类和函数
+__all__ = [
+    'BackoffThreadPool',
+    'RedisClient',
+    'get_redis_client',
+    'init_redis_client',
+    'StorageAdapter',
+    'StorageType',
+    'RedisStorageAdapter'
+]
+
+
