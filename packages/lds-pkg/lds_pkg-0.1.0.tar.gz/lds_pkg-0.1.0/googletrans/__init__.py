c6d4093a34c2200d163dae3917fe65bfc7fc0aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 15:12:31 +0000

    Add package lds-pkg/0.1.0/lds_pkg-0.1.0.tar.gz

---
+++ packages/lds-pkg/lds_pkg-0.1.0.tar.gz/lds_pkg-0.1.0/googletrans/__init__.py
@@ -0,0 +1,8 @@
+"""Free Google Translate API for Python. Translates totally free of charge."""
+__all__ = 'Translator',
+__version__ = '4.0.0-rc.1'
+
+
+from .client import Translator
+from .constants import LANGCODES, LANGUAGES  # noqa
+from .async_client import Translator as AsyncTranslator
