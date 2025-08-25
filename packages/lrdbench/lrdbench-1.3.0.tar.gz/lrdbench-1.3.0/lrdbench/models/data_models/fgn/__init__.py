873de3e8751a62a6e7ed82a0d1d09f5cc3a7ee1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 04:09:24 +0000

    Add package lrdbench/1.3.0/lrdbench-1.3.0.tar.gz

---
+++ packages/lrdbench/lrdbench-1.3.0.tar.gz/lrdbench-1.3.0/lrdbench/models/data_models/fgn/__init__.py
@@ -0,0 +1,13 @@
+"""
+Fractional Gaussian Noise (fGn) subpackage.
+
+Exposes the `FractionalGaussianNoise` model for generating stationary
+increments of fractional Brownian motion.
+"""
+
+from .fgn_model import FractionalGaussianNoise
+
+__all__ = ["FractionalGaussianNoise"]
+
+
+
