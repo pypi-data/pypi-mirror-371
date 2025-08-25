9a73e4cf49bbbbea88d44a87ca84165fc23dc03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 14:06:18 +0000

    Add package servicenow-api/1.0.4/servicenow_api-1.0.4-py2.py3-none-any.whl

---
+++ packages/servicenow-api/servicenow_api-1.0.4-py2.py3-none-any.whl/servicenow_api/__init__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from servicenow_api.version import __version__, __author__, __credits__
+from servicenow_api.servicenow_api import Api
+from servicenow_api.servicenow_api_mcp import main
+from servicenow_api.servicenow_models import (
+    ApplicationServiceModel,
+    CMDBModel,
+    CICDModel,
+    ChangeManagementModel,
+    IncidentModel,
+    ImportSetModel,
+    TableModel,
+)
+
+"""
+ServiceNow API
+
+A Python Wrapper for ServiceNow API
+"""
+
+__version__ = __version__
+__author__ = __author__
+__credits__ = __credits__
+
+__all__ = [
+    "Api",
+    "main",
+    "ApplicationServiceModel",
+    "CMDBModel",
+    "CICDModel",
+    "ChangeManagementModel",
+    "ChangeManagementModel",
+    "IncidentModel",
+    "ImportSetModel",
+    "TableModel",
+]
