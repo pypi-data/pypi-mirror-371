9efe2fb8b899b3b63566062a8a0fd361e16a28d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 04:09:50 +0000

    Add package new-python-sdk/5.0.3/new_python_sdk-5.0.3.tar.gz

---
+++ packages/new-python-sdk/new_python_sdk-5.0.3.tar.gz/new_python_sdk-5.0.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+try:
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = "Webdock Python SDK"
+
+setup(
+    name="new-python-sdk",
+    version="5.0.3",
+    author="ahmad",
+    author_email="a4amado@gmail.com",
+    description="Webdock Python SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "requests>=2.31.0,<3",
+    ],
+    keywords="webdock, sdk, api, cloud, hosting"
+
+)
