a264d901c88a533de71aae3fed579f8539ee85f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 05:06:59 +0000

    Add package z8ter/0.1.1/z8ter-0.1.1-py3-none-any.whl

---
+++ packages/z8ter/z8ter-0.1.1-py3-none-any.whl/z8ter/config.py
@@ -0,0 +1,8 @@
+from starlette.config import Config
+from z8ter import BASE_DIR
+
+
+def build_config(env_file: str) -> Config:
+    cf = Config(env_file)
+    cf.file_values["BASE_DIR"] = str(BASE_DIR)
+    return cf
