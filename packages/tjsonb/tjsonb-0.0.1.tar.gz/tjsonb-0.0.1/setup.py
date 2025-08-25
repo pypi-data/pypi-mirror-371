49a8713a7008375968969bab6fce7b335bf676c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 04:09:46 +0000

    Add package tjsonb/0.0.1/tjsonb-0.0.1.tar.gz

---
+++ packages/tjsonb/tjsonb-0.0.1.tar.gz/tjsonb-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="tjsonb",
+    version="0.0.1",
+    description="Python bindings for tjsonb PostgreSQL extension",
+    packages=["tjsonb"],
+    python_requires=">=3.7",
+)
