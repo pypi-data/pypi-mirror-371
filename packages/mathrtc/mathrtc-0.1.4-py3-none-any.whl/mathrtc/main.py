630346fbdba45809e6ac7610f4b109463547d34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-25 15:12:07 +0000

    Add package mathrtc/0.1.4/mathrtc-0.1.4-py3-none-any.whl

---
+++ packages/mathrtc/mathrtc-0.1.4-py3-none-any.whl/mathrtc/main.py
@@ -0,0 +1,14 @@
+def fact():
+  try:
+      num = int(input("Enter a number: "))
+
+      if num < 0:
+          print("Error: Factorial is not defined for negative numbers.")
+      else:
+          fact = 1
+          for i in range(1, num + 1):
+              fact *= i
+          print("Factorial:", fact)
+
+  except ValueError:
+      print("Error: Please enter a valid integer.")
